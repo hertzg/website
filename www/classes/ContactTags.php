0d1b7aa5777c972aa4e4cd4ce411dbbcb35d5e0c
Archil Imnadze <archil@imnadze.ge>
2013-12-24 15:07:27 +0000

    added ContactTags and NoteTags

---
+++ www/classes/ContactTags.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once __DIR__.'/../fns/mysqli_query_object.php';
+include_once __DIR__.'/../lib/mysqli.php';
+
+class ContactTags {
+
+    static function add ($idusers, $idcontacts, array $tagnames) {
+        global $mysqli;
+        foreach ($tagnames as $tagname) {
+            $tagname = mysqli_real_escape_string($mysqli, $tagname);
+            mysqli_query(
+                $mysqli,
+                'insert into contacttags (idusers, idcontacts, tagname)'
+                ." values ($idusers, $idcontacts, '$tagname')"
+            );
+        }
+    }
+
+    static function deleteOnUser ($idusers) {
+        global $mysqli;
+        mysqli_query($mysqli, "delete from contacttags where idusers = $idusers");
+    }
+
+    static function deleteOnContact ($idcontacts) {
+        global $mysqli;
+        mysqli_query(
+            $mysqli,
+            "delete from contacttags where idcontacts = $idcontacts"
+        );
+    }
+
+    static function indexOnContact ($idcontacts) {
+        global $mysqli;
+        return mysqli_query_object(
+            $mysqli,
+            'select * from contacttags'
+            ." where idcontacts = $idcontacts order by tagname"
+        );
+    }
+
+    static function indexOnUser ($idusers) {
+        global $mysqli;
+        return mysqli_query_object(
+            $mysqli,
+            'select distinct tagname from contacttags'
+            ." where idusers = $idusers order by tagname"
+        );
+    }
+
+}
---
+++ www/classes/NoteTags.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once __DIR__.'/../fns/mysqli_query_object.php';
+include_once __DIR__.'/../lib/mysqli.php';
+
+class NoteTags {
+
+    static function add ($idusers, $idnotes, array $tagnames) {
+        global $mysqli;
+        foreach ($tagnames as $tagname) {
+            $tagname = mysqli_real_escape_string($mysqli, $tagname);
+            mysqli_query(
+                $mysqli,
+                'insert into notetags (idusers, idnotes, tagname)'
+                ." values ($idusers, $idnotes, '$tagname')"
+            );
+        }
+    }
+
+    static function deleteOnUser ($idusers) {
+        global $mysqli;
+        mysqli_query($mysqli, "delete from notetags where idusers = $idusers");
+    }
+
+    static function deleteOnNote ($idnotes) {
+        global $mysqli;
+        mysqli_query(
+            $mysqli,
+            "delete from notetags where idnotes = $idnotes"
+        );
+    }
+
+    static function indexOnNote ($idnotes) {
+        global $mysqli;
+        return mysqli_query_object(
+            $mysqli,
+            'select * from notetags'
+            ." where idnotes = $idnotes order by tagname"
+        );
+    }
+
+    static function indexOnUser ($idusers) {
+        global $mysqli;
+        return mysqli_query_object(
+            $mysqli,
+            'select distinct tagname from notetags'
+            ." where idusers = $idusers order by tagname"
+        );
+    }
+
+}
