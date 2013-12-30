d3e8e3731978ba447c23f77dfa1f38af587b1107
Archil Imnadze <archil@imnadze.ge>
2013-12-30 15:28:33 +0000

    add larger icons for firefox marketplace

--- www/manifest.php
+++ www/manifest.php
@@ -1,9 +1,9 @@
 <?php
 
-header('Content-Type: application/json');
+header('Content-Type: application/x-web-app-manifest+json');
 echo json_encode(array(
     'name' => 'Zvini',
-    'description' => 'Save Your Data in Zvini. Save your files, contacts, notes and more. It\'s free and easy.',
+    'description' => 'Save your data in Zvini. Save your files, contacts, notes and more. It\'s free and easy.',
     'developer' => array(
         'name' => 'Zvini Developers',
         'url' => 'http://zvini.com/',
@@ -11,7 +11,11 @@ echo json_encode(array(
     'icons' => array(
         16 => '/images/icon16.png',
         32 => '/images/icon32.png',
+        60 => '/images/icon60.png',
         64 => '/images/icon64.png',
+        90 => '/images/icon90.png',
+        120 => '/images/icon120.png',
         128 => '/images/icon128.png',
+        256 => '/images/icon256.png',
     ),
 ));
