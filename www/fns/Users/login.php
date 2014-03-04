73ce12bed74762c075cb6dc0a63d83b9315e676e
Archil Imnadze <archil@imnadze.ge>
2014-03-04 19:37:51 +0000

    clear reset_password_key on login

--- www/fns/Users/login.php
+++ www/fns/Users/login.php
@@ -5,6 +5,7 @@ namespace Users;
 function login ($mysqli, $idusers) {
     $lastlogintime = time();
     $sql = 'update users set num_logins = num_logins + 1,'
+        .' reset_password_key = null, reset_password_key_time = null,'
         ." lastlogintime = $lastlogintime where idusers = $idusers";
     $mysqli->query($sql);
 }
