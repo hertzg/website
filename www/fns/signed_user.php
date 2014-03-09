91ee37b6dd0ce31114e76d90182f3ef19451aec5
Archil Imnadze <archil@imnadze.ge>
2014-03-09 19:17:54 +0000

    simplified lib/user.php to remove later

---
+++ www/fns/signed_user.php
@@ -0,0 +1,52 @@
+<?php
+
+function signed_user (mysqli $mysqli) {
+
+    include_once __DIR__.'/session_start_custom.php';
+    session_start_custom();
+
+    $user = $idusers = null;
+
+    if (!array_key_exists('user', $_SESSION)) {
+
+        include_once __DIR__.'/require_valid_token.php';
+        $token = require_valid_token($mysqli);
+
+        if ($token) {
+
+            include_once __DIR__.'/Users/get.php';
+            $user = Users\get($mysqli, $token->idusers);
+
+            if ($user) {
+                $_SESSION['user'] = $user;
+                $_SESSION['token'] = $token;
+            }
+
+        }
+
+    }
+
+    if (array_key_exists('user', $_SESSION)) {
+
+        include_once __DIR__.'/Users/get.php';
+        $user = Users\get($mysqli, $_SESSION['user']->idusers);
+
+        if ($user) {
+            setcookie('username', $user->username, time() + 60 * 60 * 24 * 30, '/');
+            $idusers = $user->idusers;
+            if (array_key_exists('token', $_SESSION)) {
+
+                $token = $_SESSION['token'];
+                setcookie('token', bin2hex($token->tokentext), time() + 60 * 60 * 24 * 30, '/');
+
+                include_once __DIR__.'/Tokens/updateAccessTime.php';
+                Tokens\updateAccessTime($mysqli, $token->tokentext);
+
+            }
+        }
+
+    }
+
+    return array($user, $idusers);
+
+}
--- www/lib/user.php
+++ www/lib/user.php
@@ -1,46 +1,5 @@
 <?php
 
-include_once __DIR__.'/../fns/session_start_custom.php';
-session_start_custom();
-
-$user = $idusers = null;
-if (!array_key_exists('user', $_SESSION)) {
-
-    include_once __DIR__.'/../fns/require_valid_token.php';
-    include_once __DIR__.'/mysqli.php';
-    $token = require_valid_token($mysqli);
-
-    if ($token) {
-
-        include_once __DIR__.'/../fns/Users/get.php';
-        $user = Users\get($mysqli, $token->idusers);
-
-        if ($user) {
-            $_SESSION['user'] = $user;
-            $_SESSION['token'] = $token;
-        }
-
-    }
-
-}
-if (array_key_exists('user', $_SESSION)) {
-
-    include_once __DIR__.'/../fns/Users/get.php';
-    include_once __DIR__.'/../lib/mysqli.php';
-    $user = Users\get($mysqli, $_SESSION['user']->idusers);
-
-    if ($user) {
-        setcookie('username', $user->username, time() + 60 * 60 * 24 * 30, '/');
-        $idusers = $user->idusers;
-        if (array_key_exists('token', $_SESSION)) {
-
-            $token = $_SESSION['token'];
-            setcookie('token', bin2hex($token->tokentext), time() + 60 * 60 * 24 * 30, '/');
-
-            include_once __DIR__.'/../fns/Tokens/updateAccessTime.php';
-            Tokens\updateAccessTime($mysqli, $token->tokentext);
-
-        }
-    }
-
-}
+include_once __DIR__.'/../fns/signed_user.php';
+include_once __DIR__.'/mysqli.php';
+list($user, $idusers) = signed_user($mysqli);
