d7f07ff56a6fd63a3525103d8781006dcd7a3639
Archil Imnadze <archil@imnadze.ge>
2014-03-12 11:06:14 +0000

    restored media queries

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = array(
-        'common.compressed.css' => 2,
+        'common.compressed.css' => 3,
     );
     return $revisions[$key];
 }
