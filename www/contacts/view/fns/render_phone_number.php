622e3d5cd2df237cc584875f6450698771903c6a
Archil Imnadze <archil@imnadze.ge>
2014-03-13 14:13:33 +0000

    ingnore spaces in phone number link

--- www/contacts/view/fns/render_phone_number.php
+++ www/contacts/view/fns/render_phone_number.php
@@ -3,7 +3,7 @@
 function render_phone_number ($label, $number, &$items) {
     if ($number !== '') {
         $escapedNumber = htmlspecialchars($number);
-        $href = "tel:$escapedNumber";
+        $href = 'tel:'.preg_replace('/\s+/', '', $escapedNumber);
         include_once '../../fns/Form/link.php';
         $items[] = Form\link($label, $escapedNumber, $href, 'phone');
     }
