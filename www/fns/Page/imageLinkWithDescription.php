b20bda80f235988863ba555779a9ba011d14a2af
Archil Imnadze <archil@imnadze.ge>
2014-03-16 12:44:41 +0000

    removed unused function create_title_and_description

--- www/fns/Page/imageLinkWithDescription.php
+++ www/fns/Page/imageLinkWithDescription.php
@@ -5,8 +5,11 @@ namespace Page;
 function imageLinkWithDescription ($title, $description, $href,
     $iconName, array $options = array()) {
 
-    include_once __DIR__.'/../create_title_and_description.php';
-    $content = create_title_and_description($title, $description);
+    $content =
+        '<div class="title_and_description">'
+            ."<div>$title</div>"
+            ."<div>$description</div>"
+        .'</div>';
 
     include_once __DIR__.'/../create_image_link.php';
     return create_image_link($content, $href, $iconName, $options);
--- www/fns/create_title_and_description.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function create_title_and_description ($title, $description) {
-    return
-        '<div class="title_and_description">'
-            ."<div>$title</div>"
-            ."<div>$description</div>"
-        .'</div>';
-}
