df01d04e1a71ee467af6658dd80bbf7dea83c35f
Archil Imnadze <archil@imnadze.ge>
2014-03-19 08:00:54 +0000

    removed password matching from sql - use Password\match php function instead

--- www/fns/Users/getByUsernameAndPassword.php
+++ www/fns/Users/getByUsernameAndPassword.php
@@ -4,14 +4,13 @@ namespace Users;
 
 function getByUsernameAndPassword ($mysqli, $username, $password) {
 
-    include_once __DIR__.'/../Password/hash.php';
-    $password_hash = \Password\hash($password);
-
-    $username = $mysqli->real_escape_string($username);
-    $password = $mysqli->real_escape_string($password_hash);
-    $sql = 'select * from users'
-        ." where username = '$username' and password = '$password'";
-    include_once __DIR__.'/../mysqli_single_object.php';
-    return mysqli_single_object($mysqli, $sql);
+    include_once __DIR__.'/getByUsername.php';
+    $user = getByUsername($mysqli, $username);
+    if ($user) {
+        include_once __DIR__.'/../Password/match.php';
+        if (\Password\match($user->password, $password)) {
+            return $user;
+        }
+    }
 
 }
