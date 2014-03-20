dd7b95a95ef26a9d7c093e4d6122cfd38b91a530
Archil Imnadze <archil@imnadze.ge>
2014-03-20 14:51:19 +0000

    find-extra-new-lines code quality script

---
+++ scripts/find-extra-new-lines.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+function scan (dir) {
+    var files = fs.readdirSync(dir)
+    files.forEach(function (file) {
+
+        if (/^\./.test(file)) return
+
+        file = dir + '/' + file
+
+        if (/\.(css|js|php|sh)$/.test(file) &&
+            !(/\.(combined|compressed)\.(css|js)$/.test(file))) {
+
+            var content = fs.readFileSync(file, 'utf8')
+            if (content.match(/(\r\n|\r|\n){3}/)) {
+                foundFiles.push({
+                    file: file.substr(2),
+                })
+            }
+
+        } else {
+            var stat = fs.statSync(file)
+            if (stat.isDirectory()) {
+                scan(file)
+            }
+        }
+
+    })
+}
+
+var foundFiles = []
+
+var fs = require('fs')
+
+process.chdir(__dirname)
+process.chdir('..')
+
+scan('.')
+foundFiles.forEach(function (foundFile) {
+    console.log(foundFile.file)
+})
--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd `dirname $BASH_SOURCE`
-echo "Running find-long-lines..."
-./find-long-lines.js
+echo "Running find-extra-new-lines..."
+./find-extra-new-lines.js
 echo "Running find-large-files..."
 ./find-large-files.js
+echo "Running find-long-lines..."
+./find-long-lines.js
 echo "Running find-trailing-spaces..."
 ./find-trailing-spaces.js
