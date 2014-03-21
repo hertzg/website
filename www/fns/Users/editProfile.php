482e1a7673721b88839bfa263f65e0602463615e
Archil Imnadze <archil@imnadze.ge>
2014-03-21 08:44:33 +0000

    shortened code

--- www/fns/Users/editProfile.php
+++ www/fns/Users/editProfile.php
@@ -5,9 +5,7 @@ namespace Users;
 function editProfile ($mysqli, $idusers, $email, $full_name) {
     $email = $mysqli->real_escape_string($email);
     $full_name = $mysqli->real_escape_string($full_name);
-    $sql = 'update users set'
-        ." email = '$email',"
-        ." full_name = '$full_name'"
-        ." where idusers = $idusers";
+    $sql = "update users set email = '$email',"
+        ." full_name = '$full_name' where idusers = $idusers";
     $mysqli->query($sql);
 }
