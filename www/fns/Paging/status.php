a905c3fd05f5f191f64db8e2111a6597e7b14a10
Archil Imnadze <archil@imnadze.ge>
2014-03-21 10:06:38 +0000

    Paging-status css class

--- www/fns/Paging/status.php
+++ www/fns/Paging/status.php
@@ -4,7 +4,7 @@ namespace Paging;
 
 function status ($offset, $limit, $total, $label) {
     return
-        '<div style="background: #eee; text-align: right; font-size: 12px; line-height: 14px; color: #555; padding: 2px">'
+        '<div class="Paging-status">'
             ."Showing $label from $offset to "
             .min($offset + $limit - 1, $total)." out of total $total."
         .'</div>'
