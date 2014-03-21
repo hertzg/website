db19bf23dfae998b5820a5ba9241189b4dfebfb1
Archil Imnadze <archil@imnadze.ge>
2014-03-21 09:06:30 +0000

    rename columns id*tags to id

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -6,23 +6,8 @@ function query ($sql) {
     global $mysqli;
     $mysqli->query($sql) || trigger_error($mysqli->error);
 }
-query('alter table bookmarks change inserttime insert_time bigint unsigned not null');
-query('alter table bookmarks change updatetime update_time bigint unsigned not null');
-query('alter table bookmarktags change inserttime insert_time bigint unsigned not null');
-query('alter table bookmarktags change updatetime update_time bigint unsigned not null');
-query('alter table channels change inserttime insert_time bigint unsigned not null');
-query('alter table feedbacks change inserttime insert_time bigint unsigned not null');
-query('alter table files change inserttime insert_time bigint unsigned not null');
-query('alter table folders change inserttime insert_time bigint unsigned not null');
-query('alter table notes change inserttime insert_time bigint unsigned not null');
-query('alter table notes change updatetime update_time bigint unsigned not null');
-query('alter table notetags change inserttime insert_time bigint unsigned not null');
-query('alter table notetags change updatetime update_time bigint unsigned not null');
-query('alter table notifications change inserttime insert_time bigint unsigned not null');
-query('alter table tasks change inserttime insert_time bigint unsigned not null');
-query('alter table tasks change updatetime update_time bigint unsigned not null');
-query('alter table tasktags change inserttime insert_time bigint unsigned not null');
-query('alter table tasktags change updatetime update_time bigint unsigned not null');
-query('alter table tokens change inserttime insert_time bigint unsigned not null');
-query('alter table tokens change accesstime access_time bigint unsigned not null');
+query('alter table bookmarktags change idbookmarktags id bigint unsigned auto_increment not null');
+query('alter table contacttags change idcontacttags id bigint unsigned auto_increment not null');
+query('alter table notetags change idnotetags id bigint unsigned auto_increment not null');
+query('alter table tasktags change idtasktags id bigint unsigned auto_increment not null');
 echo 'Done';
