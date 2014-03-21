fc3dc0e23d503a43fa1959ed076580924c3695d7
Archil Imnadze <archil@imnadze.ge>
2014-03-21 07:54:53 +0000

    clear num_new_notifications_for_home when deleting notifications

--- www/fns/Users/clearNumNewNotifications.php
+++ www/fns/Users/clearNumNewNotifications.php
@@ -3,7 +3,8 @@
 namespace Users;
 
 function clearNumNewNotifications ($mysqli, $idusers) {
-    $sql = 'update users set num_new_notifications = 0'
+    $sql = 'update users set num_new_notifications = 0,'
+        .' num_new_notifications_for_home = 0'
         ." where idusers = $idusers";
     $mysqli->query($sql);
 }
--- www/fns/Users/clearNumNotifications.php
+++ www/fns/Users/clearNumNotifications.php
@@ -4,6 +4,8 @@ namespace Users;
 
 function clearNumNotifications ($mysqli, $idusers) {
     $sql = 'update users set num_notifications = 0,'
-        ." num_new_notifications = 0 where idusers = $idusers";
+        .' num_new_notifications = 0,'
+        .' num_new_notifications_for_home = 0'
+        ." where idusers = $idusers";
     $mysqli->query($sql);
 }
