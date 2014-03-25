334d759c1366b05a1ed18decec968f8562cb3595
Archil Imnadze <archil@imnadze.ge>
2014-03-25 09:54:20 +0000

    connection delete page

---
+++ www/account/connections/delete/index.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once '../fns/require_connection.php';
+include_once '../../../lib/mysqli.php';
+list($connection, $id, $user) = require_connection($mysqli);
+
+include_once '../../../fns/create_tabs.php';
+include_once '../../../fns/Page/imageLink.php';
+include_once '../../../fns/Page/text.php';
+$content = create_tabs(
+    [
+        [
+            'title' => '&middot;&middot;&middot;',
+            'href' => '../..',
+        ],
+        [
+            'title' => 'Connections',
+            'href' => '..',
+        ],
+    ],
+    "Connection #$id",
+    Page\text(
+        'Are you sure you want to delete the connection?'
+    )
+    .'<div class="hr"></div>'
+    .Page\imageLink('Yes, delete connection', "submit.php?id=$id", 'yes')
+    .'<div class="hr"></div>'
+    .Page\imageLink('No, return back', "../view/?id=$id", 'no')
+);
+
+include_once '../../../fns/echo_page.php';
+echo_page($user, "Delete Connection #$id?", $content, '../../../');
