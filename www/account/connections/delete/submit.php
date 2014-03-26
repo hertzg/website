caff9246d1d69f1d5261a5285fada62fb328f28e
Archil Imnadze <archil@imnadze.ge>
2014-03-26 14:34:06 +0000

    require same domain referer in account/connections

--- www/account/connections/delete/submit.php
+++ www/account/connections/delete/submit.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once '../../../fns/require_same_domain_referer.php';
+require_same_domain_referer('..');
+
 include_once '../fns/require_connection.php';
 include_once '../../../lib/mysqli.php';
 list($connection, $id, $user) = require_connection($mysqli);
