27b5db87ac46a15b7823f631fb25d130e04dc3d1
Archil Imnadze <archil@imnadze.ge>
2014-03-26 14:47:23 +0000

    trigger_error on mysqli_single_object and mysqli_query_object

--- www/fns/mysqli_query_object.php
+++ www/fns/mysqli_query_object.php
@@ -3,6 +3,7 @@
 function mysqli_query_object ($mysqli, $query) {
     $objects = array();
     $result = $mysqli->query($query);
+    if ($result == false) trigger_error($mysqli->error);
     if ($result) {
         while ($object = $result->fetch_object()) {
             $objects[] = $object;
--- www/fns/mysqli_single_object.php
+++ www/fns/mysqli_single_object.php
@@ -2,6 +2,7 @@
 
 function mysqli_single_object ($mysqli, $sql) {
     $result = $mysqli->query($sql);
+    if ($result == false) trigger_error($mysqli->error);
     if ($result) {
         $object = $result->fetch_object();
         $result->close();
