884d8f325164128487eaedfffe203bc63275a6b4
Archil Imnadze <archil@imnadze.ge>
2014-03-31 11:59:51 +0000

    do not clear reset_password_key on login

--- www/fns/Users/login.php
+++ www/fns/Users/login.php
@@ -5,7 +5,6 @@ namespace Users;
 function login ($mysqli, $id_users) {
     $last_login_time = time();
     $sql = 'update users set num_logins = num_logins + 1,'
-        .' reset_password_key = null, reset_password_key_time = null,'
         ." last_login_time = $last_login_time where id_users = $id_users";
     $mysqli->query($sql);
 }
