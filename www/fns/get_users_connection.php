089cba51c326ef623d006fb456adeac620174be5
Archil Imnadze <archil@imnadze.ge>
2014-04-01 07:55:02 +0000

    return can_send_* from get_users_connections

--- www/fns/get_users_connection.php
+++ www/fns/get_users_connection.php
@@ -8,12 +8,20 @@ function get_users_connection ($mysqli, $user, $connected_id_users) {
 
     if ($connection) {
         return [
+            'can_send_bookmark' => $connection->can_send_bookmark,
             'can_send_channel' => $connection->can_send_channel,
+            'can_send_contact' => $connection->can_send_contact,
+            'can_send_note' => $connection->can_send_note,
+            'can_send_task' => $connection->can_send_task,
         ];
     }
 
     return [
+        'can_send_bookmark' => $user->anonymous_can_send_bookmark,
         'can_send_channel' => $user->anonymous_can_send_channel,
+        'can_send_contact' => $user->anonymous_can_send_contact,
+        'can_send_note' => $user->anonymous_can_send_note,
+        'can_send_task' => $user->anonymous_can_send_task,
     ];
 
 }
