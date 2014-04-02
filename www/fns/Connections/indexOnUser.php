70f8d22c14cc7087e1b8f3897e0cc4982306e956
Archil Imnadze <archil@imnadze.ge>
2014-04-02 10:13:55 +0000

    sort connections by username

--- www/fns/Connections/indexOnUser.php
+++ www/fns/Connections/indexOnUser.php
@@ -3,7 +3,8 @@
 namespace Connections;
 
 function indexOnUser ($mysqli, $id_users) {
-    $sql = "select * from connections where id_users = $id_users";
+    $sql = "select * from connections where id_users = $id_users"
+        .' order by username';
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
