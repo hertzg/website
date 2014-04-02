b187d9eb68ca5dccc77a02e008e718e168f74fd8
Archil Imnadze <archil@imnadze.ge>
2014-04-02 10:15:49 +0000

    sort received items from new to old

--- www/fns/ReceivedBookmarks/indexOnReceiver.php
+++ www/fns/ReceivedBookmarks/indexOnReceiver.php
@@ -4,7 +4,8 @@ namespace ReceivedBookmarks;
 
 function indexOnReceiver ($mysqli, $receiver_id_users) {
     $sql = 'select * from received_bookmarks'
-        ." where receiver_id_users = $receiver_id_users";
+        ." where receiver_id_users = $receiver_id_users"
+        .' order by insert_time desc';
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
--- www/fns/ReceivedContacts/indexOnReceiver.php
+++ www/fns/ReceivedContacts/indexOnReceiver.php
@@ -4,7 +4,8 @@ namespace ReceivedContacts;
 
 function indexOnReceiver ($mysqli, $receiver_id_users) {
     $sql = 'select * from received_contacts'
-        ." where receiver_id_users = $receiver_id_users";
+        ." where receiver_id_users = $receiver_id_users"
+        .' order by insert_time desc';
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
