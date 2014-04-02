4d304b4c6991f9e1cbd9ea12ef23b33d7fad91a1
Archil Imnadze <archil@imnadze.ge>
2014-04-02 11:41:29 +0000

    improved arrows in icons

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -4,7 +4,7 @@ function get_revision ($key) {
     static $revisions = [
         'common.compressed.css' => 23,
         'icons.css' => 23,
-        'images/icons.png' => 26,
+        'images/icons.png' => 27,
     ];
     return $revisions[$key];
 }
