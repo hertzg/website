8323b56a9017f8860c37d85bccedb98371ca2a2c
Archil Imnadze <archil@imnadze.ge>
2014-04-02 16:34:08 +0000

    use twoColumns in home

---
+++ www/home/fns/create_options_panel.php
@@ -0,0 +1,27 @@
+<?php
+
+function create_options_panel () {
+
+    include_once __DIR__.'/../../fns/Page/imageArrowLink.php';
+
+    $accountLink = Page\imageArrowLink('Account', '../account/', 'account');
+
+    $title = 'Customize Home';
+    $href = '../customize-home/';
+    $customizeHomeLink = Page\imageArrowLink($title, $href, 'edit-home');
+
+    $helpLink = Page\imageArrowLink('Help', '../help/', 'help');
+
+    $href = '../submit-signout.php';
+    $signOutLink = Page\imageLink('Sign Out', $href, 'sign-out');
+
+    include_once __DIR__.'/../../fns/Page/twoColumns.php';
+    $content =
+        Page\twoColumns($accountLink, $customizeHomeLink)
+        .'<div class="hr"></div>'
+        .Page\twoColumns($helpLink, $signOutLink);
+
+    include_once __DIR__.'/../../fns/create_panel.php';
+    return create_panel('Options', $content);
+
+}
--- www/home/index.php
+++ www/home/index.php
@@ -24,9 +24,8 @@ $homeItems = get_home_items($mysqli, $user);
 $items = array_merge($items, $homeItems);
 
 include_once 'fns/create_new_notifications.php';
-include_once '../fns/create_panel.php';
+include_once 'fns/create_options_panel.php';
 include_once '../fns/create_tabs.php';
-include_once '../fns/Page/imageArrowLink.php';
 include_once '../fns/Page/sessionMessages.php';
 $content =
     create_tabs(
@@ -36,17 +35,7 @@ $content =
         .create_new_notifications($mysqli, $user)
         .join('<div class="hr"></div>', $items)
     )
-    .create_panel(
-        'Options',
-        Page\imageArrowLink('Account', '../account/', 'account')
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Customize Home',
-            '../customize-home/', 'edit-home')
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Help', '../help/', 'help')
-        .'<div class="hr"></div>'
-        .Page\imageLink('Sign Out', '../submit-signout.php', 'sign-out')
-    );
+    .create_options_panel();
 
 include_once '../fns/echo_page.php';
 echo_page($user, 'Home', $content, $base);
