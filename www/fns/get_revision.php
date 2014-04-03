f66d9c987358be80aa4ce691e1f0d3db2f885c51
Archil Imnadze <archil@imnadze.ge>
2014-04-03 14:13:51 +0000

    remove bold font from form labels

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = [
-        'common.compressed.css' => 24,
+        'common.compressed.css' => 25,
         'icons.css' => 25,
         'images/icons.png' => 30,
     ];
