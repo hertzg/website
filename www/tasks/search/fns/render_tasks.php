ecd368b35997d3fa7b079dad1b699f0261e41f02
Archil Imnadze <archil@imnadze.ge>
2014-04-04 08:27:47 +0000

    separated render_tasks from tasks/search

--- www/tasks/search/fns/render_tasks.php
+++ www/tasks/search/fns/render_tasks.php
@@ -3,25 +3,25 @@
 function render_tasks (array $tasks, array &$items) {
     if ($tasks) {
 
-        include_once __DIR__.'/../../fns/Page/imageArrowLink.php';
-        include_once __DIR__.'/../../fns/Page/imageArrowLinkWithDescription.php';
+        include_once __DIR__.'/../../../fns/Page/imageArrowLink.php';
+        include_once __DIR__.'/../../../fns/Page/imageArrowLinkWithDescription.php';
 
         foreach ($tasks as $task) {
             $icon = $task->top_priority ? 'task-top-priority' : 'task';
             $title = htmlspecialchars($task->text);
-            $href = "view/?id=$task->id_tasks";
+            $href = "../view/?id=$task->id_tasks";
             $tags = $task->tags;
-            if ($tags) {
+            if ($tags === '') {
+                $items[] = Page\imageArrowLink($title, $href, $icon);
+            } else {
                 $description = 'Tags: '.htmlspecialchars($tags);
                 $items[] = Page\imageArrowLinkWithDescription($title,
                     $description, $href, $icon);
-            } else {
-                $items[] = Page\imageArrowLink($title, $href, $icon);
             }
         }
 
     } else {
-        include_once __DIR__.'/../../fns/Page/info.php';
-        $items[] = Page\info('No tasks');
+        include_once __DIR__.'/../../../fns/Page/info.php';
+        $items[] = Page\info('No tasks found');
     }
 }
