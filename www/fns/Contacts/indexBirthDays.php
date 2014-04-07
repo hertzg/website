829c11edc6f50b6f026d5a57bb94c502617ba40e
Archil Imnadze <archil@imnadze.ge>
2014-04-07 13:54:06 +0000

    count birthdays in events

--- www/fns/Contacts/indexBirthDays.php
+++ www/fns/Contacts/indexBirthDays.php
@@ -2,9 +2,9 @@
 
 namespace Contacts;
 
-function indexBirthDays ($mysqli, $day, $month) {
-    $sql = 'select * from contacts'
-        ." where birth_day = $day and birth_month = $month";
+function indexBirthDays ($mysqli, $id_users, $day, $month) {
+    $sql = "select * from contacts where id_users = $id_users"
+        ." and birth_day = $day and birth_month = $month";
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
