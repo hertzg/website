e4821ced29aa901871c8d05ce30abde93041b2d8
Archil Imnadze <archil@imnadze.ge>
2014-04-07 10:20:33 +0000

    form-select ui-invalid css class

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = [
-        'common.compressed.css' => 26,
+        'common.compressed.css' => 27,
         'icons.css' => 25,
         'images/icons.png' => 30,
     ];
