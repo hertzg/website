82fb420e3e2ae38a8d5b39c6266796e2f968e1c1
Archil Imnadze <archil@imnadze.ge>
2014-04-08 12:43:46 +0000

    extend icons image size

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -4,7 +4,7 @@ function get_revision ($key) {
     static $revisions = [
         'common.compressed.css' => 27,
         'icons.css' => 28,
-        'images/icons.png' => 33,
+        'images/icons.png' => 34,
     ];
     return $revisions[$key];
 }
