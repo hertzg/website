74695731dc4f2dfe67381f2a7cf0b2717a9a741a
Archil Imnadze <archil@imnadze.ge>
2014-04-08 10:28:17 +0000

    removed update script

--- www/scripts/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-chdir(__DIR__);
-include_once '../lib/mysqli.php';
-
-$mysqli->query('alter table received_contacts add birthday_time bigint unsigned default null after phone2') || trigger_error($mysqli->error);
-echo "Done\n";
