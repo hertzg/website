ba3f555e28cf82c65642037567019986867eaaaa
Archil Imnadze <archil@imnadze.ge>
2014-04-08 09:48:22 +0000

    script to add birthday_time to received_contacts

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -3,10 +3,5 @@
 chdir(__DIR__);
 include_once '../lib/mysqli.php';
 
-$mysqli->query('alter table contacts add birthday_time bigint unsigned default null after phone2') || trigger_error($mysqli->error);
-$mysqli->query('alter table contacts add birthday_day bigint unsigned default null after birthday_time') || trigger_error($mysqli->error);
-$mysqli->query('alter table contacts add birthday_month bigint unsigned default null after birthday_day') || trigger_error($mysqli->error);
-$mysqli->query('alter table users add num_birthdays_today bigint unsigned not null after anonymous_can_send_task') || trigger_error($mysqli->error);
-$mysqli->query('alter table users add num_birthdays_tomorrow bigint unsigned not null after num_birthdays_today') || trigger_error($mysqli->error);
-$mysqli->query('alter table users add birthdays_check_day bigint unsigned not null after num_birthdays_tomorrow') || trigger_error($mysqli->error);
+$mysqli->query('alter table received_contacts add birthday_time bigint unsigned default null after phone2') || trigger_error($mysqli->error);
 echo "Done\n";
