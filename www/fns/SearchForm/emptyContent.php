6b25243588c60c3157d683e9b02934f033dd83ef
Archil Imnadze <archil@imnadze.ge>
2014-04-08 10:22:22 +0000

    fixed: required search keyword on non-search pages

--- www/fns/SearchForm/emptyContent.php
+++ www/fns/SearchForm/emptyContent.php
@@ -6,7 +6,7 @@ function emptyContent ($placeholder) {
     return
         '<div class="search_form-content empty">'
             .'<input class="form-textfield" type="text" name="keyword"'
-            ." placeholder=\"$placeholder\" />"
+            ." required=\"required\" placeholder=\"$placeholder\" />"
         .'</div>'
         .'<button class="searchButton clickable" title="Search">'
             .'<span class="icon search"></span>'
