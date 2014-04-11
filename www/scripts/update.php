78ad41033ef587f315c3bcd5a0c648f06896edac
Archil Imnadze <archil@imnadze.ge>
2014-04-11 11:33:09 +0000

    script to add can_send_file

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -3,5 +3,6 @@
 chdir(__DIR__);
 include_once '../lib/mysqli.php';
 
-$mysqli->query('alter table channels add receive_notifications tinyint unsigned default 1 not null') || trigger_error($mysqli->error);
+$mysqli->query('alter table connections add can_send_file tinyint unsigned not null after can_send_contact') || trigger_error($mysqli->error);
+$mysqli->query('alter table users add anonymous_can_send_file tinyint unsigned not null after anonymous_can_send_contact') || trigger_error($mysqli->error);
 echo 'Done';
