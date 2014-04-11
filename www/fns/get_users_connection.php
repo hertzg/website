b06d86609b8918276df282a6f434b7be991dc0e1
Archil Imnadze <archil@imnadze.ge>
2014-04-11 11:36:42 +0000

    can_send_file in get_users_connection

--- www/fns/get_users_connection.php
+++ www/fns/get_users_connection.php
@@ -11,6 +11,7 @@ function get_users_connection ($mysqli, $user, $connected_id_users) {
             'can_send_bookmark' => $connection->can_send_bookmark,
             'can_send_channel' => $connection->can_send_channel,
             'can_send_contact' => $connection->can_send_contact,
+            'can_send_file' => $connection->can_send_file,
             'can_send_note' => $connection->can_send_note,
             'can_send_task' => $connection->can_send_task,
         ];
@@ -20,6 +21,7 @@ function get_users_connection ($mysqli, $user, $connected_id_users) {
         'can_send_bookmark' => $user->anonymous_can_send_bookmark,
         'can_send_channel' => $user->anonymous_can_send_channel,
         'can_send_contact' => $user->anonymous_can_send_contact,
+        'can_send_file' => $user->anonymous_can_send_file,
         'can_send_note' => $user->anonymous_can_send_note,
         'can_send_task' => $user->anonymous_can_send_task,
     ];
