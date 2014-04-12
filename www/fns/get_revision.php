9a7997521d6c001094c092586a0a1001e05b6e80
Archil Imnadze <archil@imnadze.ge>
2014-04-12 09:20:57 +0000

    import-file icon

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -3,8 +3,8 @@
 function get_revision ($key) {
     static $revisions = [
         'common.compressed.css' => 27,
-        'icons.css' => 28,
-        'images/icons.png' => 34,
+        'icons.css' => 29,
+        'images/icons.png' => 35,
     ];
     return $revisions[$key];
 }
