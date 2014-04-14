1f5555764d1d2c56b565ff659b19f50395cdfdfe
Archil Imnadze <archil@imnadze.ge>
2014-04-14 09:12:17 +0000

    fixed misplaced checked checkbox icon

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -4,7 +4,7 @@ function get_revision ($key) {
     static $revisions = [
         'common.compressed.css' => 28,
         'icons.css' => 29,
-        'images/icons.png' => 35,
+        'images/icons.png' => 36,
     ];
     return $revisions[$key];
 }
