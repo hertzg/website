66d6c0529c89df4c67c6e928f05132477bf03b6a
Archil Imnadze <archil@imnadze.ge>
2014-04-14 13:47:59 +0000

    script to recreate user folders

---
+++ www/scripts/create-user-folders.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+
+chdir(__DIR__);
+include_once '../lib/mysqli.php';
+
+include_once '../fns/mysqli_query_object.php';
+$users = mysqli_query_object($mysqli, 'select * from users');
+
+$mkdir = function ($dirname) {
+    if (!is_dir($dirname)) mkdir($dirname);
+    chmod($dirname, 0770);
+};
+
+foreach ($users as $user) {
+    $userDir = "../users/$user->id_users";
+    $mkdir($userDir);
+    $mkdir("$userDir/files");
+    $mkdir("$userDir/received-files");
+}
