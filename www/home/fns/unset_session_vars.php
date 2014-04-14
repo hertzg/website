0725bacd54f33fe721d0bfd66c52e43b2dc3f8db
Archil Imnadze <archil@imnadze.ge>
2014-04-14 14:13:48 +0000

    unset errors

--- www/home/fns/unset_session_vars.php
+++ www/home/fns/unset_session_vars.php
@@ -3,16 +3,20 @@
 function unset_session_vars () {
     unset(
         $_SESSION['account/messages'],
+        $_SESSION['bookmarks/errors'],
         $_SESSION['bookmarks/messages'],
         $_SESSION['calendar/messages'],
+        $_SESSION['contacts/errors'],
         $_SESSION['contacts/messages'],
         $_SESSION['files/messages'],
         $_SESSION['help/messages'],
         $_SESSION['home/customize/messages'],
+        $_SESSION['notes/errors'],
         $_SESSION['notes/messages'],
         $_SESSION['notifications/in-channel/messages'],
         $_SESSION['notifications/in-subscribed-channel/messages'],
         $_SESSION['notifications/messages'],
+        $_SESSION['tasks/errors'],
         $_SESSION['tasks/messages']
     );
 }
