e826710fbc6323d0eba18ef97e8ecf4d67ed61e4
Archil Imnadze <archil@imnadze.ge>
2014-04-14 13:47:36 +0000

    delete files from filesystem after deleting all received files

--- www/fns/ReceivedFiles/deleteOnReceiver.php
+++ www/fns/ReceivedFiles/deleteOnReceiver.php
@@ -3,7 +3,23 @@
 namespace ReceivedFiles;
 
 function deleteOnReceiver ($mysqli, $receiver_id_users) {
+
+    $sql = 'select * from received_files'
+        ." where receiver_id_users = $receiver_id_users";
+
+    include_once __DIR__.'/../mysqli_query_object.php';
+    $receivedFiles = mysqli_query_object($mysqli, $sql);
+
+    if ($receivedFiles) {
+        include_once __DIR__.'/filePath.php';
+        foreach ($receivedFiles as $receivedFile) {
+            $filePath = filePath($receiver_id_users, $receivedFile->id);
+            if (is_file($filePath)) unlink($filePath);
+        }
+    }
+
     $sql = 'delete from received_files'
         ." where receiver_id_users = $receiver_id_users";
     $mysqli->query($sql) || trigger_error($mysqli->error);
+
 }
