98ea14f9d837736a09808bacd7ca3f2866e7eec8
Archil Imnadze <archil@imnadze.ge>
2014-04-15 09:52:38 +0000

    use twoColumns on home

--- www/home/index.php
+++ www/home/index.php
@@ -23,6 +23,16 @@ $homeItems = get_home_items($mysqli, $user);
 
 $items = array_merge($items, $homeItems);
 
+$pairs = array_chunk($items, 2);
+include_once '../fns/Page/twoColumns.php';
+foreach ($items as &$item) {
+    if (count($item) == 2) {
+        $item = Page\twoColumns($item[0], $item[1]);
+    } else {
+        $item = $item[0];
+    }
+}
+
 include_once 'fns/create_new_notifications.php';
 include_once 'fns/create_options_panel.php';
 include_once '../fns/create_tabs.php';
