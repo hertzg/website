e47c7767c04439d56d867129d8ce045b8a369440
Archil Imnadze <archil@imnadze.ge>
2014-04-15 09:10:20 +0000

    fixed: number of subscribed channels not affected after closing account

--- www/fns/SubscribedChannels/deleteContainingUser.php
+++ www/fns/SubscribedChannels/deleteContainingUser.php
@@ -3,8 +3,21 @@
 namespace SubscribedChannels;
 
 function deleteContainingUser ($mysqli, $id_users) {
+
     $sql = 'delete from subscribed_channels'
-        ." where id_users = $id_users"
-        ." or subscribed_id_users = $id_users";
+        ." where subscribed_id_users = $id_users";
     $mysqli->query($sql) || trigger_error($mysqli->error);
+
+    $sql = "select * from subscribed_channels where id_users = $id_users";
+    include_once __DIR__.'/../mysqli_query_object.php';
+    $subscribedChannels = mysqli_query_object($mysqli, $sql);
+    if ($subscribedChannels) {
+        include_once __DIR__.'/delete.php';
+        include_once __DIR__.'/../Users/addNumSubscribedChannels.php';
+        foreach ($subscribedChannels as $subscribedChannel) {
+            delete($mysqli, $subscribedChannel->id);
+            \Users\addNumSubscribedChannels($mysqli,
+                $subscribedChannel->subscribed_id_users, -1);
+        }
+    }
 }
