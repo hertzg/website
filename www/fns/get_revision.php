4af3211135ad29fd34f806de952d0d7b6bd24d81
Archil Imnadze <archil@imnadze.ge>
2014-04-16 09:46:59 +0000

    locked icons

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -3,8 +3,8 @@
 function get_revision ($key) {
     static $revisions = [
         'common.compressed.css' => 29,
-        'icons.css' => 30,
-        'images/icons.svg' => 37,
+        'icons.css' => 31,
+        'images/icons.svg' => 38,
     ];
     return $revisions[$key];
 }
