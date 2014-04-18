7829f30ccf378adcfc5e13799cad0668068862e4
Archil Imnadze <archil@imnadze.ge>
2014-04-18 19:37:23 +0000

    removed update script

--- www/scripts/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-chdir(__DIR__);
-include_once '../lib/mysqli.php';
-
-$mysqli->query('alter table subscribed_channels change public_subscriber subscriber_locked tinyint not null after subscriber_username');
-$mysqli->query('alter table subscribed_channels add publisher_locked tinyint not null after publisher_username');
-$mysqli->query('update subscribed_channels set publisher_locked = 1 where subscriber_locked = 0');
