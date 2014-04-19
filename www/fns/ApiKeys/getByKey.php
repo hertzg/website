0c81ba6c9fb4600a68a9df4fda607259f2e1b119
Archil Imnadze <archil@imnadze.ge>
2014-04-19 13:55:11 +0000

    ApiKeys\getByKey

---
+++ www/fns/ApiKeys/getByKey.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ApiKeys;
+
+function getByKey ($mysqli, $key) {
+    $key = $mysqli->real_escape_string($key);
+    $sql = "select * from api_keys where `key` = '$key'";
+    include_once __DIR__.'/../mysqli_single_object.php';
+    return mysqli_single_object($mysqli, $sql);
+}
