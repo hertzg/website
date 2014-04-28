5fcc5686f258ba80a8c3b40d5afd4fffb14ab4c3
Archil Imnadze <archil@imnadze.ge>
2014-04-28 13:08:50 +0000

    bold code tag

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = [
-        'common.compressed.css' => 35,
+        'common.compressed.css' => 36,
         'icons.css' => 35,
         'images/icons.svg' => 42,
     ];
