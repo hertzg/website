6a72c8cf85738d9622076b74e1cf3881ba705dbc
Archil Imnadze <archil@imnadze.ge>
2014-04-28 15:18:03 +0000

    removed extra empty line

--- www/fns/Notes/request.php
+++ www/fns/Notes/request.php
@@ -9,7 +9,6 @@ function request () {
 
     include_once __DIR__.'/../str_collapse_spaces_multiline.php';
     $text = str_collapse_spaces_multiline($text);
-
     $text = trim($text);
 
     return [$text];
