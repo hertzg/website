45cf37dfd11f84be12108c84c407566bdd9959f4
Archil Imnadze <archil@imnadze.ge>
2014-04-29 07:53:58 +0000

    removed update script

--- www/scripts/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-chdir(__DIR__);
-include_once '../lib/mysqli.php';
-
-$mysqli->query('alter table channels add update_time bigint unsigned not null');
-$mysqli->query('update channels set update_time = insert_time');
