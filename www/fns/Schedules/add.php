00e6997b2003d220f30f040e007d33918bae13ea
Archil Imnadze <archil@imnadze.ge>
2014-04-30 09:38:55 +0000

    save time_offset

--- www/fns/Schedules/add.php
+++ www/fns/Schedules/add.php
@@ -4,8 +4,11 @@ namespace Schedules;
 
 function add ($mysqli, $id_users, $text, $time_interval, $time_offset) {
     $text = $mysqli->real_escape_string($text);
-    $sql = 'insert into schedules (id_users, text, time_interval, time_offset)'
-        ." values ($id_users, '$text', $time_interval, $time_offset)";
+    $insert_time = $update_time = time();
+    $sql = 'insert into schedules (id_users, text, time_interval,'
+        .' time_offset, insert_time, update_time)'
+        ." values ($id_users, '$text', $time_interval,"
+        ." $time_offset, $insert_time, $update_time)";
     $mysqli->query($sql) || trigger_error($mysqli->error);
     return $mysqli->insert_id;
 }
--- www/fns/Schedules/edit.php
+++ www/fns/Schedules/edit.php
@@ -4,8 +4,9 @@ namespace Schedules;
 
 function edit ($mysqli, $id, $text, $time_interval, $time_offset) {
     $text = $mysqli->real_escape_string($text);
+    $update_time = time();
     $sql = "update schedules set text = '$text',"
-        ." time_interval = $time_interval,"
-        ." time_offset = $time_offset where id = $id";
+        ." time_interval = $time_interval, time_offset = $time_offset,"
+        ." update_time = $update_time where id = $id";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 }
