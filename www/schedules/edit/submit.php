8d657391f639d4c30533dbf79655ac225c379de4
Archil Imnadze <archil@imnadze.ge>
2014-05-01 09:28:15 +0000

    edit schedule show next day

--- www/schedules/edit/submit.php
+++ www/schedules/edit/submit.php
@@ -28,10 +28,17 @@ if ($errors) {
 
 unset($_SESSION['schedules/edit/errors']);
 
+include_once '../../fns/time_today.php';
+$dayNow = time_today() / (60 * 60 * 25);
+$remainder = ($dayNow - $schedule->day_offset) % $day_interval;
+if ($remainder) $day_offset = $day_interval - $remainder;
+else $day_offset = 0;
+
 $_SESSION['schedules/edit/next/first_stage'] = [
     'id' => $id,
     'text' => $text,
     'day_interval' => $day_interval,
+    'day_offset' => $day_offset,
 ];
 
 redirect('next/');
