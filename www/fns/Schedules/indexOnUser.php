30f2912836cc441319e0c63a28c3655f82ecd140
Archil Imnadze <archil@imnadze.ge>
2014-05-02 08:52:26 +0000

    removed unused order by clause

--- www/fns/Schedules/indexOnUser.php
+++ www/fns/Schedules/indexOnUser.php
@@ -3,8 +3,7 @@
 namespace Schedules;
 
 function indexOnUser ($mysqli, $id_users) {
-    $sql = "select * from schedules where id_users = $id_users"
-        .' order by update_time';
+    $sql = "select * from schedules where id_users = $id_users";
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
