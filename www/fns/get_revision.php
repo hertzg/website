dbac4e7d5074489ae2a81a5cf477beba355a405b
Archil Imnadze <archil@imnadze.ge>
2014-05-03 19:01:31 +0000

    first variant of split icons

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -3,8 +3,8 @@
 function get_revision ($key) {
     static $revisions = [
         'common.compressed.css' => 38,
-        'icons.compressed.css' => 37,
-        'images/icons.svg' => 45,
+        'icons.compressed.css' => 38,
+        'images/icons.svg' => 46,
     ];
     return $revisions[$key];
 }
--- www/scripts/generate-icons-css.php
+++ www/scripts/generate-icons-css.php
@@ -21,8 +21,8 @@ $names = ['blue-theme', 'green-theme', 'orange-theme', 'pink-theme',
     'copy-file', 'move-file', 'copy-folder', 'move-folder', 'import-bookmark',
     'import-contact', 'import-file', 'import-note', 'import-task', 'search',
     'search-folder', 'birthday-cake', 'events', 'event', 'create-event',
-    'edit-event', 'checkbox', 'checked-checkbox', 'help', 'token',
-    'create-token', 'tokens', 'run', 'mail', 'send', 'receive', 'phone',
+    'edit-event', 'checkbox', 'checked-checkbox', 'help',
+    'run', 'mail', 'send', 'receive', 'phone',
     'edit-home', 'move-up', 'move-to-top', 'move-down', 'move-to-bottom',
     'reorder', 'show-hide', 'restore-defaults', 'connection',
     'create-connection', 'edit-connection', 'connections',
@@ -37,4 +37,17 @@ foreach ($names as $name) {
     $content .= ".icon.$name{background-position:{$x}px 0}";
     $x -= 32;
 }
+
+$names = ['token', 'create-token', 'tokens'];
+$content .=
+    join(',', array_map(function ($name) {
+        return ".icon.$name";
+    }, $names))
+    .'{background-image:url(images/token-icons.svg)}';
+$x = 0;
+foreach ($names as $name) {
+    $content .= ".icon.$name{background-position:${x}px 0}";
+    $x -= 32;
+}
+
 file_put_contents(__DIR__.'/../icons.compressed.css', $content);
