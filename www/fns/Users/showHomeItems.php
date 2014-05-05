0d6e17d38e964219ec406c169789407ed6a6a482
Archil Imnadze <archil@imnadze.ge>
2014-05-05 09:13:53 +0000

    fixed: customize home doesn't hide schedules

--- www/fns/Users/showHomeItems.php
+++ www/fns/Users/showHomeItems.php
@@ -4,7 +4,7 @@ namespace Users;
 
 function showHomeItems ($mysqli, $id_users, $bookmarks, $new_bookmark,
     $calendar, $contacts, $new_contact, $files, $notes, $new_note,
-    $notifications, $tasks, $new_task) {
+    $notifications, $schedules, $tasks, $new_task) {
 
     $bookmarks = $bookmarks ? '1' : '0';
     $new_bookmark = $new_bookmark ? '1' : '0';
@@ -15,6 +15,7 @@ function showHomeItems ($mysqli, $id_users, $bookmarks, $new_bookmark,
     $notes = $notes ? '1' : '0';
     $new_note = $new_note ? '1' : '0';
     $notifications = $notifications ? '1' : '0';
+    $schedules = $schedules ? '1' : '0';
     $tasks = $tasks ? '1' : '0';
     $new_task = $new_task ? '1' : '0';
 
@@ -22,8 +23,9 @@ function showHomeItems ($mysqli, $id_users, $bookmarks, $new_bookmark,
         ." show_new_bookmark = $new_bookmark, show_calendar = $calendar,"
         ." show_contacts = $contacts, show_new_contact = $new_contact,"
         ." show_files = $files, show_notes = $notes, show_new_note = $new_note,"
-        ." show_notifications = $notifications, show_tasks = $tasks,"
-        ." show_new_task = $new_task where id_users = $id_users";
+        ." show_notifications = $notifications, show_schedules = $schedules,"
+        ." show_tasks = $tasks, show_new_task = $new_task"
+        ." where id_users = $id_users";
 
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
--- www/home/customize/show-hide/submit.php
+++ www/home/customize/show-hide/submit.php
@@ -8,9 +8,9 @@ $user = require_user('../../../');
 
 include_once '../../../fns/request_strings.php';
 list($bookmarks, $new_bookmark, $calendar, $contacts, $new_contact, $files,
-    $notes, $new_note, $notifications, $tasks, $new_task) = request_strings(
+    $notes, $new_note, $notifications, $schedules, $tasks, $new_task) = request_strings(
     'bookmarks', 'new_bookmark', 'calendar', 'contacts', 'new_contact', 'files',
-    'notes', 'new_note', 'notifications', 'tasks', 'new_task');
+    'notes', 'new_note', 'notifications', 'schedules', 'tasks', 'new_task');
 
 $bookmarks = (bool)$bookmarks;
 $new_bookmark = (bool)$new_bookmark;
@@ -21,6 +21,7 @@ $files = (bool)$files;
 $notes = (bool)$notes;
 $new_note = (bool)$new_note;
 $notifications = (bool)$notifications;
+$schedules = (bool)$schedules;
 $tasks = (bool)$tasks;
 $new_task = (bool)$new_task;
 
@@ -28,7 +29,7 @@ include_once '../../../fns/Users/showHomeItems.php';
 include_once '../../../lib/mysqli.php';
 Users\showHomeItems($mysqli, $user->id_users, $bookmarks, $new_bookmark,
     $calendar, $contacts, $new_contact, $files, $notes, $new_note,
-    $notifications, $tasks, $new_task);
+    $notifications, $schedules, $tasks, $new_task);
 
 $_SESSION['home/customize/show-hide/messages'] = ['Changes have been saved.'];
 
