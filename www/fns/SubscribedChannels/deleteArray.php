20245814b7bf32accc51dbb624a1d08d6b2686a7
Archil Imnadze <archil@imnadze.ge>
2014-05-06 14:38:20 +0000

    delete notifications

--- www/fns/SubscribedChannels/deleteArray.php
+++ www/fns/SubscribedChannels/deleteArray.php
@@ -5,12 +5,14 @@ namespace SubscribedChannels;
 function deleteArray ($mysqli, array $subscribedChannels) {
     if ($subscribedChannels) {
         include_once __DIR__.'/delete.php';
+        include_once __DIR__.'/../Notifications/deleteOnSubscribedChannel.php';
         include_once __DIR__.'/../Users/Notifications/addNumber.php';
         include_once __DIR__.'/../Users/addNumSubscribedChannels.php';
         foreach ($subscribedChannels as $subscribedChannel) {
             delete($mysqli, $subscribedChannel->id);
             $id_users = $subscribedChannel->subscriber_id_users;
             \Users\addNumSubscribedChannels($mysqli, $id_users, -1);
+            \Notifications\deleteOnSubscribedChannel($mysqli, $subscribedChannel->id);
             \Users\Notifications\addNumber($mysqli, $id_users,
                 -$subscribedChannel->num_notifications);
         }
