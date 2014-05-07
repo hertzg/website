a26e73c0ee2514a3a45c651f57e40a984d72058d
Archil Imnadze <archil@imnadze.ge>
2014-05-07 08:13:56 +0000

    channel api update

--- www/help/api-doc/channel/add/index.php
+++ www/help/api-doc/channel/add/index.php
@@ -10,6 +10,10 @@ channel_method_page('add', [
         'name' => 'public',
         'description' => 'Whether the channel should be marked as public.',
     ],
+    [
+        'name' => 'receive_notifications',
+        'description' => 'Whether the channel owner should receive notifications.',
+    ],
 ], [
     'ENTER_CHANNEL_NAME', 'INVALID_CHANNEL_NAME', 'CHANNEL_NAME_TOO_SHORT',
     'CHANNEL_NAME_TOO_LONG', 'CHANNEL_ALREADY_EXISTS',
--- www/help/api-doc/channel/edit/index.php
+++ www/help/api-doc/channel/edit/index.php
@@ -14,6 +14,10 @@ channel_method_page('edit', [
         'name' => 'public',
         'description' => 'Whether the channel should be marked as public.',
     ],
+    [
+        'name' => 'receive_notifications',
+        'description' => 'Whether the channel owner should receive notifications.',
+    ],
 ], [
     'CHANNEL_NOT_FOUND', 'ENTER_CHANNEL_NAME', 'INVALID_CHANNEL_NAME',
     'CHANNEL_NAME_TOO_SHORT', 'CHANNEL_NAME_TOO_LONG', 'CHANNEL_ALREADY_EXISTS',
