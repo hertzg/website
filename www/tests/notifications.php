23976d8aaef640972e3c9e98a64077296df5712b
Archil Imnadze <archil@imnadze.ge>
2014-05-07 08:31:55 +0000

    notifications deleteAll api method

--- www/tests/notifications.php
+++ www/tests/notifications.php
@@ -8,11 +8,33 @@ $channel_name = 'test-channel-name';
 
 $response = $engine->request('channel/add', [
     'channel_name' => $channel_name,
+    'receive_notifications' => true,
 ]);
+$engine->expectNatural('', $response);
 
-$engine->request('channel/delete', [
-    'id' => $response,
+$id = $response;
+
+$response = $engine->request('notification/post');
+$engine->expectError('CHANNEL_NOT_FOUND');
+
+$response = $engine->request('notification/post', [
+    'channel_name' => $channel_name,
 ]);
+$engine->expectError('ENTER_NOTIFICATION_TEXT');
+
+$response = $engine->request('notification/post', [
+    'channel_name' => $channel_name,
+    'notification text' => 'notification text',
+]);
+$engine->expectValue('', true, $response);
+
+$response = $engine->request('notification/deleteAll');
+$engine->expectValue('', true, $response);
+
+$response = $engine->request('channel/delete', [
+    'id' => $id,
+]);
+$engine->expectValue('', true, $response);
 
 echo "Done\n";
 echo "$engine->numRequests requests made.\n";
