cb041aa49393472141de5c386ab7bd495268ef25
Archil Imnadze <archil@imnadze.ge>
2014-05-07 07:46:11 +0000

    optimized post

--- www/fns/Users/Notifications/post.php
+++ www/fns/Users/Notifications/post.php
@@ -14,11 +14,11 @@ function post ($mysqli, $channel, $notification_text) {
         \Notifications\add($mysqli, $id_users, $id_channels,
             $channel_name, $notification_text);
 
-        include_once __DIR__.'/addNumberNew.php';
-        addNumberNew($mysqli, $id_users, 1);
-
         include_once __DIR__.'/../../Channels/addNumNotifications.php';
         \Channels\addNumNotifications($mysqli, $id_channels, 1);
+
+        include_once __DIR__.'/addNumberNew.php';
+        addNumberNew($mysqli, $id_users, 1);
 
     }
 
@@ -26,19 +26,21 @@ function post ($mysqli, $channel, $notification_text) {
     $subscribedChannels = \SubscribedChannels\indexOnChannel(
         $mysqli, $id_channels);
 
+    $subscribedChannels = array_filter($subscribedChannels, function ($subscribedChannel) {
+        return $subscribedChannel->receive_notifications;
+    });
+
     if ($subscribedChannels) {
         include_once __DIR__.'/../../Notifications/addExternal.php';
         include_once __DIR__.'/../../SubscribedChannels/addNumNotifications.php';
         include_once __DIR__.'/addNumberNew.php';
         foreach ($subscribedChannels as $subscribedChannel) {
-            if ($subscribedChannel->receive_notifications) {
-                $id = $subscribedChannel->id;
-                $id_users = $subscribedChannel->subscriber_id_users;
-                \Notifications\addExternal($mysqli, $id_users,
-                    $id_channels, $channel_name, $notification_text, $id);
-                addNumberNew($mysqli, $id_users, 1);
-                \SubscribedChannels\addNumNotifications($mysqli, $id, 1);
-            }
+            $id = $subscribedChannel->id;
+            $id_users = $subscribedChannel->subscriber_id_users;
+            \Notifications\addExternal($mysqli, $id_users,
+                $id_channels, $channel_name, $notification_text, $id);
+            \SubscribedChannels\addNumNotifications($mysqli, $id, 1);
+            addNumberNew($mysqli, $id_users, 1);
         }
     }
 
