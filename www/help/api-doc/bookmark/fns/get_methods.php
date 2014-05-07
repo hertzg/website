bb6bd6f947a78b05b5f91d6a8041fdceb65da570
Archil Imnadze <archil@imnadze.ge>
2014-05-07 08:46:01 +0000

    bookmarks/deletAll documentation

--- www/help/api-doc/bookmark/fns/get_methods.php
+++ www/help/api-doc/bookmark/fns/get_methods.php
@@ -4,6 +4,7 @@ function get_methods () {
     return [
         'add' => 'Creates a new bookmark.',
         'delete' => 'Deletes an existing bookmark.',
+        'deleteAll' => 'Deletes all bookmarks.',
         'edit' => 'Edits an existing bookmark.',
         'get' => 'Returns a single existing bookmark.',
         'list' => 'Returns a list of all bookmarks.',
