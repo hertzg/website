d0d3ee896f88ad5927e5a54ae064b21f8ca34fd7
Archil Imnadze <archil@imnadze.ge>
2014-05-08 13:14:00 +0000

    save insert_time and update_time in contact_tags

--- www/fns/ContactTags/add.php
+++ www/fns/ContactTags/add.php
@@ -8,14 +8,15 @@ function add ($mysqli, $id_users, $id_contacts, array $tag_names,
     $full_name = $mysqli->real_escape_string($full_name);
     $alias = $mysqli->real_escape_string($alias);
     $favorite = $favorite ? '1' : '0';
+    $insert_time = $update_time = time();
 
     foreach ($tag_names as $tag_name) {
         $tag_name = $mysqli->real_escape_string($tag_name);
         $sql = 'insert into contact_tags'
-            .' (id_users, id_contacts, tag_name,'
-            .' full_name, alias, favorite)'
-            ." values ($id_users, $id_contacts, '$tag_name',"
-            ." '$full_name', '$alias', $favorite)";
+            .' (id_users, id_contacts, tag_name, full_name,'
+            .' alias, favorite, insert_time, update_time)'
+            ." values ($id_users, $id_contacts, '$tag_name', '$full_name',"
+            ." '$alias', $favorite, $insert_time, $update_time)";
         $mysqli->query($sql) || trigger_error($mysqli->error);
     }
 
