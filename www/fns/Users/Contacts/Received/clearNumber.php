1b64dfb4763d250f7bacf81701b8a9c0b77ce1bc
Archil Imnadze <archil@imnadze.ge>
2014-05-08 06:43:34 -0400

    remove clearNumber.php

--- www/fns/Users/Contacts/Received/clearNumber.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Users\Contacts\Received;
-
-function clearNumber ($mysqli, $id_users) {
-    $sql = 'update users set num_received_contacts = 0'
-        ." where id_users = $id_users";
-    $mysqli->query($sql) || trigger_error($mysqli->error);
-}
--- www/fns/Users/Contacts/Received/deleteAll.php
+++ www/fns/Users/Contacts/Received/deleteAll.php
@@ -2,13 +2,13 @@
 
 namespace Users\Contacts\Received;
 
-function deleteAll ($mysqli, $receiver_id_users) {
+function deleteAll ($mysqli, $id_users) {
 
     include_once __DIR__.'/../../../ReceivedContacts/deleteOnReceiver.php';
-    \ReceivedContacts\deleteOnReceiver($mysqli, $receiver_id_users);
+    \ReceivedContacts\deleteOnReceiver($mysqli, $id_users);
 
     $sql = 'update users set num_received_contacts = 0'
-        ." where id_users = $receiver_id_users";
+        ." where id_users = $id_users";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
 }
