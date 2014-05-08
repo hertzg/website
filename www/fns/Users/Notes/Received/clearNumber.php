c2066a692feeb51b3af501aec0c7ef0e763dff00
Archil Imnadze <archil@imnadze.ge>
2014-05-08 12:45:40 +0000

    removed unused clearNumber.php

--- www/fns/Users/Notes/Received/clearNumber.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Users\Notes\Received;
-
-function clearNumber ($mysqli, $id_users) {
-    $sql = 'update users set num_received_notes = 0'
-        ." where id_users = $id_users";
-    $mysqli->query($sql) || trigger_error($mysqli->error);
-}
