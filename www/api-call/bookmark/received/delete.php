985656223806cf5e22dbb9ee06abd4e12e0ebe29
Archil Imnadze <archil@imnadze.ge>
2014-05-12 15:00:40 -0400

    bookmarks/received/delete method

--- www/api-call/bookmark/received/delete.php
+++ www/api-call/bookmark/received/delete.php
@@ -2,3 +2,12 @@
 
 include_once '../../fns/require_api_key.php';
 list($apiKey, $user, $mysqli) = require_api_key();
+
+include_once 'fns/require_received_bookmark.php';
+$receivedBookmark = require_received_bookmark($mysqli, $user->id_users);
+
+include_once '../../../fns/Users/Bookmarks/Received/delete.php';
+Users\Bookmarks\Received\delete($mysqli, $receivedBookmark);
+
+header('Content-Type: application/json');
+echo 'true';
---
+++ www/api-call/bookmark/received/fns/require_received_bookmark.php
@@ -0,0 +1,20 @@
+<?php
+
+function require_received_bookmark ($mysqli, $id_users) {
+
+    include_once __DIR__.'/../../../../fns/request_strings.php';
+    list($id) = request_strings('id');
+
+    $id = abs((int)$id);
+
+    include_once __DIR__.'/../../../../fns/ReceivedBookmarks/getOnReceiver.php';
+    $receivedBookmark = ReceivedBookmarks\getOnReceiver($mysqli, $id_users, $id);
+
+    if (!$receivedBookmark) {
+        include_once __DIR__.'/../../../fns/bad_request.php';
+        bad_request('RECEIVED_BOOKMARK_NOT_FOUND');
+    }
+
+    return $receivedBookmark;
+
+}
