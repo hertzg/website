d64e4a2d158e554662c8129f4388a4244dcd3729
Archil Imnadze <archil@imnadze.ge>
2014-05-13 12:26:39 -0400

    fixed: birthday_time cannot be null

--- www/api-call/contact/fns/request_contact_params.php
+++ www/api-call/contact/fns/request_contact_params.php
@@ -9,7 +9,8 @@ function request_contact_params ($mysqli, $id_users, $exclude_id = 0) {
     include_once __DIR__.'/../../../fns/request_strings.php';
     list($birthday_time) = request_strings('birthday_time');
 
-    $birthday_time = (int)$birthday_time;
+    if ($birthday_time === '') $birthday_time = null;
+    else $birthday_time = (int)$birthday_time;
 
     if ($full_name === '') {
         include_once __DIR__.'/../../fns/bad_request.php';
