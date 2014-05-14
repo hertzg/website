cd97a18f12ddeb02b6a658d02d8f655bd2b5de93
Archil Imnadze <archil@imnadze.ge>
2014-05-14 14:08:07 +0000

    notification/list documentation

--- www/help/api-doc/notification/fns/get_methods.php
+++ www/help/api-doc/notification/fns/get_methods.php
@@ -3,6 +3,7 @@
 function get_methods () {
     return [
         'deleteAll' => 'Deletes all notifications.',
+        'list' => 'Returns a list of all notifications.',
         'post' => 'Sends a notification in a channel.',
     ];
 }
---
+++ www/help/api-doc/notification/list/index.php
@@ -0,0 +1,4 @@
+<?php
+
+include_once '../fns/notification_method_page.php';
+notification_method_page('list', [], []);
