7d8bb8195edc76f33b43cdc062133ead9dff6049
Archil Imnadze <archil@imnadze.ge>
2014-05-14 13:22:10 +0000

    removed duplicate code from notes

---
+++ www/notes/fns/check_receiver.php
@@ -0,0 +1,27 @@
+<?php
+
+function check_receiver ($mysqli, $id_users,
+    $username, &$receiver_id_users, &$errors) {
+
+    if ($username === '') {
+        $errors[] = 'Enter username.';
+    } else {
+        include_once __DIR__.'/../../fns/Users/getByUsername.php';
+        $receiverUser = Users\getByUsername($mysqli, $username);
+        if (!$receiverUser) {
+            $errors[] = "A user with the username doesn't exist.";
+        } else {
+            $receiver_id_users = $receiverUser->id_users;
+            if ($receiver_id_users == $id_users) {
+                $errors[] = 'You cannot send a note to yourself.';
+            } else {
+                include_once __DIR__.'/../../fns/get_users_connection.php';
+                $connection = get_users_connection($mysqli, $receiverUser, $id_users);
+                if (!$connection['can_send_note']) {
+                    $errors[] = "The user isn't receiving notes from you.";
+                }
+            }
+        }
+    }
+
+}
