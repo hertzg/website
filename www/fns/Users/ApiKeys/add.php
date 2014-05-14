915de197d9feaeb5b614a8ee1d2e192eada0bc60
Archil Imnadze <archil@imnadze.ge>
2014-05-14 13:07:53 +0000

    fixed: show api-key after adding

--- www/fns/Users/ApiKeys/add.php
+++ www/fns/Users/ApiKeys/add.php
@@ -5,9 +5,11 @@ namespace Users\ApiKeys;
 function add ($mysqli, $id_users, $name) {
 
     include_once __DIR__.'/../../ApiKeys/add.php';
-    \ApiKeys\add($mysqli, $id_users, $name);
+    $id = \ApiKeys\add($mysqli, $id_users, $name);
 
     include_once __DIR__.'/addNumber.php';
     addNumber($mysqli, $id_users, 1);
+
+    return $id;
 
 }
