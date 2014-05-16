d3135811f565f14ed3d46aff6fc3c3cc753eaaa4
Archil Imnadze <archil@imnadze.ge>
2014-05-16 10:07:05 +0000

    folder/edit api method

--- www/tests/api-call/folder/subfolder.php
+++ www/tests/api-call/folder/subfolder.php
@@ -11,14 +11,18 @@ function expect_folder_object ($engine, $variableName, $folder) {
 include_once '../classes/Engine.php';
 $engine = new Engine;
 
-$new_name = 'test folder name';
-$edited_name = 'edited folder name';
+$response = $engine->request('folder/add', [
+    'name' => 'wrapper folder',
+]);
+$engine->expectSuccess();
 
-$response = $engine->request('folder/add');
-$engine->expectError('ENTER_NAME');
+$parent_id = $response;
+
+$new_name = 'test folder name';
 
 $response = $engine->request('folder/add', [
     'name' => $new_name,
+    'parent_id' => $parent_id,
 ]);
 $engine->expectSuccess();
 $engine->expectNatural('', $response);
@@ -27,49 +31,24 @@ $id = $response;
 
 $response = $engine->request('folder/add', [
     'name' => $new_name,
+    'parent_id' => $parent_id,
 ]);
 $engine->expectError('FOLDER_ALREADY_EXISTS');
 
-$response = $engine->request('folder/get');
-$engine->expectError('FOLDER_NOT_FOUND');
-
 $response = $engine->request('folder/get', [
     'id' => $id,
 ]);
 $engine->expectSuccess();
 expect_folder_object($engine, '', $response);
-$engine->expectValue('.name', $response->name, $new_name);
 
-$response = $engine->request('folder/edit');
-$engine->expectError('FOLDER_NOT_FOUND');
-
-$response = $engine->request('folder/edit', [
-    'id' => $id,
+$response = $engine->request('folder/list', [
+    'parent_id' => $parent_id,
 ]);
-$engine->expectError('ENTER_NAME');
-
-$response = $engine->request('folder/edit', [
-    'id' => $id,
-    'name' => $edited_name,
-]);
-$engine->expectSuccess();
-
-$response = $engine->request('folder/get', [
-    'id' => $id,
-]);
-$engine->expectSuccess();
-expect_folder_object($engine, '', $response);
-$engine->expectValue('.name', $response->name, $edited_name);
-
-$response = $engine->request('folder/list');
 $engine->expectType('', 'array', $response);
 foreach ($response as $i => $folder) {
     expect_folder_object($engine, "[$i]", $folder);
 }
 
-$response = $engine->request('folder/delete');
-$engine->expectError('FOLDER_NOT_FOUND');
-
 $response = $engine->request('folder/delete', [
     'id' => $id,
 ]);
@@ -84,6 +63,11 @@ $response = $engine->request('folder/get', [
     'id' => $id,
 ]);
 $engine->expectError('FOLDER_NOT_FOUND');
+
+$response = $engine->request('folder/delete', [
+    'id' => $parent_id,
+]);
+$engine->expectSuccess();
 
 echo "Done\n";
 echo "$engine->numRequests requests made.\n";
