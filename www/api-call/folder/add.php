5bfa36ef6dc901ef2d53031f7de5b6d7800fe52b
Archil Imnadze <archil@imnadze.ge>
2014-05-16 12:41:26 +0000

    removed trailing space

--- www/api-call/folder/add.php
+++ www/api-call/folder/add.php
@@ -10,7 +10,7 @@ list($folder, $parent_id) = require_parent_folder($mysqli, $id_users);
 include_once 'fns/request_folder_params.php';
 $name = request_folder_params($mysqli, $id_users, $parent_id);
 
-include_once '../../fns/Folders/add.php'; 
+include_once '../../fns/Folders/add.php';
 $id = Folders\add($mysqli, $id_users, $parent_id, $name);
 
 header('Content-Type: application/json');
