a8cdb9e1827877e8e57b76447dcdfb2c52d73736
Archil Imnadze <archil@imnadze.ge>
2014-05-17 01:26:48 -0400

    removed update script

--- www/scripts/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-chdir(__DIR__);
-include_once '../lib/mysqli.php';
-
-$mysqli->query('alter table files add rename_time bigint unsigned not null');
-$mysqli->query('alter table folders add rename_time bigint unsigned not null');
-$mysqli->query('update files set rename_time = insert_time');
-$mysqli->query('update folders set rename_time = insert_time');
