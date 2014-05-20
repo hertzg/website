018b037dab1583c150ef9a365979cb69c98ca68d
Archil Imnadze <archil@imnadze.ge>
2014-05-20 12:39:52 +0000

    contact/received/import api method with tests

---
+++ www/tests/api-call/contact/fns/expect_received_contact_object.php
@@ -0,0 +1,24 @@
+<?php
+
+function expect_received_contact_object ($engine, $variableName, $receivedContact) {
+    $properties = ['id', 'sender_username', 'full_name', 'alias', 'address',
+        'email', 'phone1', 'phone2', 'birthday_time', 'username', 'tags',
+        'favorite', 'tags', 'insert_time'];
+    $engine->expectObject($variableName, $properties, $receivedContact);
+    $engine->expectNatural("$variableName.id", $receivedContact->id);
+    $engine->expectType("$variableName.sender_username", 'string',
+        $receivedContact->sender_username);
+    $engine->expectType("$variableName.full_name", 'string', $receivedContact->full_name);
+    $engine->expectType("$variableName.alias", 'string', $receivedContact->alias);
+    $engine->expectType("$variableName.address", 'string', $receivedContact->address);
+    $engine->expectType("$variableName.email", 'string', $receivedContact->email);
+    $engine->expectType("$variableName.phone1", 'string', $receivedContact->phone1);
+    $engine->expectType("$variableName.phone2", 'string', $receivedContact->phone2);
+    $engine->expectType("$variableName.birthday_time",
+        'integer', $receivedContact->birthday_time);
+    $engine->expectType("$variableName.username", 'string', $receivedContact->username);
+    $engine->expectType("$variableName.tags", 'string', $receivedContact->tags);
+    $engine->expectType("$variableName.favorite", 'boolean', $receivedContact->favorite);
+    $engine->expectType("$variableName.tags", 'string', $receivedContact->tags);
+    $engine->expectNatural("$variableName.insert_time", $receivedContact->insert_time);
+}
---
+++ www/tests/api-call/contact/fns/receive.php
@@ -0,0 +1,23 @@
+<?php
+
+function receive () {
+
+    include_once __DIR__.'/../../fns/get_sender_engine.php';
+    $engine = get_sender_engine();
+
+    $engine->request('contact/send', [
+        'full_name' => 'sample full_name',
+        'alias' => 'sample alias',
+        'address' => 'sample address',
+        'email' => 'sample email',
+        'phone1' => 'sample phone1',
+        'phone2' => 'sample phone2',
+        'birthday_time' => 0,
+        'username' => 'sample username',
+        'tags' => 'tag1 tag2',
+        'favorite' => true,
+        'receiver_username' => 'aimnadze',
+    ]);
+    $engine->expectSuccess();
+
+}
