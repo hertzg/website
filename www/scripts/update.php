292e2701c7d912129be086521d2c44599bc6c0ec
Archil Imnadze <archil@imnadze.ge>
2014-05-22 14:36:09 -0400

    removed update script

--- www/scripts/update.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-chdir(__DIR__);
-include_once '../lib/mysqli.php';
-
-$mysqli->query('alter table events change event_text text text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL after insert_time') || trigger_error($mysqli->error);
