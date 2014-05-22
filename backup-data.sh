449628e09ea982cc2f3d86dd3ac1eb61a1812b37
Archil Imnadze <archil@imnadze.ge>
2014-05-22 10:53:40 +0000

    removed password from backup-data.sh

--- backup-data.sh
+++ backup-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname $BASH_SOURCE`
-mysqldump -h173.201.214.106 -uzviniwww -pJandaba7En --databases zviniwww > zviniwww.sql
+mysqldump -hlocalhost -uzviniwww -p --databases zviniwww > zviniwww.sql
 tar czf backup-data.tgz www/users zviniwww.sql
 rm zviniwww.sql
