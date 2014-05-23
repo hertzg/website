fb1390ad368d29f305e6abe7d013f4de32046d29
Archil Imnadze <archil@imnadze.ge>
2014-05-23 12:39:29 +0000

    removed get_subgroups functions

--- www/help/api-doc/bookmark/index.php
+++ www/help/api-doc/bookmark/index.php
@@ -1,6 +1,10 @@
 <?php
 
 include_once 'fns/get_methods.php';
-include_once 'fns/get_subgroups.php';
 include_once '../fns/group_page.php';
-group_page('bookmark', get_methods(), get_subgroups());
+group_page('bookmark', get_methods(), [
+    'received' => [
+        'title' => 'Received',
+        'description' => 'Methods for manipulating received bookmarks',
+    ],
+]);
