e7cb3cbcf95de7e2bfd729b7f0afd6d16459f733
Archil Imnadze <archil@imnadze.ge>
2014-05-29 09:55:38 +0000

    encrypt encrypted notes in search results

--- www/notes/search/fns/render_notes.php
+++ www/notes/search/fns/render_notes.php
@@ -16,8 +16,15 @@ function render_notes (array $notes, array &$items, array $params, $keyword) {
             );
             $href = "../view/?$queryString";
 
-            $title = htmlspecialchars($note->text);
-            $title = preg_replace($regex, '<mark>$0</mark>', $title);
+            $text = $note->text;
+            if ($note->encrypt) {
+                include_once __DIR__.'/../../../fns/encrypt_text.php';
+                $title = htmlspecialchars(encrypt_text($text));
+            } else {
+                $escapedText = htmlspecialchars($text);
+                $title = preg_replace($regex, '<mark>$0</mark>', $escapedText);
+            }
+
             $items[] = Page\imageArrowLink($title, $href, 'note');
 
         }
--- www/search/fns/render_notes.php
+++ www/search/fns/render_notes.php
@@ -3,8 +3,16 @@
 function render_notes (array $notes, array &$items, $regex) {
     include_once __DIR__.'/../../fns/Page/imageArrowLink.php';
     foreach ($notes as $note) {
-        $title = htmlspecialchars($note->text);
-        $title = preg_replace($regex, '<mark>$0</mark>', $title);
+
+        $text = $note->text;
+        if ($note->encrypt) {
+            include_once __DIR__.'/../../fns/encrypt_text.php';
+            $title = htmlspecialchars(encrypt_text($text));
+        } else {
+            $escapedText = htmlspecialchars($text);
+            $title = preg_replace($regex, '<mark>$0</mark>', $escapedText);
+        }
+
         $href = "../notes/view/?id=$note->id_notes";
         $items[] = Page\imageArrowLink($title, $href, 'note');
     }
