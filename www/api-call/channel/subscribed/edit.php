5b158fcc0473a5ccc1e60beb16fee41b5186f106
Archil Imnadze <archil@imnadze.ge>
2014-05-30 12:06:35 +0000

    channel/subscribed/edit

---
+++ www/api-call/channel/subscribed/edit.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once '../../fns/require_api_key.php';
+list($apiKey, $user, $mysqli) = require_api_key();
+
+include_once 'fns/require_subscribed_channel.php';
+$subscribedChannel = require_subscribed_channel($mysqli, $user->id_users);
+
+include_once '../../../fns/request_strings.php';
+list($receive_notifications) = request_strings('receive_notifications');
+
+$receive_notifications = (bool)$receive_notifications;
+
+include_once '../../../fns/SubscribedChannels/setReceiveNotifications.php';
+SubscribedChannels\setReceiveNotifications(
+    $mysqli, $subscribedChannel->id, $receive_notifications);
+
+header('Content-Type: application/json');
+echo 'true';
