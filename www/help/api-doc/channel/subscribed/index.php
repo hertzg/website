12e8622f31ce8c249fb8402fae21dab38b896a70
Archil Imnadze <archil@imnadze.ge>
2014-06-09 12:22:41 +0000

    fixed channel/subscribed doc

--- www/help/api-doc/channel/subscribed/index.php
+++ www/help/api-doc/channel/subscribed/index.php
@@ -5,6 +5,8 @@ $subgroupKey = 'subscribed';
 include_once '../../fns/channel/get_subgroups.php';
 $subgroup = channel\get_subgroups()['subscribed'];
 
-include_once '../../fns/channel/get_methods.php';
+include_once '../../fns/channel/subscribed/get_methods.php';
+$methods = channel\subscribed\get_methods();
+
 include_once '../../fns/subgroup_page.php';
-subgroup_page('channel', $subgroup, $subgroupKey, channel\get_methods());
+subgroup_page('channel', $subgroup, $subgroupKey, $methods);
