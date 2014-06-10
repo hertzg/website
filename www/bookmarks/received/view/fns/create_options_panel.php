2bfeaef286a34dcc167d87cc54ee2c5958beec71
Archil Imnadze <archil@imnadze.ge>
2014-06-10 08:51:19 +0000

    shortened include paths

--- www/bookmarks/received/view/fns/create_options_panel.php
+++ www/bookmarks/received/view/fns/create_options_panel.php
@@ -4,11 +4,14 @@ function create_options_panel ($receivedBookmark) {
 
     $queryString = "?id=$receivedBookmark->id";
 
-    include_once __DIR__.'/../../../fns/create_open_links.php';
+    $bookmarksDir = __DIR__.'/../../..';
+    $fnsDir = "$bookmarksDir/../fns";
+
+    include_once "$bookmarksDir/fns/create_open_links.php";
     $values = create_open_links($receivedBookmark->url, '../../../');
     list($openLink, $openInNewTabLink) = $values;
 
-    include_once __DIR__.'/../../../../fns/Page/imageArrowLink.php';
+    include_once "$fnsDir/Page/imageArrowLink.php";
 
     $href = "submit-import.php$queryString";
     $importLink = Page\imageArrowLink('Import', $href, 'import-bookmark');
@@ -17,7 +20,7 @@ function create_options_panel ($receivedBookmark) {
     $icon = 'import-bookmark';
     $editAndImportLink = Page\imageArrowLink('Edit and Import', $href, $icon);
 
-    include_once __DIR__.'/../../../../fns/Page/imageLink.php';
+    include_once "$fnsDir/Page/imageLink.php";
     if ($receivedBookmark->archived) {
         $archiveLink = Page\imageLink('Unarchive',
             "submit-unarchive.php$queryString", 'TODO');
@@ -29,15 +32,16 @@ function create_options_panel ($receivedBookmark) {
     $href = "../delete/$queryString";
     $deleteLink = Page\imageArrowLink('Delete', $href, 'trash-bin');
 
-    include_once __DIR__.'/../../../../fns/Page/twoColumns.php';
+    include_once "$fnsDir/Page/staticTwoColumns.php";
+    include_once "$fnsDir/Page/twoColumns.php";
     $content =
         Page\twoColumns($openLink, $openInNewTabLink)
         .'<div class="hr"></div>'
         .Page\twoColumns($importLink, $editAndImportLink)
         .'<div class="hr"></div>'
-        .Page\twoColumns($archiveLink, $deleteLink);
+        .Page\staticTwoColumns($archiveLink, $deleteLink);
 
-    include_once __DIR__.'/../../../../fns/create_panel.php';
+    include_once "$fnsDir/create_panel.php";
     return create_panel('Bookmark Options', $content);
 
 }
--- www/bookmarks/view/fns/create_options_panel.php
+++ www/bookmarks/view/fns/create_options_panel.php
@@ -2,14 +2,17 @@
 
 function create_options_panel ($bookmark) {
 
-    include_once __DIR__.'/../../fns/create_open_links.php';
+    $bookmarksDir = __DIR__.'/../..';
+    $fnsDir = "$bookmarksDir/../fns";
+
+    include_once "$bookmarksDir/fns/create_open_links.php";
     $values = create_open_links($bookmark->url, '../../');
     list($openLink, $openInNewTabLink) = $values;
 
-    include_once __DIR__.'/../../../fns/ItemList/escapedItemQuery.php';
+    include_once "$fnsDir/ItemList/escapedItemQuery.php";
     $queryString = ItemList\escapedItemQuery($bookmark->id_bookmarks);
 
-    include_once __DIR__.'/../../../fns/Page/imageArrowLink.php';
+    include_once "$fnsDir/Page/imageArrowLink.php";
 
     $href = "../edit/$queryString";
     $editLink = Page\imageArrowLink('Edit', $href, 'edit-bookmark');
@@ -20,15 +23,16 @@ function create_options_panel ($bookmark) {
     $href = "../delete/$queryString";
     $deleteLink = Page\imageArrowLink('Delete', $href, 'trash-bin');
 
-    include_once __DIR__.'/../../../fns/Page/twoColumns.php';
+    include_once "$fnsDir/Page/staticTwoColumns.php";
+    include_once "$fnsDir/Page/twoColumns.php";
     $content =
         Page\twoColumns($openLink, $openInNewTabLink)
         .'<div class="hr"></div>'
-        .Page\twoColumns($editLink, $sendLink)
+        .Page\staticTwoColumns($editLink, $sendLink)
         .'<div class="hr"></div>'
         .$deleteLink;
 
-    include_once __DIR__.'/../../../fns/create_panel.php';
+    include_once "$fnsDir/create_panel.php";
     return create_panel('Bookmark Options', $content);
 
 }
