8584b34b779de1da8d62faa0b24d2d04b4f37f0e
Archil Imnadze <archil@imnadze.ge>
2014-06-13 09:55:32 +0000

    fixed id_tasks param

--- www/fns/Users/DeletedItems/addReceivedTask.php
+++ www/fns/Users/DeletedItems/addReceivedTask.php
@@ -6,7 +6,7 @@ function addReceivedTask ($mysqli, $receivedTask) {
     $id_users = $receivedTask->receiver_id_users;
     include_once __DIR__.'/../../DeletedItems/add.php';
     \DeletedItems\add($mysqli, $id_users, 'receivedTask', [
-        'id' => $receivedTask->id_tasks,
+        'id' => $receivedTask->id,
         'text' => $receivedTask->text,
         'tags' => $receivedTask->tags,
         'top_priority' => $receivedTask->top_priority,
