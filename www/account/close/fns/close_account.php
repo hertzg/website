5a94bddf7de766c358e1da1e2d60bae6d5b0282c
Archil Imnadze <archil@imnadze.ge>
2014-06-16 09:52:14 +0000

    separate close_account function from account/close/submit.php

---
+++ www/account/close/fns/close_account.php
@@ -0,0 +1,82 @@
+<?php
+
+function close_account ($mysqli, $id_users) {
+
+    $fnsDir = __DIR__.'/../../../fns';
+
+    include_once "$fnsDir/ApiKeys/deleteOnUser.php";
+    ApiKeys\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Bookmarks/deleteOnUser.php";
+    Bookmarks\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/BookmarkTags/deleteOnUser.php";
+    BookmarkTags\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Channels/deleteOnUser.php";
+    Channels\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Connections/deleteContainingUser.php";
+    Connections\deleteContainingUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Contacts/deleteOnUser.php";
+    Contacts\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/ContactTags/deleteOnUser.php";
+    ContactTags\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Events/deleteOnUser.php";
+    Events\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Feedbacks/deleteOnUser.php";
+    Feedbacks\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Files/deleteOnUser.php";
+    Files\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Folders/deleteOnUser.php";
+    Folders\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Notes/deleteOnUser.php";
+    Notes\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/NoteTags/deleteOnUser.php";
+    NoteTags\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Notifications/deleteOnUser.php";
+    Notifications\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/ReceivedBookmarks/deleteOnReceiver.php";
+    ReceivedBookmarks\deleteOnReceiver($mysqli, $id_users);
+
+    include_once "$fnsDir/ReceivedContacts/deleteOnReceiver.php";
+    ReceivedContacts\deleteOnReceiver($mysqli, $id_users);
+
+    include_once "$fnsDir/ReceivedFiles/deleteOnReceiver.php";
+    ReceivedFiles\deleteOnReceiver($mysqli, $id_users);
+
+    include_once "$fnsDir/ReceivedNotes/deleteOnReceiver.php";
+    ReceivedNotes\deleteOnReceiver($mysqli, $id_users);
+
+    include_once "$fnsDir/ReceivedTasks/deleteOnReceiver.php";
+    ReceivedTasks\deleteOnReceiver($mysqli, $id_users);
+
+    include_once "$fnsDir/Schedules/deleteOnUser.php";
+    Schedules\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/SubscribedChannels/deleteContainingUser.php";
+    SubscribedChannels\deleteContainingUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Tasks/deleteOnUser.php";
+    Tasks\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/TaskTags/deleteOnUser.php";
+    TaskTags\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Tokens/deleteOnUser.php";
+    Tokens\deleteOnUser($mysqli, $id_users);
+
+    include_once "$fnsDir/Users/delete.php";
+    Users\delete($mysqli, $id_users);
+
+}
--- www/account/close/submit.php
+++ www/account/close/submit.php
@@ -32,82 +32,9 @@ if ($errors) {
 
 unset($_SESSION['account/close/errors']);
 
+include_once 'fns/close_account.php';
 include_once '../../lib/mysqli.php';
-
-include_once '../../fns/ApiKeys/deleteOnUser.php';
-ApiKeys\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Bookmarks/deleteOnUser.php';
-Bookmarks\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/BookmarkTags/deleteOnUser.php';
-BookmarkTags\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Channels/deleteOnUser.php';
-Channels\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Connections/deleteContainingUser.php';
-Connections\deleteContainingUser($mysqli, $id_users);
-
-include_once '../../fns/Contacts/deleteOnUser.php';
-Contacts\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/ContactTags/deleteOnUser.php';
-ContactTags\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Events/deleteOnUser.php';
-Events\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Feedbacks/deleteOnUser.php';
-Feedbacks\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Files/deleteOnUser.php';
-Files\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Folders/deleteOnUser.php';
-Folders\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Notes/deleteOnUser.php';
-Notes\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/NoteTags/deleteOnUser.php';
-NoteTags\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Notifications/deleteOnUser.php';
-Notifications\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/ReceivedBookmarks/deleteOnReceiver.php';
-ReceivedBookmarks\deleteOnReceiver($mysqli, $id_users);
-
-include_once '../../fns/ReceivedContacts/deleteOnReceiver.php';
-ReceivedContacts\deleteOnReceiver($mysqli, $id_users);
-
-include_once '../../fns/ReceivedFiles/deleteOnReceiver.php';
-ReceivedFiles\deleteOnReceiver($mysqli, $id_users);
-
-include_once '../../fns/ReceivedNotes/deleteOnReceiver.php';
-ReceivedNotes\deleteOnReceiver($mysqli, $id_users);
-
-include_once '../../fns/ReceivedTasks/deleteOnReceiver.php';
-ReceivedTasks\deleteOnReceiver($mysqli, $id_users);
-
-include_once '../../fns/Schedules/deleteOnUser.php';
-Schedules\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/SubscribedChannels/deleteContainingUser.php';
-SubscribedChannels\deleteContainingUser($mysqli, $id_users);
-
-include_once '../../fns/Tasks/deleteOnUser.php';
-Tasks\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/TaskTags/deleteOnUser.php';
-TaskTags\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Tokens/deleteOnUser.php';
-Tokens\deleteOnUser($mysqli, $id_users);
-
-include_once '../../fns/Users/delete.php';
-Users\delete($mysqli, $id_users);
+close_account($mysqli, $id_users);
 
 $_SESSION['sign-in/messages'] = [
     'Your account has been closed.',
