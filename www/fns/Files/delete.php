245a388097165eb5b534e622d9ca91ebf9ebedb0
Archil Imnadze <archil@imnadze.ge>
2014-06-16 13:17:20 +0000

    removed file manipulation from Files\delete

--- www/fns/Files/delete.php
+++ www/fns/Files/delete.php
@@ -2,12 +2,7 @@
 
 namespace Files;
 
-function delete ($mysqli, $id_users, $id) {
-
-    $sql = "delete from files where id_users = $id_users and id_files = $id";
+function delete ($mysqli, $id) {
+    $sql = "delete from files where id_files = $id";
     $mysqli->query($sql) || trigger_error($mysqli->error);
-
-    include_once __DIR__.'/File/delete.php';
-    \Files\File\delete($id_users, $id);
-
 }
--- www/fns/Users/Files/delete.php
+++ www/fns/Users/Files/delete.php
@@ -4,12 +4,16 @@ namespace Users\Files;
 
 function delete ($mysqli, $file) {
 
+    $id = $file->id_files;
     $id_users = $file->id_users;
 
     include_once __DIR__.'/../../Files/delete.php';
-    \Files\delete($mysqli, $id_users, $file->id_files);
+    \Files\delete($mysqli, $id);
 
     include_once __DIR__.'/../addStorageUsed.php';
     \Users\addStorageUsed($mysqli, $id_users, -$file->size);
+
+    include_once __DIR__.'/../../Files/File/delete.php';
+    \Files\File\delete($id_users, $id);
 
 }
