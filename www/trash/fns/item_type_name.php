8381761b4ae073f74ab79235b9c87990a493210f
Archil Imnadze <archil@imnadze.ge>
2014-06-16 12:59:08 +0000

    added file type name

--- www/trash/fns/item_type_name.php
+++ www/trash/fns/item_type_name.php
@@ -3,6 +3,7 @@
 function item_type_name ($type) {
     if ($type == 'bookmark' || $type == 'receivedBookmark') return 'Bookmark';
     if ($type == 'contact' || $type == 'receivedContact') return 'Contact';
+    if ($type == 'file' || $type == 'receivedFile') return 'File';
     if ($type == 'note' || $type == 'receivedNote') return 'Note';
     if ($type == 'task' || $type == 'receivedTask') return 'Task';
 }
