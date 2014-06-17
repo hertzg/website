3c0584f93aa6b885b90e4419978479047cb6bca5
Archil Imnadze <archil@imnadze.ge>
2014-06-17 07:47:57 +0000

    fixed: cannot hide trash menu item from home

--- www/home/fns/render_trash.php
+++ www/home/fns/render_trash.php
@@ -1,6 +1,10 @@
 <?php
 
 function render_trash ($user, &$items) {
+
+    if (!$user->show_trash) return;
+
     include_once __DIR__.'/../../fns/Page/imageArrowLink.php';
     $items['trash'] = Page\imageArrowLink('Trash', '../trash/', 'trash-bin');
+
 }
