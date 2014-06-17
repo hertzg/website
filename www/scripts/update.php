4b4c1b4228e89ec674bba7c78143e7e150b8c573
Archil Imnadze <archil@imnadze.ge>
2014-06-17 15:29:28 +0000

    script to create received_folders table

---
+++ www/scripts/update.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once '../lib/mysqli.php';
+
+$mysqli->query('create table received_folders'
+    .' (id bigint unsigned not null auto_increment primary key,'
+    .' insert_time bigint(20) unsigned not null,'
+    .' name varchar(255) character set utf8 collate utf8_unicode_ci not null,'
+    .' receiver_id_users bigint unsigned not null,'
+    .' sender_id_users bigint unsigned not null,'
+    .' sender_username varchar(32) character set ascii collate ascii_bin not null)') || trigger_error($mysqli->error);
