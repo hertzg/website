6ac2e5e086778563f0664bc8ebf5ad2eaee48cf0
Archil Imnadze <archil@imnadze.ge>
2014-06-17 15:23:44 +0000

    removed update script

--- www/scripts/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-chdir(__DIR__);
-include_once '../lib/mysqli.php';
-
-$mysqli->query('alter table users add num_deleted_items bigint unsigned not null after num_contacts') || trigger_error($mysqli->error);
-
-echo 'Done';
