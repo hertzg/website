62ff32c716e12549e4b88cd9074183c3a395a9e9
Archil Imnadze <archil@imnadze.ge>
2014-06-18 12:44:20 +0000

    show num_received_folders on home

--- www/home/fns/render_files.php
+++ www/home/fns/render_files.php
@@ -12,15 +12,17 @@ function render_files ($user, array &$items) {
     $icon = 'files';
     $storage_used = $user->storage_used;
     $num_received_files = $user->num_received_files;
-    if ($num_received_files || $storage_used) {
+    $num_received_folders = $user->num_received_folders;
+    if ($num_received_files || $num_received_folders || $storage_used) {
 
         $descriptionItems = [];
         if ($storage_used) {
             include_once __DIR__.'/../../fns/bytestr.php';
             $descriptionItems[] = bytestr($storage_used).' used.';
         }
-        if ($num_received_files) {
-            $descriptionItems[] = "$num_received_files received.";
+        if ($num_received_files || $num_received_folders) {
+            $n = $num_received_files + $num_received_folders;
+            $descriptionItems[] = "$n received.";
         }
         $description = join(' ', $descriptionItems);
 
