2502abd8b356abd47bc0319dac59d67ea7b66c06
Archil Imnadze <archil@imnadze.ge>
2014-06-18 10:53:18 +0000

    send folder contents recursively

--- www/fns/Users/Folders/send.php
+++ www/fns/Users/Folders/send.php
@@ -5,10 +5,38 @@ namespace Users\Folders;
 function send ($mysqli, $user, $receiver_id_users, $folder) {
 
     include_once __DIR__.'/../../ReceivedFolders/add.php';
-    \ReceivedFolders\add($mysqli, $user->id_users, $user->username,
-        $receiver_id_users, $folder->name);
+    $id_received_folders = \ReceivedFolders\add($mysqli, $user->id_users,
+        $user->username, $receiver_id_users, $folder->name);
 
     include_once __DIR__.'/Received/addNumber.php';
     \Users\Folders\Received\addNumber($mysqli, $user->id_users, 1);
+
+    $copy = function ($id, $parent_id, $copy) use ($mysqli, $id_received_folders) {
+
+        include_once __DIR__.'/../../Files/indexInFolder.php';
+        $files = \Files\indexInFolder($mysqli, $id);
+
+        if ($files) {
+            include_once __DIR__.'/../../ReceivedFolderFiles/add.php';
+            foreach ($files as $file) {
+                \ReceivedFolderFiles\add($mysqli, $id_received_folders,
+                    $parent_id, $file->name, $file->size);
+            }
+        }
+
+        include_once __DIR__.'/../../Folders/indexInFolder.php';
+        $folders = \Folders\indexInFolder($mysqli, $id);
+
+        if ($folders) {
+            include_once __DIR__.'/../../ReceivedFolderSubfolders/add.php';
+            foreach ($folders as $folder) {
+                $id = \ReceivedFolderSubfolders\add($mysqli,
+                    $id_received_folders, $parent_id, $folder->name);
+                $copy($folder->id_folders, $id, $copy);
+            }
+        }
+
+    };
+    $copy($folder->id_folders, 0, $copy);
 
 }
