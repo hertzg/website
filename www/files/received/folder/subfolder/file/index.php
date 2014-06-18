1ba948a36d08d7ea267d7fb79b48b760aa296c4e
Archil Imnadze <archil@imnadze.ge>
2014-06-18 13:32:53 +0000

    added file previews in received folders

--- www/files/received/folder/subfolder/file/index.php
+++ www/files/received/folder/subfolder/file/index.php
@@ -5,14 +5,14 @@ include_once '../../../../../lib/mysqli.php';
 list($receivedFolderFile, $id, $user) = require_received_folder_file($mysqli);
 
 $id_received_folders = $receivedFolderFile->id_received_folders;
-
 $fnsDir = '../../../../../fns';
 $title = "Received Folder #$id_received_folders";
-
 $parentHref = "../?id=$id_received_folders";
+$name = $receivedFolderFile->name;
 
 include_once "$fnsDir/bytestr.php";
 include_once "$fnsDir/Form/label.php";
+include_once "$fnsDir/Page/filePreview.php";
 include_once "$fnsDir/Page/imageLink.php";
 include_once "$fnsDir/Page/tabs.php";
 $content = Page\tabs(
@@ -29,9 +29,11 @@ $content = Page\tabs(
     $title,
     Page\imageLink('.. Parent folder', $parentHref, 'parent-folder')
     .'<div class="hr"></div>'
-    .Form\label('File name', htmlspecialchars($receivedFolderFile->name))
+    .Form\label('File name', htmlspecialchars($name))
     .'<div class="hr"></div>'
     .Form\label('Size', bytestr($receivedFolderFile->size))
+    .'<div class="hr"></div>'
+    .Form\label('Preview', Page\filePreview($name, $id, '../../download-file/'))
 );
 
 include_once "$fnsDir/echo_page.php";
