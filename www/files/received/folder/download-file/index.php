d25975b0877e971f43367b32b871d1ebf7002e74
Archil Imnadze <archil@imnadze.ge>
2014-06-19 09:14:03 +0000

    changed var name

--- www/files/received/folder/download-file/index.php
+++ www/files/received/folder/download-file/index.php
@@ -2,7 +2,7 @@
 
 include_once '../fns/require_received_folder_file.php';
 include_once '../../../../lib/mysqli.php';
-list($file, $id, $user) = require_received_folder_file($mysqli);
+list($receivedFolderFile, $id, $user) = require_received_folder_file($mysqli);
 
 session_commit();
 
@@ -14,10 +14,10 @@ $contentType = str_collapse_spaces($contentType);
 
 if ($contentType === '') $contentType = 'application/x-octet-stream';
 
-$filename = addslashes($file->name);
+$filename = addslashes($receivedFolderFile->name);
 header("Content-Disposition: attachment; filename=\"$filename\"");
 header("Content-Type: $contentType");
-header("Content-Length: $file->size");
+header("Content-Length: $receivedFolderFile->size");
 
 include_once '../../../../fns/ReceivedFolderFiles/File/path.php';
 readfile(ReceivedFolderFiles\File\path($user->id_users, $id));
