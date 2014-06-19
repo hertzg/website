b01424354f84a2df5e18f9073ffa540378f448bb
Archil Imnadze <archil@imnadze.ge>
2014-06-19 07:48:48 +0000

    add drop table if exists to update script

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -3,6 +3,8 @@
 chdir(__DIR__);
 include_once '../lib/mysqli.php';
 
+$mysqli->query('drop table if exists received_folders') || trigger_error($mysqli->error);
+
 $mysqli->query('create table received_folders'
     .' (archived tinyint unsigned not null,'
     .' id bigint unsigned not null auto_increment primary key,'
@@ -15,12 +17,16 @@ $mysqli->query('create table received_folders'
 $mysqli->query('alter table users add num_received_folders bigint unsigned not null after num_received_files') || trigger_error($mysqli->error);
 $mysqli->query('alter table users add num_archived_received_folders bigint unsigned not null after num_archived_received_files') || trigger_error($mysqli->error);
 
+$mysqli->query('drop table if exists received_folder_subfolders') || trigger_error($mysqli->error);
+
 $mysqli->query('create table received_folder_subfolders'
     .' (id bigint unsigned not null auto_increment primary key,'
     .' id_received_folders bigint unsigned not null,'
     .' id_users bigint unsigned not null,'
     .' name varchar(255) character set utf8 collate utf8_unicode_ci not null,'
     .' parent_id bigint unsigned not null)') || trigger_error($mysqli->error);
+
+$mysqli->query('drop table if exists received_folder_files') || trigger_error($mysqli->error);
 
 $mysqli->query('create table received_folder_files'
     .' (id bigint unsigned not null auto_increment primary key,'
