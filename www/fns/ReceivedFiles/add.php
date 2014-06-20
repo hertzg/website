be9197925c67b1ada052bca1eb44f7054b7af436
Archil Imnadze <archil@imnadze.ge>
2014-06-20 10:58:12 +0000

    improved wrapping

--- www/fns/ReceivedFiles/add.php
+++ www/fns/ReceivedFiles/add.php
@@ -2,18 +2,18 @@
 
 namespace ReceivedFiles;
 
-function add ($mysqli, $sender_id_users, $sender_username, $receiver_id_users,
-    $name, $size) {
+function add ($mysqli, $sender_id_users,
+    $sender_username, $receiver_id_users, $name, $size) {
 
     $sender_username = $mysqli->real_escape_string($sender_username);
     $name = $mysqli->real_escape_string($name);
     $insert_time = time();
 
     $sql = 'insert into received_files'
-        .' (sender_id_users, sender_username, receiver_id_users,'
-        .' name, size, insert_time)'
-        ." values ($sender_id_users, '$sender_username', $receiver_id_users,"
-        ." '$name', '$size', $insert_time)";
+        .' (sender_id_users, sender_username,'
+        .' receiver_id_users, name, size, insert_time)'
+        ." values ($sender_id_users, '$sender_username',"
+        ." $receiver_id_users, '$name', '$size', $insert_time)";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
     return $mysqli->insert_id;
