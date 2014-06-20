7e1238d1a635a4e393b171d72c996dd5bd1ace47
Archil Imnadze <archil@imnadze.ge>
2014-06-20 11:00:30 +0000

    fixed: users can get other user's received files

--- www/fns/ReceivedFiles/getOnReceiver.php
+++ www/fns/ReceivedFiles/getOnReceiver.php
@@ -3,7 +3,8 @@
 namespace ReceivedFiles;
 
 function getOnReceiver ($mysqli, $receiver_id_users, $id) {
-    $sql = "select * from received_files where id = $id";
+    $sql = "select * from received_files where id = $id"
+        ." and receiver_id_users = $receiver_id_users";
     include_once __DIR__.'/../mysqli_single_object.php';
     return mysqli_single_object($mysqli, $sql);
 }
