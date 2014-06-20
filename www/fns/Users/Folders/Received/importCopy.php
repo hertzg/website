8436f20ba3d6b61ba28d44ff46922a94b4311dc5
Archil Imnadze <archil@imnadze.ge>
2014-06-20 11:35:33 +0000

    importCopy now imports files

--- www/fns/Users/Folders/Received/importCopy.php
+++ www/fns/Users/Folders/Received/importCopy.php
@@ -19,6 +19,35 @@ function importCopy ($mysqli, $receivedFolder, $parent_id) {
     include_once __DIR__.'/../../../Folders/add.php';
     $id_folders = \Folders\add($mysqli, $id_users, $parent_id, $name);
 
+    $import = function ($parent_id, $parent_id_folders, $import) use ($mysqli, $receivedFolder, $id_users) {
+
+        include_once __DIR__.'/../../../ReceivedFolderSubfolders/indexOnParent.php';
+        $subfolders = \ReceivedFolderSubfolders\indexOnParent(
+            $mysqli, $receivedFolder->id, $parent_id);
+
+        foreach ($subfolders as $subfolder) {
+            $id_folders = \Folders\add($mysqli, $id_users,
+                $parent_id_folders, $subfolder->name);
+            $import($subfolder->id, $id_folders, $import);
+        }
+
+        include_once __DIR__.'/../../../ReceivedFolderFiles/indexOnParent.php';
+        $files = \ReceivedFolderFiles\indexOnParent(
+            $mysqli, $receivedFolder->id, $parent_id);
+
+        if ($files) {
+            include_once __DIR__.'/../../Files/add.php';
+            include_once __DIR__.'/../../../ReceivedFolderFiles/File/path.php';
+            foreach ($files as $file) {
+                $filePath = \ReceivedFolderFiles\File\path($id_users, $file->id);
+                \Users\Files\add($mysqli, $id_users,
+                    $parent_id_folders, $file->name, $filePath);
+            }
+        }
+
+    };
+    $import(0, $id_folders, $import);
+
     return $id_folders;
 
 }
