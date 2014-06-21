e99e490742b44bf0a35d78cd3922a8ac9bfc2323
Archil Imnadze <archil@imnadze.ge>
2014-06-21 08:18:40 +0400

    notifications link description improvement

--- www/home/fns/render_notifications.php
+++ www/home/fns/render_notifications.php
@@ -14,11 +14,16 @@ function render_notifications ($user, array &$items) {
         $description = '';
         $num_new_notifications = $user->num_new_notifications;
         if ($num_new_notifications) {
-            $description = "$num_new_notifications new."
-                ." $num_notifications total.";
+
+            $description = "$num_new_notifications new.";
+            if ($num_new_notifications != $num_notifications) {
+                $description .= " $num_notifications total.";
+            }
+
             include_once "$fnsPageDir/imageArrowLinkWithDescription.php";
             $items[$key] = Page\imageArrowLinkWithDescription($title,
                 $description, $href, 'notification');
+
         } else {
             $description = "$num_notifications total.";
             include_once "$fnsPageDir/imageArrowLinkWithDescription.php";
