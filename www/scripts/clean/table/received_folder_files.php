dba67380aa0b46c3863d0c9cbc090809cfd2e03e
Archil Imnadze <archil@imnadze.ge>
2014-06-23 14:32:16 +0000

    fixed clean scripts, fixed deleting trashed items

--- www/scripts/clean/table/received_folder_files.php
+++ www/scripts/clean/table/received_folder_files.php
@@ -3,16 +3,33 @@
 
 chdir(__DIR__);
 include_once '../../../lib/mysqli.php';
+include_once '../../../fns/mysqli_query_object.php';
 
 $microtime = microtime(true);
+$deleted = 0;
 
-// TODO fix this, the item may be in trash
 $sql = 'delete from received_folder_files'
-    .' where id_received_folders not in (select id from received_folders)'
-    .' or id_users not in (select id_users from users)'
+    .' where id_users not in (select id_users from users)'
     .' or (parent_id != 0 and parent_id not in (select id from received_folder_subfolders))';
 $mysqli->query($sql) || trigger_error($mysqli->error);
+$deleted = $mysqli->affected_rows;
+
+$ids = [];
+$sql = "select data_json from deleted_items where data_type = 'receivedFolder'";
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) $ids[] = json_decode($row->data_json)->id;
+
+$sql = 'select id, id_received_folders from received_folder_files'
+    .' where id_received_folders not in (select id from received_folders)';
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) {
+    if (!in_array($row->id_received_folders, $ids)) {
+        $sql = "delete from received_folder_files where id = $row->id";
+        $mysqli->query($sql) || trigger_error($mysqli->error);
+        $deleted++;
+    }
+}
 
 $elapsedSeconds = number_format(microtime(true) - $microtime, 3);
 echo "Done in $elapsedSeconds seconds.\n"
-    ." $mysqli->affected_rows row(s) deleted.\n";
+    ." $deleted row(s) deleted.\n";
--- www/scripts/clean/table/received_folder_subfolders.php
+++ www/scripts/clean/table/received_folder_subfolders.php
@@ -1,23 +1,42 @@
 #!/usr/bin/php
 <?php
 
+function delete ($mysqli, $id, &$deleted) {
+    $sql = "delete from received_folder_subfolders where id = $id";
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+    $deleted++;
+}
+
 chdir(__DIR__);
 include_once '../../../lib/mysqli.php';
 include_once '../../../fns/mysqli_query_object.php';
 
 $microtime = microtime(true);
 
-// TODO fix this, the item may be in trash
-$sql = 'select id from received_folder_subfolders'
-    .' where id_received_folders not in (select id from received_folders)'
-    .' or id_users not in (select id_users from users)'
-    .' or (parent_id != 0 and parent_id not in (select id from received_folder_subfolders))';
+$sql = 'delete from received_folder_subfolders'
+    .' where id_users not in (select id_users from users)';
+$mysqli->query($sql) || trigger_error($mysqli->error);
+$deleted = $mysqli->affected_rows;
+
+$sql = 'select id from received_folder_subfolders where parent_id != 0'
+    .' and parent_id not in (select id from received_folder_subfolders)';
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) delete($mysqli, $row->id, $deleted);
+
+$ids = [];
+$sql = "select data_json from deleted_items where data_type = 'receivedFolder'";
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) $ids[] = json_decode($row->data_json)->id;
+
+$sql = 'select id, id_received_folders from received_folder_subfolders'
+    .' where id_received_folders not in (select id from received_folders)';
 $rows = mysqli_query_object($mysqli, $sql);
 foreach ($rows as $row) {
-    $sql = "delete from received_folder_subfolders where id = $row->id";
-    $mysqli->query($sql) || trigger_error($mysqli->error);
+    if (!in_array($row->id_received_folders, $ids)) {
+        delete($mysqli, $row->id, $deleted);
+    }
 }
 
 $elapsedSeconds = number_format(microtime(true) - $microtime, 3);
 echo "Done in $elapsedSeconds seconds.\n"
-    .' '.count($rows)." row(s) deleted.\n";
+    ." $deleted row(s) deleted.\n";
