c2ffbbd78e421f13e014ef527953fc0b2ba73a06
Archil Imnadze <archil@imnadze.ge>
2014-06-29 15:19:57 +0400

    fixed ReceivedTasks\add parameters

--- www/fns/Users/Tasks/Received/add.php
+++ www/fns/Users/Tasks/Received/add.php
@@ -7,7 +7,7 @@ function add ($mysqli, $sender_id_users, $sender_username,
 
     include_once __DIR__.'/../../../ReceivedTasks/add.php';
     \ReceivedTasks\add($mysqli, $sender_id_users, $sender_username,
-        $receiver_id_users, $text, $top_priority, $tags);
+        $receiver_id_users, $text, $tags, $top_priority);
 
     include_once __DIR__.'/addNumber.php';
     addNumber($mysqli, $receiver_id_users, 1);
