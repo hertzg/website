8d480e28ea0c87052fbc95318e2b6584b92fbfec
Archil Imnadze <archil@imnadze.ge>
2014-06-29 15:21:40 +0400

    fixed ReeivedTasks\addDelete parameters

--- www/fns/Users/Tasks/Received/addDeleted.php
+++ www/fns/Users/Tasks/Received/addDeleted.php
@@ -9,7 +9,7 @@ function addDeleted ($mysqli, $receiver_id_users, $data) {
     include_once __DIR__.'/../../../ReceivedTasks/addDeleted.php';
     \ReceivedTasks\addDeleted($mysqli, $data->id, $data->sender_id_users,
         $data->sender_username, $receiver_id_users, $data->text,
-        $data->top_priority, $data->tags, $archived, $data->insert_time);
+        $data->tags, $data->top_priority, $archived, $data->insert_time);
 
     include_once __DIR__.'/addNumber.php';
     addNumber($mysqli, $receiver_id_users, 1);
