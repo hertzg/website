57b82ccd4cd283c267c8be55d252e88afd1fb238
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:44:58 +0000

    restore deadline_time field from trash

--- www/fns/Users/Tasks/Received/addDeleted.php
+++ www/fns/Users/Tasks/Received/addDeleted.php
@@ -6,12 +6,11 @@ function addDeleted ($mysqli, $receiver_id_users, $data) {
 
     $archived = $data->archived;
 
-    $deadline_time = null;
-
     include_once __DIR__.'/../../../ReceivedTasks/addDeleted.php';
     \ReceivedTasks\addDeleted($mysqli, $data->id, $data->sender_id_users,
-        $data->sender_username, $receiver_id_users, $data->text, $deadline_time,
-        $data->tags, $data->top_priority, $archived, $data->insert_time);
+        $data->sender_username, $receiver_id_users, $data->text,
+        $data->deadline_time, $data->tags, $data->top_priority,
+        $archived, $data->insert_time);
 
     include_once __DIR__.'/addNumber.php';
     addNumber($mysqli, $receiver_id_users, 1);
