11d623679ad58f023d03cf62fc40c6c1b44d6637
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:51:42 +0000

    shorter code

--- www/api-call/task/fns/request_task_params.php
+++ www/api-call/task/fns/request_task_params.php
@@ -2,16 +2,18 @@
 
 function request_task_params () {
 
-    include_once __DIR__.'/../../../fns/Tasks/request.php';
+    $fnsDir = __DIR__.'/../../../fns';
+
+    include_once "$fnsDir/Tasks/request.php";
     list($text, $top_priority) = Tasks\request();
 
-    include_once __DIR__.'/../../../fns/request_strings.php';
+    include_once "$fnsDir/request_strings.php";
     list($deadline_time) = request_strings('deadline_time');
 
     if ($deadline_time === '') {
         $deadline_time = null;
     } else {
-        include_once __DIR__.'/../../../fns/time_today.php';
+        include_once "$fnsDir/time_today.php";
         $deadline_time = time_today($deadline_time);
     }
 
