da44b06087564fc41410c9e125d96a5aac9e3c00
Archil Imnadze <archil@imnadze.ge>
2014-06-30 11:11:37 +0000

    unwrapped lines

--- www/submit-signout.php
+++ www/submit-signout.php
@@ -23,9 +23,7 @@ if (array_key_exists('token', $_SESSION)) {
 
 session_destroy();
 
-$_SESSION['sign-in/messages'] = [
-    'You have been signed out.',
-];
+$_SESSION['sign-in/messages'] = ['You have been signed out.'];
 
 include_once 'fns/redirect.php';
 redirect('sign-in/');
