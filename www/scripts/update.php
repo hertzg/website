b561d32ed11d58c7df91b5b04625d57dccf99b0b
Archil Imnadze <archil@imnadze.ge>
2014-06-30 11:08:43 +0000

    script to make id_users nullable in feedbacks table

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -3,25 +3,7 @@
 chdir(__DIR__);
 include_once '../lib/mysqli.php';
 
-$sql = 'alter table users change num_new_notifications_for_home home_num_new_notifications bigint unsigned not null after insert_time';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
-$sql = 'alter table users add home_num_new_received_bookmarks bigint unsigned not null after home_num_new_notifications';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
-$sql = 'alter table users add home_num_new_received_contacts bigint unsigned not null after home_num_new_received_bookmarks';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
-$sql = 'alter table users add home_num_new_received_files bigint unsigned not null after home_num_new_received_contacts';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
-$sql = 'alter table users add home_num_new_received_folders bigint unsigned not null after home_num_new_received_files';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
-$sql = 'alter table users add home_num_new_received_notes bigint unsigned not null after home_num_new_received_folders';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
-$sql = 'alter table users add home_num_new_received_tasks bigint unsigned not null after home_num_new_received_notes';
+$sql = 'alter table feedbacks change id_users id_users bigint unsigned first';
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
 echo "Done\n";
