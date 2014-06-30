1fd2645e94f14d4d983cf945900a8165cd3a33fa
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:38:21 +0000

    script to set deadline_time to null in deleted_items

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -9,4 +9,16 @@ $mysqli->query($sql) || trigger_error($mysqli->error);
 $sql = 'alter table received_tasks add deadline_time bigint unsigned after archived';
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
+$sql = "select * from deleted_items where data_type in ('task', 'receivedTask')";
+include_once '../fns/mysqli_query_object.php';
+$items = mysqli_query_object($mysqli, $sql);
+foreach ($items as $item) {
+    $data = json_decode($item->data_json);
+    $data->deadline_time = null;
+    $data = json_encode($data);
+    $data = $mysqli->real_escape_string($data);
+    $sql = "update deleted_items set data_json = '$data' where id = $item->id";
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+}
+
 echo "Done\n";
