b60ddd8037aef1f68067d6b2882ccbccb9d67f94
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:21:35 +0000

    ReceivedTasks\addDeleted deadline_time param

--- www/fns/ReceivedTasks/addDeleted.php
+++ www/fns/ReceivedTasks/addDeleted.php
@@ -3,20 +3,22 @@
 namespace ReceivedTasks;
 
 function addDeleted ($mysqli, $id, $sender_id_users, $sender_username,
-    $receiver_id_users, $text, $tags, $top_priority, $archived, $insert_time) {
+    $receiver_id_users, $text, $deadline_time, $tags, $top_priority,
+    $archived, $insert_time) {
 
     $sender_username = $mysqli->real_escape_string($sender_username);
     $text = $mysqli->real_escape_string($text);
+    if ($deadline_time === null) $deadline_time = 'null';
     $tags = $mysqli->real_escape_string($tags);
     $top_priority = $top_priority ? '1' : '0';
     $archived = $archived ? '1' : '0';
 
     $sql = 'insert into received_tasks'
         .' (id, sender_id_users, sender_username,'
-        .' receiver_id_users, text, tags,'
+        .' receiver_id_users, text, deadline_time, tags,'
         .' top_priority, archived, insert_time)'
         ." values ($id, $sender_id_users, '$sender_username'"
-        .", $receiver_id_users, '$text', '$tags',"
+        .", $receiver_id_users, '$text', $deadline_time, '$tags',"
         ." $top_priority, $archived, $insert_time)";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
