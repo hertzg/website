eb866b7c905929ee13961e81430be0f5be502b0b
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:24:07 +0000

    use deadline_time and import received task

--- www/fns/Users/Tasks/Received/importCopy.php
+++ www/fns/Users/Tasks/Received/importCopy.php
@@ -9,11 +9,9 @@ function importCopy ($mysqli, $receivedTask) {
     include_once __DIR__.'/../../../Tags/parse.php';
     $tag_names = \Tags\parse($tags);
 
-    $deadline_time = null;
-
     include_once __DIR__.'/../add.php';
     return \Users\Tasks\add($mysqli, $receivedTask->receiver_id_users,
-        $receivedTask->text, $deadline_time, $tags, $tag_names,
+        $receivedTask->text, $receivedTask->deadline_time, $tags, $tag_names,
         $receivedTask->top_priority);
 
 }
