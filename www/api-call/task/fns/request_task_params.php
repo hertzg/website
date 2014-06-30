c360d6d745c0f3a22d88b5c329065ff238a38f7b
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:49:30 +0000

    task/add, edit deadline_time param

--- www/api-call/task/fns/request_task_params.php
+++ www/api-call/task/fns/request_task_params.php
@@ -5,6 +5,16 @@ function request_task_params () {
     include_once __DIR__.'/../../../fns/Tasks/request.php';
     list($text, $top_priority) = Tasks\request();
 
+    include_once __DIR__.'/../../../fns/request_strings.php';
+    list($deadline_time) = request_strings('deadline_time');
+
+    if ($deadline_time === '') {
+        $deadline_time = null;
+    } else {
+        include_once __DIR__.'/../../../fns/time_today.php';
+        $deadline_time = time_today($deadline_time);
+    }
+
     if ($text === '') {
         include_once __DIR__.'/../../fns/bad_request.php';
         bad_request('ENTER_TEXT');
@@ -12,8 +22,6 @@ function request_task_params () {
 
     include_once __DIR__.'/../../fns/request_tags.php';
     list($tags, $tag_names) = request_tags();
-
-    $deadline_time = null;
 
     return [$text, $deadline_time, $tags, $tag_names, $top_priority];
 
