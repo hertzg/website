782f42f6a35054d7eba7250cfd6efa66509d1a06
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:15:13 +0000

    script to create deadline_time column on received_tasks table

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -6,4 +6,7 @@ include_once '../lib/mysqli.php';
 $sql = 'alter table tasks add deadline_time bigint unsigned first';
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
+$sql = 'alter table received_tasks add deadline_time bigint unsigned after archived';
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
 echo "Done\n";
