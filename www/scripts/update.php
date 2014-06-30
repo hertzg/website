2f9e8b5a105f9198b3607ccfe2a081a936511667
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:07:17 +0000

    script to create deadline column

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -3,7 +3,7 @@
 chdir(__DIR__);
 include_once '../lib/mysqli.php';
 
-$sql = 'alter table feedbacks change id_users id_users bigint unsigned first';
+$sql = 'alter table tasks add deadline_time bigint unsigned first';
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
 echo "Done\n";
