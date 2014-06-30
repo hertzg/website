d660cefc087bed51fe240b5d11ba1e83e9ca3db4
Archil Imnadze <archil@imnadze.ge>
2014-06-30 13:17:37 +0000

    ReceivedTasks.add deadline_time param

--- www/fns/ReceivedTasks/add.php
+++ www/fns/ReceivedTasks/add.php
@@ -3,19 +3,22 @@
 namespace ReceivedTasks;
 
 function add ($mysqli, $sender_id_users, $sender_username,
-    $receiver_id_users, $text, $tags, $top_priority) {
+    $receiver_id_users, $text, $deadline_time, $tags, $top_priority) {
 
     $sender_username = $mysqli->real_escape_string($sender_username);
     $text = $mysqli->real_escape_string($text);
+    if ($deadline_time === null) $deadline_time = 'null';
     $tags = $mysqli->real_escape_string($tags);
     $top_priority = $top_priority ? '1' : '0';
     $insert_time = time();
 
     $sql = 'insert into received_tasks'
         .' (sender_id_users, sender_username,'
-        .' receiver_id_users, text, tags, top_priority, insert_time)'
+        .' receiver_id_users, text, deadline_time,'
+        .' tags, top_priority, insert_time)'
         ." values ($sender_id_users, '$sender_username'"
-        .", $receiver_id_users, '$text', '$tags', $top_priority, $insert_time)";
+        .", $receiver_id_users, '$text', $deadline_time,"
+        ." '$tags', $top_priority, $insert_time)";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
 }
--- www/fns/Users/Tasks/Received/add.php
+++ www/fns/Users/Tasks/Received/add.php
@@ -5,9 +5,11 @@ namespace Users\Tasks\Received;
 function add ($mysqli, $sender_id_users, $sender_username,
     $receiver_id_users, $text, $tags, $top_priority) {
 
+    $deadline_time = null;
+
     include_once __DIR__.'/../../../ReceivedTasks/add.php';
     \ReceivedTasks\add($mysqli, $sender_id_users, $sender_username,
-        $receiver_id_users, $text, $tags, $top_priority);
+        $receiver_id_users, $text, $deadline_time, $tags, $top_priority);
 
     include_once __DIR__.'/addNumberNew.php';
     addNumberNew($mysqli, $receiver_id_users, 1);
