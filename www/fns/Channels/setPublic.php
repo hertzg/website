f3e3d8aaa33465960f7bead97b458a7e58d56929
Archil Imnadze <archil@imnadze.ge>
2014-07-03 12:09:10 +0000

    removed unused php files

--- www/fns/Channels/setPublic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Channels;
-
-function setPublic ($mysqli, $id, $public) {
-    $public = $public ? '1' : '0';
-    $sql = "update channels set public = $public where id = $id";
-    $mysqli->query($sql) || trigger_error($mysqli->error);
-}
