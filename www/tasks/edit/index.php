19e0d5cfa22bf6943d52b675b6a15f8883dfefcf
Archil Imnadze <archil@imnadze.ge>
2014-07-03 15:28:01 +0000

    split tasks/edit into multiple parts

--- www/tasks/edit/index.php
+++ www/tasks/edit/index.php
@@ -34,72 +34,8 @@ unset(
     $_SESSION['tasks/view/messages']
 );
 
-include_once '../../fns/Tasks/maxLengths.php';
-$maxLengths = Tasks\maxLengths();
-
-$base = '../../';
-
-include_once '../../fns/ItemList/escapedItemQuery.php';
-include_once '../../fns/ItemList/listHref.php';
-include_once '../../fns/Form/button.php';
-include_once '../../fns/Form/checkbox.php';
-include_once '../../fns/Form/datefield.php';
-include_once '../../fns/Form/hidden.php';
-include_once '../../fns/Form/textarea.php';
-include_once '../../fns/Form/textfield.php';
-include_once '../../fns/ItemList/itemHiddenInputs.php';
-include_once '../../fns/Page/sessionErrors.php';
-include_once '../../fns/Page/staticTwoColumns.php';
-include_once '../../fns/Page/tabs.php';
-$content = Page\tabs(
-    [
-        [
-            'title' => '&middot;&middot;&middot;',
-            'href' => ItemList\listHref(),
-        ],
-        [
-            'title' => "Task #$id",
-            'href' => '../view/'.ItemList\escapedItemQuery($id),
-        ],
-    ],
-    'Edit',
-    Page\sessionErrors('tasks/edit/errors')
-    .'<form action="submit.php" method="post">'
-        .Form\textarea('text', 'Text', [
-            'value' => $values['text'],
-            'maxlength' => $maxLengths['text'],
-            'autofocus' => true,
-            'required' => true,
-        ])
-        .'<div class="hr"></div>'
-        .Form\datefield([
-            'name' => 'deadline_day',
-            'value' => $values['deadline_day'],
-        ], [
-            'name' => 'deadline_month',
-            'value' => $values['deadline_month'],
-        ], [
-            'name' => 'deadline_year',
-            'value' => $values['deadline_year'],
-            'min' => date('Y'),
-            'max' => date('Y') + 2,
-        ], 'Deadline', false, true)
-        .'<div class="hr"></div>'
-        .Form\textfield('tags', 'Tags', [
-            'value' => $values['tags'],
-            'maxlength' => $maxLengths['tags'],
-        ])
-        .'<div class="hr"></div>'
-        .Form\checkbox($base, 'top_priority',
-            'Mark as Top Priority', $values['top_priority'])
-        .'<div class="hr"></div>'
-        .Page\staticTwoColumns(
-            Form\button('Save Changes'),
-            Form\button('Send', 'sendButton')
-        )
-        .ItemList\itemHiddenInputs($id)
-    .'</form>'
-);
+include_once 'fns/create_content.php';
+$content = create_content($id, $values);
 
 include_once '../../fns/echo_page.php';
-echo_page($user, "Edit Task #$id", $content, $base);
+echo_page($user, "Edit Task #$id", $content, '../../');
