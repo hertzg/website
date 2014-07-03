18cab445ad077f8002f02bf4ec443fe95c0d0f3c
Archil Imnadze <archil@imnadze.ge>
2014-07-03 12:32:39 +0000

    delete subscribed channel when connection is broken

--- www/fns/Users/SubscribedChannels/deleteDisconnected.php
+++ www/fns/Users/SubscribedChannels/deleteDisconnected.php
@@ -3,4 +3,24 @@
 namespace Users\SubscribedChannels;
 
 function deleteDisconnected ($mysqli, $id_users) {
+
+    $fnsDir = __DIR__.'/../..';
+
+    include_once "$fnsDir/SubscribedChannels/indexPublisherLockedOnSubscriber.php";
+    $subscribedChannels = \SubscribedChannels\indexPublisherLockedOnSubscriber(
+        $mysqli, $id_users);
+
+    if ($subscribedChannels) {
+        include_once "$fnsDir/get_users_connection.php";
+        include_once "$fnsDir/Users/get.php";
+        foreach ($subscribedChannels as $subscribedChannel) {
+            $user = \Users\get($mysqli, $subscribedChannel->publisher_id_users);
+            $connection = get_users_connection($mysqli, $user, $id_users);
+            if (!$connection['can_send_channel']) {
+                include_once "$fnsDir/Users/Channels/Users/delete.php";
+                \Users\Channels\Users\delete($mysqli, $subscribedChannel);
+            }
+        }
+    }
+
 }
