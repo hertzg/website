1d0f8a0ee549caf0f77f819be169396978d307c1
Archil Imnadze <archil@imnadze.ge>
2014-07-05 00:56:34 +0400

    save media_type when adding files

--- www/fns/Files/add.php
+++ www/fns/Files/add.php
@@ -4,13 +4,18 @@ namespace Files;
 
 function add ($mysqli, $id_users, $id_folders, $name, $size) {
 
+    include_once __DIR__.'/../detect_media_type.php';
+    $media_type = \detect_media_type($name);
+    if ($media_type === null) $media_type = 'null';
+    else $media_type = "'$media_type'";
+
     $name = $mysqli->real_escape_string($name);
     $insert_time = $rename_time = time();
 
     $sql = 'insert into files'
-        .' (id_users, id_folders, name,'
+        .' (id_users, id_folders, media_type, name,'
         .' size, insert_time, rename_time)'
-        ." value ($id_users, $id_folders, '$name',"
+        ." value ($id_users, $id_folders, $media_type, '$name',"
         ." $size, $insert_time, $rename_time)";
 
     $mysqli->query($sql) || trigger_error($mysqli->error);
--- www/fns/ReceivedFolderFiles/add.php
+++ www/fns/ReceivedFolderFiles/add.php
@@ -5,12 +5,17 @@ namespace ReceivedFolderFiles;
 function add ($mysqli, $id_received_folders,
     $id_users, $parent_id, $name, $size) {
 
+    include_once __DIR__.'/../detect_media_type.php';
+    $media_type = \detect_media_type($name);
+    if ($media_type === null) $media_type = 'null';
+    else $media_type = "'$media_type'";
+
     $name = $mysqli->real_escape_string($name);
     $sql = 'insert into received_folder_files'
         .' (id_received_folders, id_users,'
-        .' parent_id, name, size)'
+        .' parent_id, media_type, name, size)'
         ." values ($id_received_folders, $id_users,"
-        ." $parent_id, '$name', $size)";
+        ." $parent_id, $media_type, '$name', $size)";
     $mysqli->query($sql) || trigger_error($mysqli->error);
     return $mysqli->insert_id;
 
