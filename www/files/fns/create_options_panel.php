b3c05b973cdded16ebfbb14da257b07762e99c24
Archil Imnadze <archil@imnadze.ge>
2014-07-04 08:13:10 +0000

    removed base parameter from files/fns/create_options_panel

--- www/files/fns/create_options_panel.php
+++ www/files/fns/create_options_panel.php
@@ -1,6 +1,6 @@
 <?php
 
-function create_options_panel ($user, $id_folders, $base = '') {
+function create_options_panel ($user, $id_folders) {
 
     $fnsDir = __DIR__.'/../../fns';
 
@@ -8,30 +8,30 @@ function create_options_panel ($user, $id_folders, $base = '') {
 
     include_once "$fnsDir/Page/imageArrowLink.php";
 
-    $href = "{$base}new-folder/";
+    $href = 'new-folder/';
     if ($id_folders) $href .= "?parent_id_folders=$id_folders";
     $options[] = Page\imageArrowLink('New Folder', $href, 'create-folder');
 
-    $href = "{$base}upload-files/";
+    $href = 'upload-files/';
     if ($id_folders) $href .= "?parent_id_folders=$id_folders";
     $options[] = Page\imageArrowLink('Upload Files', $href, 'upload');
 
     if ($id_folders) {
 
         $title = 'Rename This Folder';
-        $href = "{$base}rename-folder/?id_folders=$id_folders";
+        $href = "rename-folder/?id_folders=$id_folders";
         $options[] = Page\imageArrowLink($title, $href, 'rename');
 
         $title = 'Move This Folder';
-        $href = "{$base}move-folder/?id_folders=$id_folders";
+        $href = "move-folder/?id_folders=$id_folders";
         $options[] = Page\imageArrowLink($title, $href, 'move-folder');
 
         $title = 'Send This Folder';
-        $href = "{$base}send-folder/?id_folders=$id_folders";
+        $href = "send-folder/?id_folders=$id_folders";
         $options[] = Page\imageArrowLink($title, $href, 'send');
 
         $title = 'Delete This Folder';
-        $href = "{$base}delete-folder/?id_folders=$id_folders";
+        $href = "delete-folder/?id_folders=$id_folders";
         $options[] = Page\imageArrowLink($title, $href, 'trash-bin');
 
     } else {
@@ -42,7 +42,7 @@ function create_options_panel ($user, $id_folders, $base = '') {
             include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
             $title = 'Received Files';
             $description = "$n total.";
-            $href = "{$base}received/";
+            $href = 'received/';
             $options[] = Page\imageArrowLinkWithDescription($title,
                 $description, $href, 'mail');
         }
