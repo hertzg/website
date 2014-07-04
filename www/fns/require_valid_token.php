868016e4983a99de6ef2560cc28b576d587812ca
Archil Imnadze <archil@imnadze.ge>
2014-07-04 14:49:03 +0000

    suppressed hex2bin warnings

--- www/fns/require_valid_token.php
+++ www/fns/require_valid_token.php
@@ -7,26 +7,29 @@ function require_valid_token ($mysqli) {
         $username = $_COOKIE['username'];
         $token_text = $_COOKIE['token'];
         if (is_string($username) && is_string($token_text)) {
+            include_once __DIR__.'/is_md5.php';
+            if (is_md5($token_text)) {
 
-            $token_text = hex2bin($token_text);
+                $token_text = hex2bin($token_text);
 
-            include_once __DIR__.'/Tokens/getByUsernameTokenText.php';
-            $token = Tokens\getByUsernameTokenText(
-                $mysqli, $username, $token_text);
+                include_once __DIR__.'/Tokens/getByUsernameTokenText.php';
+                $token = Tokens\getByUsernameTokenText(
+                    $mysqli, $username, $token_text);
 
-            if ($token) {
+                if ($token) {
 
-                $key = 'HTTP_USER_AGENT';
-                if (array_key_exists($key, $_SERVER)) {
-                    $user_agent = $_SERVER[$key];
-                } else {
-                    $user_agent = null;
+                    $key = 'HTTP_USER_AGENT';
+                    if (array_key_exists($key, $_SERVER)) {
+                        $user_agent = $_SERVER[$key];
+                    } else {
+                        $user_agent = null;
+                    }
+
+                    if ($token->user_agent !== $user_agent) $token = null;
+
                 }
 
-                if ($token->user_agent !== $user_agent) $token = null;
-
             }
-
         }
     }
     return $token;
