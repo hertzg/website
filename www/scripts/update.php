04e62483d36c39f86e7fd0d8fd8f32cc992ac305
Archil Imnadze <archil@imnadze.ge>
2014-07-05 14:01:20 +0400

    wrapped code

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -3,16 +3,20 @@
 chdir(__DIR__);
 include_once '../lib/mysqli.php';
 
-$sql = "alter table deleted_files add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after insert_time";
+$sql = 'alter table deleted_files'
+    ." add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after insert_time";
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
-$sql = "alter table files add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after insert_time";
+$sql = 'alter table files'
+    ." add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after insert_time";
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
-$sql = "alter table received_files add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after insert_time";
+$sql = 'alter table received_files'
+    ." add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after insert_time";
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
-$sql = "alter table received_folder_files add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after id_users";
+$sql = 'alter table received_folder_files'
+    ." add media_type enum('audio', 'image', 'video', 'unknown') default 'unknown' not null after id_users";
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
 echo "Done\n";
