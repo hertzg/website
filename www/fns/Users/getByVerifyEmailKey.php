bd3245a9db03668a0b1a77e6faf4746b1f2808d2
Archil Imnadze <archil@imnadze.ge>
2014-08-12 16:28:54 +0400

    forbid verifying an empty email

--- www/fns/Users/getByVerifyEmailKey.php
+++ www/fns/Users/getByVerifyEmailKey.php
@@ -8,7 +8,7 @@ function getByVerifyEmailKey ($mysqli, $id_users, $verify_email_key) {
     $verify_email_key = $mysqli->real_escape_string($verify_email_key);
 
     $sql = "select * from users where id_users = $id_users"
-        ." and verify_email_key = '$verify_email_key'";
+        ." and email != '' and verify_email_key = '$verify_email_key'";
 
     include_once __DIR__.'/../mysqli_single_object.php';
     return mysqli_single_object($mysqli, $sql);
