f013c4d0122fa66e27bbcc3fd14e19dc5611a9c6
Archil Imnadze <archil@imnadze.ge>
2014-08-20 13:19:19 +0000

    allow any number of days as expire day

--- www/account/api-keys/fns/request_expires.php
+++ www/account/api-keys/fns/request_expires.php
@@ -5,13 +5,14 @@ function request_expires (&$expires, &$expire_time) {
     include_once __DIR__.'/../../../fns/request_strings.php';
     list($expires) = request_strings('expires');
 
-    include_once __DIR__.'/../../../fns/time_today.php';
-    $time_today = time_today();
+    if ($expires !== '') {
 
-    $day = 60 * 60 * 24;
-    if ($expires === '7') $expire_time = $time_today + $day * 7;
-    elseif ($expires === '30') $expire_time = $time_today + $day * 30;
-    elseif ($expires === '360') $expire_time = $time_today + $day * 360;
-    else $expires = '';
+        include_once __DIR__.'/../../../fns/time_today.php';
+        $time_today = time_today();
+
+        $expires = abs((int)$expires);
+        $expire_time = $time_today + 60 * 60 * 24 * $expires;
+
+    }
 
 }
