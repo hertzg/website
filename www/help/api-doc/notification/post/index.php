e13115bb35589e69054a3c65ad121a679b9d5a46
Archil Imnadze <archil@imnadze.ge>
2014-08-24 14:29:56 +0400

    notification method error descriptions

--- www/help/api-doc/notification/post/index.php
+++ www/help/api-doc/notification/post/index.php
@@ -11,4 +11,7 @@ notification_method_page('post', [
         'name' => 'text',
         'description' => 'The text to send.',
     ],
-], ['CHANNEL_NOT_FOUND', 'ENTER_TEXT']);
+], [
+    'CHANNEL_NOT_FOUND' => "A channel with the name doesn't exist.",
+    'ENTER_TEXT' => 'The text is empty.',
+]);
