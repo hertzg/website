db6949786238f546cfd28e0fc64c0d567894c0e9
Archil Imnadze <archil@imnadze.ge>
2014-08-24 12:01:12 +0400

    fixed: select items should be capitalized

--- www/account/api-keys/fns/create_expires_field.php
+++ www/account/api-keys/fns/create_expires_field.php
@@ -5,16 +5,16 @@ function create_expires_field ($expires) {
     $options = [];
 
     if ($expires !== 'never' && $expires !== '7' && $expires !== '30' && $expires !== '360') {
-        if ($expires === '0') $text = 'today';
-        elseif ($expires === '1') $text = 'tomorrow';
-        else $text = "in $expires days";
+        if ($expires === '0') $text = 'Today';
+        elseif ($expires === '1') $text = 'Tomorrow';
+        else $text = "In $expires days";
         $options[$expires] = $text;
     }
 
-    $options['never'] = 'never';
-    $options['7'] = 'in 7 days';
-    $options['30'] = 'in 30 days';
-    $options['360'] = 'in 360 days';
+    $options['never'] = 'Never';
+    $options['7'] = 'In 7 days';
+    $options['30'] = 'In 30 days';
+    $options['360'] = 'In 360 days';
 
     include_once __DIR__.'/../../../fns/Form/select.php';
     return Form\select('expires', 'Expires', $options, $expires);
