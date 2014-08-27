4fcb91b4c6ac7f88b5219dd3fd4294a347d61871
Archil Imnadze <archil@imnadze.ge>
2014-08-27 07:45:39 +0000

    fixed: Unknown column 'num_received_folderss' in 'field list'

--- www/fns/Users/Folders/Received/addNumber.php
+++ www/fns/Users/Folders/Received/addNumber.php
@@ -3,7 +3,7 @@
 namespace Users\Folders\Received;
 
 function addNumber ($mysqli, $id_users, $n) {
-    $sql = "update users set num_received_folderss = num_received_folders + $n"
+    $sql = "update users set num_received_folders = num_received_folders + $n"
         ." where id_users = $id_users";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 }
