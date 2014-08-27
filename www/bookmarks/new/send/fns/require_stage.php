d70d202ed0b0b4a904593191a20e63f43bd3d205
Archil Imnadze <archil@imnadze.ge>
2014-08-27 13:29:07 +0000

    fix base on bookmarks/new/send/remove-recipient page

--- www/bookmarks/new/send/fns/require_stage.php
+++ www/bookmarks/new/send/fns/require_stage.php
@@ -1,16 +1,16 @@
 <?php
 
-function require_stage () {
+function require_stage ($base = '') {
 
     $fnsDir = __DIR__.'/../../../../fns';
 
     include_once "$fnsDir/require_user.php";
-    $user = require_user('../../../');
+    $user = require_user("$base../../../");
 
     $key = 'bookmarks/new/values';
     if (!array_key_exists($key, $_SESSION)) {
         include_once "$fnsDir/redirect.php";
-        redirect('..');
+        redirect("$base..");
     }
 
     return [$user, $_SESSION[$key]];
--- www/bookmarks/new/send/remove-recipient/fns/require_recipient.php
+++ www/bookmarks/new/send/remove-recipient/fns/require_recipient.php
@@ -3,7 +3,7 @@
 function require_recipient () {
 
     include_once '../fns/require_stage.php';
-    list($user) = require_stage();
+    list($user) = require_stage('../');
 
     include_once '../../../../fns/SendForm/NewItem/requireRecipient.php';
     $username = SendForm\NewItem\requireRecipient('bookmarks/new/send/values');
