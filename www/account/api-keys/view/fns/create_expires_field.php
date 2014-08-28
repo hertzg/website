d5f67601fdde3de528d1da62ad06eb5394eebcca
Archil Imnadze <archil@imnadze.ge>
2014-08-28 13:27:14 +0000

    show when an api key was expired

--- www/account/api-keys/view/fns/create_expires_field.php
+++ www/account/api-keys/view/fns/create_expires_field.php
@@ -1,14 +1,30 @@
 <?php
 
 function create_expires_field ($apiKey) {
+
+    include_once __DIR__.'/../../../../fns/time_today.php';
+    $time_today = time_today();
+
+    $label = 'Expires';
+
     $expire_time = $apiKey->expire_time;
-    if ($expire_time === null) $content = 'never';
-    else {
-        $days = ceil(($expire_time - time()) / (60 * 60 * 24));
-        if ($days == 0) $content = 'Today';
-        elseif ($days == 1) $content = 'Tomorrow';
-        else $content = 'on '.date('M j, l', $expire_time)." ($days days left)";
+    if ($expire_time === null) {
+        $content = 'never';
+    } else {
+        $days = ceil(($expire_time - $time_today) / (60 * 60 * 24));
+        if ($expire_time >= $time_today) {
+            if ($days == 0) $content = 'today';
+            elseif ($days == 1) $content = 'tomorrow';
+            else $content = 'on '.date('M j, l', $expire_time)." ($days days left)";
+        } else {
+            $days = -$days;
+            $label = 'Expired';
+            if ($days == 1) $content = 'yesterday';
+            else $content = 'on '.date('M j, l', $expire_time)." ($days days ago)";
+        }
     }
+
     include_once __DIR__.'/../../../../fns/Form/label.php';
-    return Form\label('Expires', $content);
+    return Form\label($label, $content);
+
 }
