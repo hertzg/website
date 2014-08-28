67cb55be7fb9d89af5332bdd66c76dc68efba8f9
Archil Imnadze <archil@imnadze.ge>
2014-08-28 16:00:56 +0400

    reuse SendForm\renderRecipients instead of SendForm\EditItem\renderRecipients

--- www/fns/SendForm/EditItem/recipientsPage.php
+++ www/fns/SendForm/EditItem/recipientsPage.php
@@ -38,8 +38,8 @@ function recipientsPage ($mysqli, $user, $id, $pageTitle,
     } else {
         if ($recipients) {
 
-            include_once __DIR__.'/renderRecipients.php';
-            $content = renderRecipients($recipients, $itemParams);
+            include_once __DIR__.'/../renderRecipients.php';
+            $content = \SendForm\renderRecipients($recipients, $itemParams);
 
             include_once __DIR__.'/../../RecipientList/enterPanel.php';
             if ($contacts) {
