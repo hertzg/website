6b434d7815603712d9fc6ffb9acfbb1be55b7599
Archil Imnadze <archil@imnadze.ge>
2014-09-04 07:25:11 +0100

    restore timezone of contact from trash

--- www/fns/Contacts/addDeleted.php
+++ www/fns/Contacts/addDeleted.php
@@ -2,9 +2,9 @@
 
 namespace Contacts;
 
-function addDeleted ($mysqli, $id, $id_users, $full_name, $alias, $address,
-    $email, $phone1, $phone2, $birthday_time, $username, $tags, $favorite,
-    $insert_time, $update_time) {
+function addDeleted ($mysqli, $id, $id_users, $full_name, $alias,
+    $address, $email, $phone1, $phone2, $birthday_time, $username,
+    $timezone, $tags, $favorite, $insert_time, $update_time) {
 
     $full_name = $mysqli->real_escape_string($full_name);
     $alias = $mysqli->real_escape_string($alias);
@@ -14,16 +14,17 @@ function addDeleted ($mysqli, $id, $id_users, $full_name, $alias, $address,
     $phone2 = $mysqli->real_escape_string($phone2);
     if ($birthday_time === null) $birthday_time = 'null';
     $username = $mysqli->real_escape_string($username);
+    if ($timezone === null) $timezone = 'null';
     $tags = $mysqli->real_escape_string($tags);
     $favorite = $favorite ? '1' : '0';
 
     $sql = 'insert into contacts'
         .' (id_contacts, id_users, full_name, alias, address,'
         .' email, phone1, phone2, birthday_time, username,'
-        .' tags, favorite, insert_time, update_time)'
+        .' timezone, tags, favorite, insert_time, update_time)'
         ." values ($id, $id_users, '$full_name', '$alias', '$address',"
         ." '$email', '$phone1', '$phone2', $birthday_time, '$username',"
-        ." '$tags', $favorite, $insert_time, $update_time)";
+        ." $timezone, '$tags', $favorite, $insert_time, $update_time)";
 
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
--- www/fns/Users/Contacts/addDeleted.php
+++ www/fns/Users/Contacts/addDeleted.php
@@ -21,7 +21,7 @@ function addDeleted ($mysqli, $user, $data) {
     include_once __DIR__.'/../../Contacts/addDeleted.php';
     \Contacts\addDeleted($mysqli, $id, $id_users, $full_name, $alias,
         $data->address, $data->email, $phone1, $phone2, $birthday_time,
-        $data->username, $data->tags, $favorite,
+        $data->username, $data->timezone, $data->tags, $favorite,
         $data->insert_time, $data->update_time);
 
     include_once __DIR__.'/../../ContactTags/add.php';
