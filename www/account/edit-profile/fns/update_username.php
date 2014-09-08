9afdda8ccfd8c318d851f58c5837664fb22a33ea
Archil Imnadze <archil@imnadze.ge>
2014-09-08 11:03:40 +0400

    update username in deleted items

--- www/account/edit-profile/fns/update_username.php
+++ www/account/edit-profile/fns/update_username.php
@@ -24,4 +24,21 @@ function update_username ($mysqli, $id_users, $username) {
         $mysqli->query($sql) || trigger_error($mysqli->error);
     }
 
+    $sql = 'select * from deleted_items where data_type in'
+        ." ('receivedBookmark', 'receivedContact', 'receivedFile',"
+        ." 'receivedFolder', 'receivedNote', 'receivedTask')";
+    include_once __DIR__.'/../../../fns/mysqli_query_object.php';
+    $deletedItems = mysqli_query_object($mysqli, $sql);
+
+    foreach ($deletedItems as $deletedItem) {
+        $data = json_decode($deletedItem->data_json);
+        if ($data->sender_id_users == $id_users) {
+            $data->sender_username = $username;
+            $data_json = json_encode($data);
+            $sql = "update deleted_items set data_json = '$data_json'"
+                ." where id = $deletedItem->id";
+            $mysqli->query($sql) || trigger_error($mysqli->error);
+        }
+    }
+
 }
