c977c6fe475a80323d513675375a494e7249ed99
Archil Imnadze <archil@imnadze.ge>
2014-09-10 16:40:06 +0400

    upload phoot page

--- www/contacts/photo/edit/index.php
+++ www/contacts/photo/edit/index.php
@@ -4,6 +4,8 @@ include_once '../../fns/require_contact.php';
 include_once '../../../lib/mysqli.php';
 list($contact, $id, $user) = require_contact($mysqli, '../');
 
+include_once '../../../fns/Form/button.php';
+include_once '../../../fns/Form/filefield.php';
 include_once '../../../fns/ItemList/escapedItemQuery.php';
 include_once '../../../fns/ItemList/listHref.php';
 include_once '../../../fns/Page/tabs.php';
@@ -19,8 +21,12 @@ $content = Page\tabs(
         ],
     ],
     'Edit Photo',
-    ''
+    '<form action="submit.php" method="post">'
+        .Form\filefield('file', 'Select a file', ['required' => true])
+        .'<div class="hr"></div>'
+        .Form\button('Upload Photo')
+    .'</form>'
 );
 
 include_once '../../../fns/echo_page.php';
-echo_page($user, "Edit Contact#$id Photo", $content, '../../../');
+echo_page($user, 'Edit Contact Photo', $content, '../../../');
--- www/fns/Form/filefield.php
+++ www/fns/Form/filefield.php
@@ -9,9 +9,15 @@ function filefield ($name, $text, $options) {
         $multipleAttribute = ' multiple="multiple"';
     }
 
+    $requiredAttribute = '';
+    if (array_key_exists('required', $options) && $options['required']) {
+        $requiredAttribute = ' required="required"';
+    }
+
     include_once __DIR__.'/association.php';
     return association(
-        "<input class=\"form-filefield\" type=\"file\"$multipleAttribute"
+        '<input class="form-filefield" type="file"'
+        ."$multipleAttribute$requiredAttribute"
         ." id=\"$name\" name=\"$name\" />",
         "<label for=\"$name\">$text:</label>"
     );
