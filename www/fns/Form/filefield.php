22d0224e9fd07eb23a465e50be60f24cb7fff3f7
Archil Imnadze <archil@imnadze.ge>
2014-09-10 16:36:01 +0400

    filefield options param

--- www/fns/Form/filefield.php
+++ www/fns/Form/filefield.php
@@ -2,10 +2,16 @@
 
 namespace Form;
 
-function filefield ($name, $text) {
+function filefield ($name, $text, $options) {
+
+    $multipleAttribute = '';
+    if (array_key_exists('multiple', $options) && $options['multiple']) {
+        $multipleAttribute = ' multiple="multiple"';
+    }
+
     include_once __DIR__.'/association.php';
     return association(
-        '<input class="form-filefield" type="file" multiple="multiple"'
+        "<input class=\"form-filefield\" type=\"file\"$multipleAttribute"
         ." id=\"$name\" name=\"$name\" />",
         "<label for=\"$name\">$text:</label>"
     );
