49f6a750baaa76580b0b83ee15e86b58d783faff
Archil Imnadze <archil@imnadze.ge>
2014-09-12 20:01:32 +0400

    api key name field maxlength attribute

--- www/account/api-keys/fns/create_general_fields.php
+++ www/account/api-keys/fns/create_general_fields.php
@@ -1,15 +1,22 @@
 <?php
 
 function create_general_fields ($values) {
+
     $fnsDir = __DIR__.'/../../../fns';
-    include_once "$fnsDir/Form/textfield.php";
+
+    include_once "$fnsDir/ApiKeys/maxLengths.php";
+    $maxLengths = ApiKeys\maxLengths();
+
     include_once __DIR__.'/create_expires_field.php';
+    include_once "$fnsDir/Form/textfield.php";
     return
         Form\textfield('name', 'Name', [
             'value' => $values['name'],
+            'maxlength' => $maxLengths['name'],
             'required' => true,
             'autofocus' => true,
         ])
         .'<div class="hr"></div>'
         .create_expires_field($values['expires']);
+
 }
