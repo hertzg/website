38811762b0a0ce7696b634d166312d618b1bec83
Archil Imnadze <archil@imnadze.ge>
2014-09-12 19:54:08 +0400

    set success messages after the operation is complete

--- www/account/api-keys/delete/submit.php
+++ www/account/api-keys/delete/submit.php
@@ -7,11 +7,11 @@ include_once '../fns/require_api_key.php';
 include_once '../../../lib/mysqli.php';
 list($apiKey, $id, $user) = require_api_key($mysqli);
 
-$_SESSION['account/api-keys/messages'] = ['The key has been deleted.'];
-unset($_SESSION['account/api-keys/errors']);
-
 include_once '../../../fns/Users/ApiKeys/delete.php';
 Users\ApiKeys\delete($mysqli, $apiKey);
+
+$_SESSION['account/api-keys/messages'] = ['The key has been deleted.'];
+unset($_SESSION['account/api-keys/errors']);
 
 include_once '../../../fns/redirect.php';
 redirect('..');
