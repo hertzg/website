a276a557894b22d08068541d1f04e76dda198602
Archil Imnadze <archil@imnadze.ge>
2014-09-12 18:49:12 +0400

    fixed import received contact photo ref increasing issue

--- www/fns/Users/Contacts/Received/import.php
+++ www/fns/Users/Contacts/Received/import.php
@@ -10,6 +10,12 @@ function import ($mysqli, $user, $receivedContact) {
     include_once __DIR__.'/purge.php';
     purge($mysqli, $receivedContact);
 
+    $photo_id = $receivedContact->photo_id;
+    if ($photo_id) {
+        include_once __DIR__.'/../../../ContactPhotos/delete.php';
+        \ContactPhotos\delete($mysqli, $photo_id);
+    }
+
     return $id;
 
 }
