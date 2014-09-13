459b47887fc3d6ff92099f145e39d443b900e82a
Archil Imnadze <archil@imnadze.ge>
2014-09-13 09:51:26 +0400

    Users\Tokens\deleteAll

--- www/account/tokens/delete-all/submit.php
+++ www/account/tokens/delete-all/submit.php
@@ -5,14 +5,10 @@ require_same_domain_referer('./');
 
 include_once '../../../fns/require_user.php';
 $user = require_user('../../../');
-$id_users = $user->id_users;
 
-include_once '../../../fns/Tokens/deleteOnUser.php';
+include_once '../../../fns/Users/Tokens/deleteAll.php';
 include_once '../../../lib/mysqli.php';
-Tokens\deleteOnUser($mysqli, $id_users);
-
-include_once '../../../fns/Users/Tokens/clearNumber.php';
-Users\Tokens\clearNumber($mysqli, $id_users);
+Users\Tokens\deleteAll($mysqli, $user->id_users);
 
 unset($_SESSION['tokens/errors']);
 $_SESSION['tokens/messages'] = ['All remembered sessions have been deleted.'];
--- www/fns/Users/Tokens/deleteAll.php
+++ www/fns/Users/Tokens/deleteAll.php
@@ -2,7 +2,12 @@
 
 namespace Users\Tokens;
 
-function clearNumber ($mysqli, $id_users) {
+function deleteAll ($mysqli, $id_users) {
+
+    include_once __DIR__.'/../../Tokens/deleteOnUser.php';
+    \Tokens\deleteOnUser($mysqli, $id_users);
+
     $sql = "update users set num_tokens = 0 where id_users = $id_users";
     $mysqli->query($sql) || trigger_error($mysqli->error);
+
 }
