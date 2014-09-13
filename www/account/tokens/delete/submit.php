73417a84882e61bbdb6ed5e0ff074238357572e4
Archil Imnadze <archil@imnadze.ge>
2014-09-13 09:48:02 +0400

    Users\Tokens\delete

--- www/account/tokens/delete/submit.php
+++ www/account/tokens/delete/submit.php
@@ -7,11 +7,8 @@ include_once '../fns/require_token.php';
 include_once '../../../lib/mysqli.php';
 list($token, $id, $user) = require_token($mysqli);
 
-include_once '../../../fns/Tokens/delete.php';
-Tokens\delete($mysqli, $id);
-
-include_once '../../../fns/Users/Tokens/addNumber.php';
-Users\Tokens\addNumber($mysqli, $user->id_users, -1);
+include_once '../../../fns/Users/Tokens/delete.php';
+Users\Tokens\delete($mysqli, $token);
 
 unset($_SESSION['tokens/errors']);
 $_SESSION['tokens/messages'] = ['Remembered session has been deleted.'];
---
+++ www/fns/Users/Tokens/delete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Users\Tokens;
+
+function delete ($mysqli, $token) {
+
+    include_once __DIR__.'/../../Tokens/delete.php';
+    \Tokens\delete($mysqli, $token->id);
+
+    include_once __DIR__.'/addNumber.php';
+    addNumber($mysqli, $token->id_users, -1);
+
+}
