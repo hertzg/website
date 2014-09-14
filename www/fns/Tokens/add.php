dcbca5d2a39c5ff661666b53b1da8088254e38ea
Archil Imnadze <archil@imnadze.ge>
2014-09-14 15:37:45 +0400

    sort token by insert_time

--- www/fns/Tokens/add.php
+++ www/fns/Tokens/add.php
@@ -8,16 +8,13 @@ function add ($mysqli, $id_users, $username, $token_text, $user_agent) {
     $token_text = $mysqli->real_escape_string($token_text);
     $insert_time = $access_time = time();
 
-    if ($user_agent === null) {
-        $user_agent = 'null';
-    } else {
-        $user_agent = "'".$mysqli->real_escape_string($user_agent)."'";
-    }
+    if ($user_agent === null) $user_agent = 'null';
+    else $user_agent = "'".$mysqli->real_escape_string($user_agent)."'";
 
-    $sql = 'insert into tokens (id_users, username, token_text, user_agent,'
-        .' insert_time, access_time)'
-        ." values ($id_users, '$username', '$token_text', $user_agent,"
-        ." $insert_time, $access_time)";
+    $sql = 'insert into tokens (id_users, username, token_text,'
+        .' user_agent, insert_time, access_time)'
+        ." values ($id_users, '$username', '$token_text',"
+        ." $user_agent, $insert_time, $access_time)";
 
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
--- www/fns/Tokens/indexOnUser.php
+++ www/fns/Tokens/indexOnUser.php
@@ -4,7 +4,7 @@ namespace Tokens;
 
 function indexOnUser ($mysqli, $id_users) {
     $sql = "select * from tokens where id_users = $id_users"
-        .' order by token_text';
+        .' order by insert_time';
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
