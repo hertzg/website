f6e45a97413545b2a52d5586aa011bfb02786921
Archil Imnadze <archil@imnadze.ge>
2014-09-14 13:47:01 +0400

    unwrapped lines

--- www/sign-in/fns/remember_session.php
+++ www/sign-in/fns/remember_session.php
@@ -5,17 +5,16 @@ function remember_session ($mysqli, $user) {
     $token_text = md5(uniqid(), true);
 
     $key = 'HTTP_USER_AGENT';
-    if (array_key_exists($key, $_SERVER)) {
-        $user_agent = $_SERVER[$key];
-    } else {
-        $user_agent = null;
-    }
+    if (array_key_exists($key, $_SERVER)) $user_agent = $_SERVER[$key];
+    else $user_agent = null;
 
-    include_once __DIR__.'/../../fns/Users/Tokens/add.php';
-    $id = Users\Tokens\add($mysqli, $user->id_users, $user->username,
-        $token_text, $user_agent);
+    $fnsDir = __DIR__.'/../../fns';
 
-    include_once __DIR__.'/../../fns/Tokens/get.php';
+    include_once "$fnsDir/Users/Tokens/add.php";
+    $id = Users\Tokens\add($mysqli, $user->id_users,
+        $user->username, $token_text, $user_agent);
+
+    include_once "$fnsDir/Tokens/get.php";
     $token = Tokens\get($mysqli, $id);
 
     if ($token) {
