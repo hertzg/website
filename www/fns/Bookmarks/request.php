1d888bc8961909bf8fd84970039bba2ed529659c
Archil Imnadze <archil@imnadze.ge>
2014-09-15 15:31:10 +0400

    use fnsDir var in Bookmarks

--- www/fns/Bookmarks/request.php
+++ www/fns/Bookmarks/request.php
@@ -4,10 +4,12 @@ namespace Bookmarks;
 
 function request () {
 
-    include_once __DIR__.'/../request_strings.php';
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/request_strings.php";
     list($url, $title) = request_strings('url', 'title');
 
-    include_once __DIR__.'/../str_collapse_spaces.php';
+    include_once "$fnsDir/str_collapse_spaces.php";
     $url = str_collapse_spaces($url);
     $title = str_collapse_spaces($title);
 
