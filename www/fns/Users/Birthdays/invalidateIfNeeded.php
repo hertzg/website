071148b706741f322123f2f37e9092a57e03cd5c
Archil Imnadze <archil@imnadze.ge>
2014-09-15 15:41:15 +0400

    less indentation

--- www/fns/Users/Birthdays/invalidateIfNeeded.php
+++ www/fns/Users/Birthdays/invalidateIfNeeded.php
@@ -3,27 +3,27 @@
 namespace Users\Birthdays;
 
 function invalidateIfNeeded ($mysqli, &$user, $birthday_time) {
-    if ($birthday_time !== null) {
 
-        include_once __DIR__.'/../../time_today.php';
-        $timeToday = time_today();
-        $timeTomorrow = $timeToday + 60 * 60 * 24;
+    if ($birthday_time === null) return;
 
-        $day = date('j', $birthday_time);
-        $month = date('n', $birthday_time);
-        $year = date('Y', $timeToday);
-        $birthdayTimeThisYear = mktime(0, 0, 0, $month, $day, $year);
+    include_once __DIR__.'/../../time_today.php';
+    $timeToday = time_today();
+    $timeTomorrow = $timeToday + 60 * 60 * 24;
 
-        if ($birthdayTimeThisYear == $timeToday ||
-            $birthdayTimeThisYear == $timeTomorrow) {
+    $day = date('j', $birthday_time);
+    $month = date('n', $birthday_time);
+    $year = date('Y', $timeToday);
+    $birthdayTimeThisYear = mktime(0, 0, 0, $month, $day, $year);
 
-            if ($user->birthdays_check_day) {
-                $user->birthdays_check_day = 0;
-                include_once __DIR__.'/invalidate.php';
-                invalidate($mysqli, $user->id_users);
-            }
+    if ($birthdayTimeThisYear == $timeToday ||
+        $birthdayTimeThisYear == $timeTomorrow) {
 
+        if ($user->birthdays_check_day) {
+            $user->birthdays_check_day = 0;
+            include_once __DIR__.'/invalidate.php';
+            invalidate($mysqli, $user->id_users);
         }
 
     }
+
 }
