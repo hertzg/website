d9d6bd65e71f90f9ac65f148128c563be99b0d62
Archil Imnadze <archil@imnadze.ge>
2014-09-15 15:32:07 +0400

    use fnsDir var in Contacts\maxLengths

--- www/fns/Contacts/maxLengths.php
+++ www/fns/Contacts/maxLengths.php
@@ -3,10 +3,11 @@
 namespace Contacts;
 
 function maxLengths () {
-    include_once __DIR__.'/../Email/maxLength.php';
-    include_once __DIR__.'/../FullName/maxLength.php';
-    include_once __DIR__.'/../Tags/maxLength.php';
-    include_once __DIR__.'/../Username/maxLength.php';
+    $fnsDir = __DIR__.'/..';
+    include_once "$fnsDir/Email/maxLength.php";
+    include_once "$fnsDir/FullName/maxLength.php";
+    include_once "$fnsDir/Tags/maxLength.php";
+    include_once "$fnsDir/Username/maxLength.php";
     return [
         'address' => 128,
         'alias' => 32,
