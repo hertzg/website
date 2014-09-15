464aa3edbef23e893c171aa79b2cd81b1baea75d
Archil Imnadze <archil@imnadze.ge>
2014-09-15 18:29:38 +0400

    wrapped long line

--- www/fns/Users/Notifications/deleteAll.php
+++ www/fns/Users/Notifications/deleteAll.php
@@ -12,7 +12,8 @@ function deleteAll ($mysqli, $id_users) {
     include_once "$fnsDir/Channels/clearNumNotificationsOnUser.php";
     \Channels\clearNumNotificationsOnUser($mysqli, $id_users);
 
-    include_once "$fnsDir/SubscribedChannels/clearNumNotificationsOnSubscriber.php";
+    $path = "$fnsDir/SubscribedChannels/clearNumNotificationsOnSubscriber.php";
+    include_once $path;
     \SubscribedChannels\clearNumNotificationsOnSubscriber($mysqli, $id_users);
 
     $sql = 'update users set num_notifications = 0,'
