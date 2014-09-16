01640d1da00cc95bce0db1bf6188fdf2435a0718
Archil Imnadze <archil@imnadze.ge>
2014-09-16 13:59:56 +0400

    new item button in contacts

--- www/contacts/view/fns/create_content.php
+++ www/contacts/view/fns/create_content.php
@@ -10,12 +10,15 @@ function create_content ($contact, $infoText, $items) {
         $photoSrc = '../../images/empty-photo.svg';
     }
 
+    $fnsDir = __DIR__.'/../../../fns';
+
     include_once __DIR__.'/../fns/create_options_panel.php';
     include_once __DIR__.'/../fns/create_photo_options_panel.php';
-    include_once __DIR__.'/../../../fns/create_contact_panel.php';
-    include_once __DIR__.'/../../../fns/ItemList/listHref.php';
-    include_once __DIR__.'/../../../fns/Page/tabs.php';
-    include_once __DIR__.'/../../../fns/Page/sessionMessages.php';
+    include_once "$fnsDir/create_contact_panel.php";
+    include_once "$fnsDir/create_new_item_button.php";
+    include_once "$fnsDir/ItemList/listHref.php";
+    include_once "$fnsDir/Page/sessionMessages.php";
+    include_once "$fnsDir/Page/tabs.php";
     return
         Page\tabs(
             [
@@ -31,7 +34,8 @@ function create_content ($contact, $infoText, $items) {
             "Contact #$contact->id_contacts",
             Page\sessionMessages('contacts/view/messages')
             .create_contact_panel($photoSrc, join('<div class="hr"></div>', $items))
-            .$infoText
+            .$infoText,
+            create_new_item_button('Contact', '../')
         )
         .create_options_panel($contact)
         .create_photo_options_panel($contact);
