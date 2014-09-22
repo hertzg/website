07b5b03ee337645ed66f2865f2d036a90ce4360e
Archil Imnadze <archil@imnadze.ge>
2014-09-22 22:56:00 +0400

    js confirm in received tasks

---
+++ www/tasks/received/fns/create_page.php
@@ -0,0 +1,73 @@
+<?php
+
+function create_page ($mysqli, $user, $base = '') {
+
+    $id_users = $user->id_users;
+    $fnsDir = __DIR__.'/../../../fns';
+
+    include_once "$fnsDir/request_strings.php";
+    list($all) = request_strings('all');
+
+    if ($all) {
+        include_once "$fnsDir/ReceivedTasks/indexOnReceiver.php";
+        $receivedTasks = ReceivedTasks\indexOnReceiver($mysqli, $id_users);
+    } else {
+        include_once "$fnsDir/ReceivedTasks/indexNotArchivedOnReceiver.php";
+        $receivedTasks = ReceivedTasks\indexNotArchivedOnReceiver(
+            $mysqli, $id_users);
+    }
+
+    include_once "$fnsDir/create_sender_description.php";
+    include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
+
+    $items = [];
+
+    if ($receivedTasks) {
+        foreach ($receivedTasks as $receivedTask) {
+
+            if ($receivedTask->top_priority) $icon = 'task-top-priority';
+            else $icon = 'task';
+
+            $title = htmlspecialchars($receivedTask->text);
+            $description = create_sender_description($receivedTask);
+            $href = "{$base}view/?id=$receivedTask->id";
+            $items[] = Page\imageArrowLinkWithDescription($title,
+                $description, $href, $icon);
+
+        }
+    } else {
+        include_once "$fnsDir/Page/info.php";
+        $items[] = Page\info('No received tasks');
+    }
+
+    if (!$all && $user->num_archived_received_tasks) {
+        include_once "$fnsDir/Page/buttonLink.php";
+        $items[] = Page\buttonLink('Show Archived Tasks', '?all=1');
+    }
+
+    include_once "$fnsDir/Page/imageArrowLink.php";
+    $title = 'Delete All Tasks';
+    $deleteAllLink =
+        '<div id="deleteAllLink">'
+            .Page\imageArrowLink($title, "{$base}delete-all/", 'trash-bin')
+        .'</div>';
+
+    include_once "$fnsDir/create_new_item_button.php";
+    include_once "$fnsDir/create_panel.php";
+    include_once "$fnsDir/Page/sessionMessages.php";
+    include_once "$fnsDir/Page/tabs.php";
+    return Page\tabs(
+        [
+            [
+                'title' => 'Tasks',
+                'href' => "{$base}..",
+            ],
+        ],
+        'Received',
+        Page\sessionMessages('tasks/received/messages')
+        .join('<div class="hr"></div>', $items)
+        .create_panel('Options', $deleteAllLink),
+        create_new_item_button('Task', "{$base}../")
+    );
+
+}
--- www/tasks/received/index.php
+++ www/tasks/received/index.php
@@ -2,11 +2,13 @@
 
 include_once 'fns/require_received_tasks.php';
 $user = require_received_tasks();
-$id_users = $user->id_users;
 
-include_once '../../fns/Users/Tasks/Received/clearNumberNew.php';
+$base = '../../';
+$fnsDir = '../../fns';
+
+include_once "$fnsDir/Users/Tasks/Received/clearNumberNew.php";
 include_once '../../lib/mysqli.php';
-Users\Tasks\Received\clearNumberNew($mysqli, $id_users);
+Users\Tasks\Received\clearNumberNew($mysqli, $user->id_users);
 
 unset(
     $_SESSION['tasks/errors'],
@@ -14,67 +16,16 @@ unset(
     $_SESSION['tasks/received/view/messages']
 );
 
-include_once '../../fns/request_strings.php';
-list($all) = request_strings('all');
+include_once 'fns/create_page.php';
+include_once "$fnsDir/compressed_js_script.php";
+include_once '../../lib/mysqli.php';
+$content =
+    create_page($mysqli, $user)
+    .compressed_js_script('confirmDialog', $base)
+    .'<script type="text/javascript" src="index.js"></script>';
 
-if ($all) {
-    include_once '../../fns/ReceivedTasks/indexOnReceiver.php';
-    $receivedTasks = ReceivedTasks\indexOnReceiver($mysqli, $id_users);
-} else {
-    include_once '../../fns/ReceivedTasks/indexNotArchivedOnReceiver.php';
-    $receivedTasks = ReceivedTasks\indexNotArchivedOnReceiver(
-        $mysqli, $id_users);
-}
-
-include_once '../../fns/create_sender_description.php';
-include_once '../../fns/Page/imageArrowLinkWithDescription.php';
-
-$items = [];
-
-if ($receivedTasks) {
-    foreach ($receivedTasks as $receivedTask) {
-
-        if ($receivedTask->top_priority) $icon = 'task-top-priority';
-        else $icon = 'task';
-
-        $title = htmlspecialchars($receivedTask->text);
-        $description = create_sender_description($receivedTask);
-        $href = "view/?id=$receivedTask->id";
-        $items[] = Page\imageArrowLinkWithDescription($title,
-            $description, $href, $icon);
-
-    }
-} else {
-    include_once '../../fns/Page/info.php';
-    $items[] = Page\info('No received tasks');
-}
-
-if (!$all && $user->num_archived_received_tasks) {
-    include_once '../../fns/Page/buttonLink.php';
-    $items[] = Page\buttonLink('Show Archived Tasks', '?all=1');
-}
-
-include_once '../../fns/Page/imageArrowLink.php';
-$title = 'Delete All Tasks';
-$deleteAllLink = Page\imageArrowLink($title, 'delete-all/', 'trash-bin');
-
-include_once '../../fns/create_new_item_button.php';
-include_once '../../fns/create_panel.php';
-include_once '../../fns/Page/sessionMessages.php';
-include_once '../../fns/Page/tabs.php';
-$content = Page\tabs(
-    [
-        [
-            'title' => 'Tasks',
-            'href' => '..',
-        ],
-    ],
-    'Received',
-    Page\sessionMessages('tasks/received/messages')
-    .join('<div class="hr"></div>', $items)
-    .create_panel('Options', $deleteAllLink),
-    create_new_item_button('Task', '../')
-);
-
-include_once '../../fns/echo_page.php';
-echo_page($user, 'Received Tasks', $content, '../../');
+include_once "$fnsDir/compressed_css_link.php";
+include_once "$fnsDir/echo_page.php";
+echo_page($user, 'Received Tasks', $content, $base, [
+    'head' => compressed_css_link('confirmDialog', $base),
+]);
