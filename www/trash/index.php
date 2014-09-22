d3524d1338bb8ee9da2492c9bbdf5f1ab5b13eed
Archil Imnadze <archil@imnadze.ge>
2014-09-22 07:09:29 +0000

    call require_user in trash create_page function

--- www/trash/index.php
+++ www/trash/index.php
@@ -3,17 +3,12 @@
 $base = '../';
 $fnsDir = '../fns';
 
-include_once '../fns/require_user.php';
-$user = require_user($base);
-
-unset($_SESSION['home/messages']);
-
-include_once "$fnsDir/get_revision.php";
-$confirmDialogJsRevision = get_revision('js/confirmDialog.js');
-
 include_once 'fns/create_page.php';
 include_once '../lib/mysqli.php';
 $content = create_page($mysqli, $user);
+
+include_once "$fnsDir/get_revision.php";
+$confirmDialogJsRevision = get_revision('js/confirmDialog.js');
 
 if ($user->num_deleted_items) {
 
@@ -30,6 +25,8 @@ if ($user->num_deleted_items) {
 } else {
     $head = '';
 }
+
+unset($_SESSION['home/messages']);
 
 include_once "$fnsDir/compressed_css_link.php";
 include_once "$fnsDir/echo_page.php";
