36be1f555018bf805f6b8c899dce420c9c318ab8
Archil Imnadze <archil@imnadze.ge>
2014-09-24 12:59:53 +0400

    js confirm in files/send-file

--- www/fns/SendForm/renderRecipientsList.php
+++ www/fns/SendForm/renderRecipientsList.php
@@ -6,14 +6,18 @@ function renderRecipientsList ($recipients, $params, $base= '') {
     $html = '';
     include_once __DIR__.'/../Page/removableItem.php';
     foreach ($recipients as $recipient) {
-        $username = htmlspecialchars($recipient);
-        $href = "{$base}remove-recipient/?".htmlspecialchars(
+        $escapedUsername = htmlspecialchars($recipient);
+        $query = '?'.htmlspecialchars(
             http_build_query(
                 array_merge($params, ['username' => $recipient])
             )
         );
+        $href = "{$base}remove-recipient/$query";
         $html .=
-            \Page\removableItem($username, $href, 'user')
+            "<div class=\"deleteLinkWrapper\" data-query=\"$query\""
+            ." data-username=\"$escapedUsername\">"
+                .\Page\removableItem($escapedUsername, $href, 'user')
+            .'</div>'
             .'<div class="hr"></div>';
     }
     return $html;
---
+++ www/js/removeRecipient/removeRecipient.js
@@ -0,0 +1,39 @@
+(function () {
+
+    var escapeHtml = (function () {
+        var element = document.createElement('div')
+        return function (plain) {
+            element.appendChild(document.createTextNode(plain))
+            var escaped = element.innerHTML
+            element.removeChild(element.firstChild)
+            return escaped
+        }
+    })()
+
+    var dialogShown = false
+
+    var wrappers = document.querySelectorAll('.deleteLinkWrapper')
+    Array.prototype.forEach.call(wrappers, function (wrapper) {
+
+        var dataset = wrapper.dataset
+
+        var questionHtml =
+            'Are you sure you want to remove the recipient "<b>' +
+            escapeHtml(dataset.username) +'</b>"?'
+        var yesText = 'Yes, remove recipient'
+        var yesHref = 'remove-recipient/submit.php' + dataset.query
+
+        var deleteLink = wrapper.querySelector('.clickable.rightButton')
+        deleteLink.addEventListener('click', function (e) {
+            e.preventDefault()
+            deleteLink.blur()
+            if (dialogShown) return
+            dialogShown = true
+            confirmDialog(questionHtml, yesText, yesHref, function () {
+                dialogShown = false
+            })
+        })
+
+    })
+
+})()
