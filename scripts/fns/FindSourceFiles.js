a137b23ae15486feb0a93ae76a33d306aca31722
Archil Imnadze <archil@imnadze.ge>
2014-09-28 23:34:27 +0400

    exclude compressed files in quality check scripts

--- scripts/fns/FindSourceFiles.js
+++ scripts/fns/FindSourceFiles.js
@@ -6,15 +6,15 @@ function ForEachSourceFile (callback) {
 
             if (/^\./.test(file)) return
 
-            file = dir + '/' + file
+            var fullFile = dir + '/' + file
 
-            var stat = fs.statSync(file)
-            if (stat.isDirectory()) scan(file)
+            var stat = fs.statSync(fullFile)
+            if (stat.isDirectory()) scan(fullFile)
             else if (/\.(css|js|php|sh)$/.test(file) &&
-                !(/\.(combined|compressed)\.(css|js)$/.test(file))) {
+                !(/^(combined|compressed)\.(css|js)$/.test(file))) {
 
-                var content = fs.readFileSync(file, 'utf8')
-                var fileSubstr = file.substr(rootDir.length + 1)
+                var content = fs.readFileSync(fullFile, 'utf8')
+                var fileSubstr = fullFile.substr(rootDir.length + 1)
                 if (callback(fileSubstr, content)) foundFiles.push(fileSubstr)
 
             }
