854da4ddbfd58d7c17e62a244387d9a88760f13e
Archil Imnadze <archil@imnadze.ge>
2014-09-29 16:29:10 +0400

    do not query database if offset is more than total

--- www/fns/Notifications/indexPageOnSubscribedChannel.php
+++ www/fns/Notifications/indexPageOnSubscribedChannel.php
@@ -5,16 +5,20 @@ namespace Notifications;
 function indexPageOnSubscribedChannel ($mysqli,
     $id_subscribed_channels, $offset, $limit, &$total) {
 
+    $fnsDir = __DIR__.'/..';
+
     $fromWhere = 'from notifications'
         ." where id_subscribed_channels = $id_subscribed_channels";
 
     $sql = "select count(*) total $fromWhere";
-    include_once __DIR__.'/../mysqli_single_object.php';
+    include_once "$fnsDir/mysqli_single_object.php";
     $total = mysqli_single_object($mysqli, $sql)->total;
+
+    if ($offset >= $total) return [];
 
     $sql = "select * $fromWhere order by insert_time"
         ." limit $limit offset $offset";
-    include_once __DIR__.'/../mysqli_query_object.php';
+    include_once "$fnsDir/mysqli_query_object.php";
     return mysqli_query_object($mysqli, $sql);
 
 }
--- www/fns/Notifications/indexPageOnUser.php
+++ www/fns/Notifications/indexPageOnUser.php
@@ -4,15 +4,19 @@ namespace Notifications;
 
 function indexPageOnUser ($mysqli, $id_users, $offset, $limit, &$total) {
 
+    $fnsDir = __DIR__.'/..';
+
     $fromWhere = "from notifications where id_users = $id_users";
 
     $sql = "select count(*) total $fromWhere";
-    include_once __DIR__.'/../mysqli_single_object.php';
+    include_once "$fnsDir/mysqli_single_object.php";
     $total = mysqli_single_object($mysqli, $sql)->total;
+
+    if ($offset >= $total) return [];
 
     $sql = "select * $fromWhere order by insert_time desc"
         ." limit $limit offset $offset";
-    include_once __DIR__.'/../mysqli_query_object.php';
+    include_once "$fnsDir/mysqli_query_object.php";
     return mysqli_query_object($mysqli, $sql);
 
 }
