03c96672eeea5c25455868bfe06ca30a86110f18
Archil Imnadze <archil@imnadze.ge>
2014-10-07 11:04:32 +0000

    check for invalid username before adding a user to channel

--- www/notifications/channels/users/add/submit.php
+++ www/notifications/channels/users/add/submit.php
@@ -8,41 +8,18 @@ require_same_domain_referer('../../..');
 include_once '../../fns/require_channel.php';
 include_once '../../../../lib/mysqli.php';
 list($channel, $id, $user) = require_channel($mysqli);
-$id_users = $user->id_users;
 
 include_once "$fnsDir/request_strings.php";
 list($subscriber_username) = request_strings('subscriber_username');
 
+include_once "$fnsDir/str_collapse_spaces.php";
+$subscriber_username = str_collapse_spaces($subscriber_username);
+
 $errors = [];
 
-if ($subscriber_username === '') {
-    $errors[] = 'Enter username.';
-} else {
-    include_once "$fnsDir/Users/getByUsername.php";
-    $subscriberUser = Users\getByUsername($mysqli, $subscriber_username);
-    if (!$subscriberUser) {
-        $errors[] = "A user with the username doesn't exist.";
-    } else {
-        $subscriber_id_users = $subscriberUser->id_users;
-        if ($subscriber_id_users == $id_users) {
-            $errors[] = "You don't have to add yourself in the list.";
-        } else {
-            include_once "$fnsDir/SubscribedChannels/getExistingSubscriber.php";
-            $subscribedChannel = SubscribedChannels\getExistingSubscriber(
-                $mysqli, $id, $subscriber_id_users);
-            if ($subscribedChannel && $subscribedChannel->publisher_locked) {
-                $errors[] = 'The user is already added.';
-            } else {
-                include_once "$fnsDir/get_users_connection.php";
-                $connection = get_users_connection(
-                    $mysqli, $subscriberUser, $id_users);
-                if (!$connection['can_send_channel']) {
-                    $errors[] = "The user isn't receiving channels from you.";
-                }
-            }
-        }
-    }
-}
+include_once 'fns/check_username.php';
+check_username($mysqli, $id, $user->id_users,
+    $subscriber_username, $subscribedChannel, $subscriberUser, $errors);
 
 include_once "$fnsDir/redirect.php";
 
