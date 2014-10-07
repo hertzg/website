7820c1a96899bff90e76164fdf5be052c33abc80
Archil Imnadze <archil@imnadze.ge>
2014-10-07 11:42:35 +0000

    check for invalid usernames in api methods

--- www/api-call/fns/request_receiver_user.php
+++ www/api-call/fns/request_receiver_user.php
@@ -2,7 +2,9 @@
 
 function request_receiver_user ($mysqli, $id_users, $permission) {
 
-    include_once __DIR__.'/../../fns/request_strings.php';
+    $fnsDir = __DIR__.'/../../fns';
+
+    include_once "$fnsDir/request_strings.php";
     list($receiver_username) = request_strings('receiver_username');
 
     if ($receiver_username === '') {
@@ -10,7 +12,13 @@ function request_receiver_user ($mysqli, $id_users, $permission) {
         bad_request('ENTER_RECEIVER_USERNAME');
     }
 
-    include_once __DIR__.'/../../fns/Users/getByUsername.php';
+    include_once "$fnsDir/Username/isValid.php";
+    if (!Username\isValid($receiver_username)) {
+        include_once __DIR__.'/bad_request.php';
+        bad_request('RECEIVER_NOT_FOUND');
+    }
+
+    include_once "$fnsDir/Users/getByUsername.php";
     $receiverUser = Users\getByUsername($mysqli, $receiver_username);
 
     if (!$receiverUser) {
@@ -18,7 +26,7 @@ function request_receiver_user ($mysqli, $id_users, $permission) {
         bad_request('RECEIVER_NOT_FOUND');
     }
 
-    include_once __DIR__.'/../../fns/get_users_connection.php';
+    include_once "$fnsDir/get_users_connection.php";
     $connection = get_users_connection($mysqli, $receiverUser, $id_users);
     if (!$connection[$permission]) {
         include_once __DIR__.'/bad_request.php';
