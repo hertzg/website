dc7101a8d20a34b8e5f3efd82ae5bc073a6abcaf
Archil Imnadze <archil@imnadze.ge>
2014-10-07 09:42:41 +0400

    removed using listHref for /bookmarks/ page

--- www/bookmarks/received/fns/create_page.php
+++ www/bookmarks/received/fns/create_page.php
@@ -57,14 +57,13 @@ function create_page ($mysqli, $user, $base = '') {
 
     include_once "$fnsDir/create_new_item_button.php";
     include_once "$fnsDir/create_panel.php";
-    include_once "$fnsDir/ItemList/Received/listHref.php";
     include_once "$fnsDir/Page/sessionMessages.php";
     include_once "$fnsDir/Page/tabs.php";
     return Page\tabs(
         [
             [
                 'title' => 'Bookmarks',
-                'href' => "$base../".ItemList\Received\listHref(),
+                'href' => "$base..",
             ],
         ],
         'Received',
