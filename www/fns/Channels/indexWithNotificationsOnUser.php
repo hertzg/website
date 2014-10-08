66d30274e5a50465b9fb78daf85d2caf4baf0c8d
Archil Imnadze <archil@imnadze.ge>
2014-10-08 19:54:11 +0400

    fixed sql statements

--- www/fns/Channels/indexWithNotificationsOnUser.php
+++ www/fns/Channels/indexWithNotificationsOnUser.php
@@ -4,7 +4,7 @@ namespace Channels;
 
 function indexWithNotificationsOnUser ($mysqli, $id_users) {
     $sql = "select * from channels where id_users = $id_users"
-        ." and num_notifications > 1 order by lowercase_name";
+        ." and num_notifications > 0 order by lowercase_name";
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
--- www/fns/SubscribedChannels/indexWithNotificationsOnSubscriber.php
+++ www/fns/SubscribedChannels/indexWithNotificationsOnSubscriber.php
@@ -5,7 +5,7 @@ namespace SubscribedChannels;
 function indexWithNotificationsOnSubscriber ($mysqli, $subscriber_id_users) {
     $sql = 'select * from subscribed_channels'
         ." where subscriber_id_users = $subscriber_id_users"
-        .' and num_notifications > 1 order by lowercase_name';
+        .' and num_notifications > 0 order by lowercase_name';
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
