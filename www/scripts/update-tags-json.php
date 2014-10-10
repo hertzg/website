ce11d4cac2d2241e7849b1151a7191b11c908ceb
Archil Imnadze <archil@imnadze.ge>
2014-10-10 10:27:33 +0000

    script to recalculate tags_json

---
+++ www/scripts/update-tags-json.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+
+function process ($mysqli, $table) {
+    $id_column = "id_$table";
+    $rows = mysqli_query_object($mysqli, "select * from $table");
+    foreach ($rows as $row) {
+        $tag_names = Tags\parse($row->tags);
+        $tags_json = $mysqli->real_escape_string(json_encode($tag_names));
+        $id = $row->$id_column;
+        $sql = "update $table set tags_json = '$tags_json' where $id_column = $id";
+        $mysqli->query($sql) || trigger_error($mysqli->error);
+    }
+}
+
+chdir(__DIR__);
+include_once '../../lib/cli.php';
+include_once '../fns/mysqli_query_object.php';
+include_once '../fns/mysqli_single_object.php';
+include_once '../fns/Tags/parse.php';
+include_once '../lib/mysqli.php';
+
+$microtime = microtime(true);
+
+process($mysqli, 'bookmarks');
+process($mysqli, 'contacts');
+process($mysqli, 'notes');
+process($mysqli, 'tasks');
+
+$elapsedSeconds = number_format(microtime(true) - $microtime, 3);
+echo "Done in $elapsedSeconds seconds.\n";
