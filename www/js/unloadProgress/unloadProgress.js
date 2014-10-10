fbc32caee40117197134448ebdae5dd7d0f32a50
Archil Imnadze <archil@imnadze.ge>
2014-10-10 08:51:25 +0000

    unload overlay

--- www/js/unloadProgress/unloadProgress.js
+++ www/js/unloadProgress/unloadProgress.js
@@ -7,21 +7,33 @@
         if (unloaded) return
         unloaded = true
 
-        var div = document.createElement('div')
+        var progressDiv = document.createElement('div')
         ;(function (style) {
-            style.position = 'absolute'
-            style.top = style.right = style.left = '0'
-            style.height = '2px'
+            style.position = 'fixed'
+            style.right = style.bottom = style.left = '0'
+            style.height = '4px'
             style.backgroundColor = '#aaa'
             style.backgroundImage = 'url(' + base + 'images/progress.svg)'
             style.backgroundPosition = '50% 0'
-        })(div.style)
+        })(progressDiv.style)
 
-        document.body.appendChild(div)
+        var overlayDiv = document.createElement('div')
+        ;(function (style) {
+            style.position = 'fixed'
+            style.top = style.right = style.bottom = style.left = '0'
+            style.background = 'rgba(0, 0, 0, 0.5)'
+        })(overlayDiv.style)
+        overlayDiv.addEventListener('click', function () {
+            body.removeChild(overlayDiv)
+        })
+
+        var body = document.body
+        body.appendChild(overlayDiv)
+        body.appendChild(progressDiv)
 
         var x = 0
         setInterval(function () {
-            div.style.backgroundPosition = 'calc(50% + ' + x + 'px) 0'
+            progressDiv.style.backgroundPosition = 'calc(50% + ' + x + 'px) 0'
             x = (x + 1) % 8
         }, 50)
 
