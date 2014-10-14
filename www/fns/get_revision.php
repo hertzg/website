0244d21f99f2dbb5785c6e972421234159ea5344
Archil Imnadze <archil@imnadze.ge>
2014-10-15 00:13:13 +0400

    increased common css revision

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = [
-        'css/common/compressed.css' => 60,
+        'css/common/compressed.css' => 61,
         'css/confirmDialog/compressed.css' => 1,
         'css/contact/compressed.css' => 1,
         'css/icons/compressed.css' => 57,
