bcb0448d65cc1748158817309b938c8a5d4532e1
Archil Imnadze <archil@imnadze.ge>
2014-10-15 12:21:47 +0000

    script to check few miscongurations

---
+++ www/scripts/check-installation.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+
+function visual_assert ($ok, $text) {
+    $ok = $ok ? 'OK' : 'NOT OK';
+    echo str_pad($text, 30, ' ')." $ok\n";
+}
+
+chdir(__DIR__);
+
+$ok = date_default_timezone_get() === 'UTC';
+visual_assert($ok, 'Default Timezone UTC');
+
+$ok = function_exists('curl_init');
+visual_assert($ok, 'PHP Client URL Library');
+
+$ok = function_exists('imagecreatefromstring');
+visual_assert($ok, 'PHP Image Processing and GD');
+
+$ok = function_exists('mysqli_connect');
+visual_assert($ok, 'PHP MySQL Improved Extension');
+
+include_once '../fns/Users/Directory/dir.php';
+$dir =  Users\Directory\dir();
+
+$ok = is_dir($dir);
+visual_assert($ok, "Data directory exists");
+
+$file = "$dir/test";
+$ok = file_put_contents($file, 'test');
+if ($ok) unlink($file);
+visual_assert($ok, 'Data directory writable');
