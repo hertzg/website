739c814497c2ba9b50dba8b3524a9c9cab3f6ecc
Archil Imnadze <archil@imnadze.ge>
2014-10-15 08:17:41 +0000

    removed ">" from removableItem css

--- www/fns/Page/removableItem.php
+++ www/fns/Page/removableItem.php
@@ -5,7 +5,7 @@ namespace Page;
 function removableItem ($text, $removeHref, $icon) {
     return
         '<div class="removableItem">'
-            ."<div class=\"icon $icon\"></div>"
+            ."<div class=\"removableItem-icon icon $icon\"></div>"
             ."<div class=\"removableItem-text\">$text</div>"
             ."<a class=\"clickable rightButton\" href=\"$removeHref\">"
                 .'<span class="rightButton-icon icon no"></span>'
