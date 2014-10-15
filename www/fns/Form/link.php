5b19f998f6bf3082387504e379c820ae6221bcf9
Archil Imnadze <archil@imnadze.ge>
2014-10-15 09:20:44 +0000

    use Page\imageLink in Form\link

--- www/fns/Form/link.php
+++ www/fns/Form/link.php
@@ -4,11 +4,9 @@ namespace Form;
 
 function link ($text, $title, $href, $icon) {
     include_once __DIR__.'/association.php';
+    include_once __DIR__.'/../Page/imageLink.php';
     return association(
-        "<a class=\"clickable link form-link\" href=\"$href\">"
-            ."<span class=\"icon $icon\"></span>"
-            .$title
-        .'</a>',
+        \Page\imageLink($title, $href, $icon),
         "<label>$text:</label>"
     );
 }
