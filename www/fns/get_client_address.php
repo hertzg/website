0186d0fadfe64c76852d7b63beff6d078856e7d7
Archil Imnadze <archil@imnadze.ge>
2014-10-16 08:49:15 +0000

    fixed: Only variables should be passed by reference in www/fns/get_client_address.php

--- www/fns/get_client_address.php
+++ www/fns/get_client_address.php
@@ -3,7 +3,8 @@
 function get_client_address () {
 
 //    // uncomment when using the site behind a reverse proxy
-//    return array_pop(explode(', ', $_SERVER['HTTP_X_FORWARDED_FOR']));
+//    $addresses = explode(', ', $_SERVER['HTTP_X_FORWARDED_FOR']);
+//    return array_pop($addresses);
 
     return $_SERVER['REMOTE_ADDR'];
 
