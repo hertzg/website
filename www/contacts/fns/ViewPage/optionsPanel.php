cd1d0d9844477af60855157f2c84ff5feba83674
Archil Imnadze <archil@imnadze.ge>
2014-10-17 21:17:46 +0400

    fixed: use imageLink for contact download link

--- www/contacts/fns/ViewPage/optionsPanel.php
+++ www/contacts/fns/ViewPage/optionsPanel.php
@@ -10,18 +10,18 @@ function optionsPanel ($contact, $base) {
     include_once "$fnsDir/ItemList/escapedItemQuery.php";
     $escapedItemQuery = \ItemList\escapedItemQuery($id_contacts);
 
-    include_once "$fnsDir/Page/imageArrowLink.php";
+    include_once "$fnsDir/Page/imageLink.php";
     $safe_name = str_replace('/', '_', $contact->full_name);
     $href = "$base../download/$id_contacts/$safe_name.vcf";
-    $downloadLink = \Page\imageArrowLink('Download', $href, 'download');
+    $downloadLink = \Page\imageLink('Download', $href, 'download');
 
+    include_once "$fnsDir/Page/imageArrowLink.php";
     $href = "$base../edit/$escapedItemQuery";
     $editLink = \Page\imageArrowLink('Edit', $href, 'edit-contact');
 
     $href = "$base../send/$escapedItemQuery";
     $sendLink = \Page\imageArrowLink('Send', $href, 'send');
 
-    include_once "$fnsDir/Page/imageLink.php";
     include_once __DIR__.'/../contact_sms_text.php';
     $href = 'sms:?body='.rawurlencode(contact_sms_text($contact));
     $sendViaSmsLink = \Page\imageLink('Send via SMS', $href, 'send-sms');
