027e47a4878ef1f8a4d1ffd673197e098b64f5bf
Archil Imnadze <archil@imnadze.ge>
2014-10-24 12:38:56 +0000

    split large file contacts/received/fns/ViewPage/create

--- www/contacts/received/fns/ViewPage/create.php
+++ www/contacts/received/fns/ViewPage/create.php
@@ -67,34 +67,7 @@ function create ($receivedContact) {
         .'</div>'
     );
 
-    $contactContent = join('<div class="hr"></div>', $items);
-
-    $photo_id = $receivedContact->photo_id;
-    if ($photo_id) $photoSrc = "../download-photo/?id=$id";
-    else $photoSrc = '../../../images/empty-photo.svg';
-
-    include_once "$fnsDir/create_contact_panel.php";
-    $contactPanel = create_contact_panel($photoSrc, $contactContent);
-
-    include_once __DIR__.'/optionsPanel.php';
-    include_once "$fnsDir/create_panel.php";
-    include_once "$fnsDir/ItemList/Received/listHref.php";
-    include_once "$fnsDir/Page/sessionMessages.php";
-    include_once "$fnsDir/Page/tabs.php";
-    return \Page\tabs(
-        [
-            [
-                'title' => 'Received',
-                'href' => '../'.\ItemList\Received\listHref(),
-            ],
-        ],
-        "Received Contact #$id",
-        \Page\sessionMessages('contacts/received/view/messages')
-        .\Form\label('Received from',
-            htmlspecialchars($receivedContact->sender_username))
-        .create_panel('The Contact', $contactPanel)
-        .$infoText
-        .optionsPanel($receivedContact)
-    );
+    include_once __DIR__.'/createContent.php';
+    return createContent($receivedContact, $infoText, $items);
 
 }
