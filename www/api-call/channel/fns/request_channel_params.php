fa726a0b13816eb66f2cd188aa899451648127f7
Archil Imnadze <archil@imnadze.ge>
2014-10-25 13:05:09 +0400

    use common function for validating channel name

--- www/api-call/channel/fns/request_channel_params.php
+++ www/api-call/channel/fns/request_channel_params.php
@@ -2,36 +2,35 @@
 
 function request_channel_params ($mysqli, $exclude_id = 0) {
 
-    include_once __DIR__.'/../../../fns/Channels/request.php';
+    $fnsDir = __DIR__.'/../../../fns';
+
+    include_once "$fnsDir/Channels/request.php";
     list($channel_name, $public, $receive_notifications) = Channels\request();
 
     if ($channel_name === '') {
         include_once __DIR__.'/../../fns/bad_request.php';
         bad_request('ENTER_CHANNEL_NAME');
     }
-    if (preg_match('/[^a-z0-9._-]/ui', $channel_name)) {
+
+    include_once "$fnsDir/ChannelName/containsIllegalChars.php";
+    if (ChannelName\containsIllegalChars($channel_name)) {
         include_once __DIR__.'/../../fns/bad_request.php';
         bad_request('INVALID_CHANNEL_NAME');
     }
 
-    $length = strlen($channel_name);
-
-    include_once __DIR__.'/../../../fns/ChannelName/minLength.php';
-    $minLength = ChannelName\minLength();
-
-    if ($length < $minLength) {
+    include_once "$fnsDir/ChannelName/isShort.php";
+    if (ChannelName\isShort($channel_name)) {
         include_once __DIR__.'/../../fns/bad_request.php';
         bad_request('CHANNEL_NAME_TOO_SHORT');
     }
 
-    include_once __DIR__.'/../../../fns/ChannelName/maxLength.php';
-    $maxLength = ChannelName\maxLength();
-
-    if ($length > $maxLength) {
+    include_once "$fnsDir/ChannelName/isLong.php";
+    if (ChannelName\isLong($channel_name)) {
         include_once __DIR__.'/../../fns/bad_request.php';
         bad_request('CHANNEL_NAME_TOO_LONG');
     }
-    include_once __DIR__.'/../../../fns/Channels/getByName.php';
+
+    include_once "$fnsDir/Channels/getByName.php";
     if (Channels\getByName($mysqli, $channel_name, $exclude_id)) {
         include_once __DIR__.'/../../fns/bad_request.php';
         bad_request('CHANNEL_ALREADY_EXISTS');
