b226e1dc310e15433e08af2889f3106f2eab5265
Archil Imnadze <archil@imnadze.ge>
2014-10-26 12:07:08 +0400

    add num_tags, tags and tags_json columns to schedules table

--- www/fns/Schedules/maxLengths.php
+++ www/fns/Schedules/maxLengths.php
@@ -3,5 +3,12 @@
 namespace Schedules;
 
 function maxLengths () {
-    return ['text' => 1024];
+    $fnsDir = __DIR__.'/..';
+    include_once "$fnsDir/Tags/maxLength.php";
+    include_once "$fnsDir/TagsJson/maxLength.php";
+    return [
+        'tags' => \Tags\maxLength(),
+        'tags_json' => \TagsJson\maxLength(),
+        'text' => 1024,
+    ];
 }
--- www/scripts/ensure/table/schedules.php
+++ www/scripts/ensure/table/schedules.php
@@ -22,11 +22,24 @@ ensure_table('schedules', [
     'interval' => [
         'type' => 'bigint(20) unsigned',
     ],
+    'num_tags' => [
+        'type' => 'tinyint(3) unsigned',
+    ],
     'offset' => [
         'type' => 'bigint(20) unsigned',
     ],
     'revision' => [
         'type' => 'bigint(20) unsigned',
+    ],
+    'tags' => [
+        'type' => "varchar($maxLengths[tags])",
+        'characterSet' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    ],
+    'tags_json' => [
+        'type' => "varchar($maxLengths[tags_json])",
+        'characterSet' => 'ascii',
+        'collation' => 'ascii_bin',
     ],
     'text' => [
         'type' => "varchar($maxLengths[text])",
