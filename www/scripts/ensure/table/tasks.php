b72316174524d28a577320eef5c61812ada7f499
Archil Imnadze <archil@imnadze.ge>
2014-10-28 06:18:52 +0000

    add api key names to tasks table

--- www/scripts/ensure/table/tasks.php
+++ www/scripts/ensure/table/tasks.php
@@ -20,6 +20,16 @@ ensure_table('tasks', [
     'id_users' => [
         'type' => 'bigint(20) unsigned',
     ],
+    'insert_api_key_id' => [
+        'type' => 'bigint(20) unsigned',
+        'nullable' => true,
+    ],
+    'insert_api_key_name' => [
+        'type' => "varchar($maxLengths[insert_api_key_name])",
+        'nullable' => true,
+        'characterSet' => 'utf8',
+        'collation' => 'utf8_general_ci',
+    ],
     'insert_time' => [
         'type' => 'bigint(20) unsigned',
     ],
@@ -46,6 +56,16 @@ ensure_table('tasks', [
     ],
     'top_priority' => [
         'type' => 'tinyint(4) unsigned',
+    ],
+    'update_api_key_id' => [
+        'type' => 'bigint(20) unsigned',
+        'nullable' => true,
+    ],
+    'update_api_key_name' => [
+        'type' => "varchar($maxLengths[update_api_key_name])",
+        'nullable' => true,
+        'characterSet' => 'utf8',
+        'collation' => 'utf8_general_ci',
     ],
     'update_time' => [
         'type' => 'bigint(20) unsigned',
