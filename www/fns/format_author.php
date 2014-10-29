2c74b6c7e452f708ed75c276cb08f631a36cccff
Archil Imnadze <archil@imnadze.ge>
2014-10-29 08:08:20 +0000

    do not bold author name

--- www/fns/format_author.php
+++ www/fns/format_author.php
@@ -4,7 +4,8 @@ function format_author ($time, $api_key_name) {
     include_once __DIR__.'/date_ago.php';
     $html = date_ago($time);
     if ($api_key_name !== null) {
-        $html .= ' with "<b>'.htmlspecialchars($api_key_name).'</b>"';
+        $escapedName = htmlspecialchars($api_key_name);
+        $html .= " with \"<b class=\"author\">$escapedName</b>\"";
     }
     return $html;
 }
