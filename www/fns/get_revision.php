6337216394e0184caecf612ea0c27acb9a65d015
Archil Imnadze <archil@imnadze.ge>
2014-11-02 18:40:04 +0400

    backward compatibility of @font-face

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = [
-        'css/common/compressed.css' => 70,
+        'css/common/compressed.css' => 71,
         'css/confirmDialog/compressed.css' => 1,
         'css/contact/compressed.css' => 2,
         'css/icons/compressed.css' => 59,
