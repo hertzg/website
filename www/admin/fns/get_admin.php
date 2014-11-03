2ff4f4474a31415405183dbc833078492ff65b04
Archil Imnadze <archil@imnadze.ge>
2014-11-03 09:50:49 +0000

    default admin password

--- www/admin/fns/get_admin.php
+++ www/admin/fns/get_admin.php
@@ -1,7 +1,7 @@
 <?php
 
 function get_admin (&$username, &$hash, &$salt) {
-    $username = 'admin1';
-    $hash = 'c1f357e8feedf7f96b08ded4193c9fa0';
-    $salt = '5573db135ca1e56f0a6c896948c156cc069ba394176a2d3fdb6670e7284935ca';
+    $username = 'admin';
+    $hash = '5cff78d33ccb3f503b51041162536651';
+    $salt = '9855f6723335752c7cb102f4077e52def86b515c86b277d9f4da1041d612adfd';
 }
