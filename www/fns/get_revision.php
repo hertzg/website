6e91c2439bb49ea45c287a547cf374004f6667eb
Archil Imnadze <archil@imnadze.ge>
2014-11-04 11:15:00 +0000

    flexTextarea script

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -9,6 +9,7 @@ function get_revision ($key) {
         'css/index/compressed.css' => 1,
         'js/batteryAndClock/compressed.js' => 1,
         'js/confirmDialog/compressed.js' => 5,
+        'js/flexTextarea/compressed.js' => 1,
         'js/formCheckbox/compressed.js' => 1,
         'js/imageProgress/compressed.js' => 2,
         'js/lineSizeRounding/compressed.js' => 2,
---
+++ www/js/flexTextarea/flexTextarea.js
@@ -0,0 +1,29 @@
+(function () {
+    var nodeList = document.querySelectorAll('.form-textarea')
+    Array.prototype.forEach.call(nodeList, function (textarea) {
+
+        function update () {
+            invisibleTextarea.value = textarea.value
+            invisibleTextarea.style.width = textarea.offsetWidth + 'px'
+            textarea.style.height = invisibleTextarea.scrollHeight +
+                textarea.offsetHeight - textarea.clientHeight + 'px'
+        }
+
+        var invisibleTextarea = document.createElement('textarea')
+        invisibleTextarea.className = textarea.className
+        invisibleTextarea.style.overflow = 'hidden'
+        invisibleTextarea.value = textarea.value
+
+        var wrapperElement = document.createElement('div')
+        wrapperElement.style.overflow = 'hidden'
+        wrapperElement.style.height = '0'
+        wrapperElement.style.visibility = 'hidden'
+        wrapperElement.appendChild(invisibleTextarea)
+
+        document.body.appendChild(wrapperElement)
+        textarea.addEventListener('input', update)
+        addEventListener('resize', update)
+        update()
+
+    })
+})()
