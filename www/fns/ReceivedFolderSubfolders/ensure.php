a441e7dd7c6766d8aa401cc24bbae9ba843055f3
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:39:22 +0000

    ensure received_folder_subfolders

---
+++ www/fns/ReceivedFolderSubfolders/ensure.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ReceivedFolderSubfolders;
+
+function ensure ($mysqli) {
+
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/Folders/maxLengths.php";
+    $maxLengths = \Folders\maxLengths();
+
+    include_once "$fnsDir/Table/ensure.php";
+    return \Table\ensure($mysqli, 'received_folder_subfolders', [
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_received_folders' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'name' => [
+            'type' => "varchar($maxLengths[name])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'parent_id' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/received_folder_subfolders.php
+++ www/scripts/ensure/table/received_folder_subfolders.php
@@ -3,28 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Folders/maxLengths.php';
-$maxLengths = Folders\maxLengths();
-
-include_once 'fns/ensure_table.php';
-ensure_table('received_folder_subfolders', [
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_received_folders' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'name' => [
-        'type' => "varchar($maxLengths[name])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'parent_id' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
+include_once '../../../fns/ReceivedFolderSubfolders/ensure.php';
+echo ReceivedFolderSubfolders\ensure($mysqli);
