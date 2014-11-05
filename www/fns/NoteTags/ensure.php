5b147d1f298e7fdf23f81630d7b39ffe29436d45
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:26:34 +0000

    ensure note_tags

---
+++ www/fns/NoteTags/ensure.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace NoteTags;
+
+function ensure ($mysqli) {
+
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/Notes/maxLengths.php";
+    $maxLengths = \Notes\maxLengths();
+
+    include_once "$fnsDir/Table/ensure.php";
+    include_once "$fnsDir/Tag/maxLength.php";
+    return \Table\ensure($mysqli, 'note_tags', [
+        'encrypt' => [
+            'type' => 'tinyint(3) unsigned',
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_notes' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'tag_name' => [
+            'type' => 'varchar('.\Tag\maxLength().')',
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'text' => [
+            'type' => "varchar($maxLengths[text])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'update_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/note_tags.php
+++ www/scripts/ensure/table/note_tags.php
@@ -3,40 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Notes/maxLengths.php';
-$maxLengths = Notes\maxLengths();
-
-include_once 'fns/ensure_table.php';
-include_once '../../../fns/Tag/maxLength.php';
-ensure_table('note_tags', [
-    'encrypt' => [
-        'type' => 'tinyint(3) unsigned',
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_notes' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'tag_name' => [
-        'type' => 'varchar('.Tag\maxLength().')',
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'text' => [
-        'type' => "varchar($maxLengths[text])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'update_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
+include_once '../../../fns/NoteTags/ensure.php';
+echo NoteTags\ensure($mysqli);
