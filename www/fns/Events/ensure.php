3e0da7d6ef1d3fc09e2a94a16912eda50811c5fa
Archil Imnadze <archil@imnadze.ge>
2014-11-05 08:45:44 +0000

    ensure events

---
+++ www/fns/Events/ensure.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Events;
+
+function ensure ($mysqli) {
+
+    include_once __DIR__.'/maxLengths.php';
+    $maxLengths = maxLengths();
+
+    include_once __DIR__.'/../Table/ensure.php';
+    return \Table\ensure($mysqli, 'events', [
+        'event_time' => [
+            'type' => 'bigint(20)',
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'insert_api_key_id' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'insert_api_key_name' => [
+            'type' => "varchar($maxLengths[insert_api_key_name])",
+            'nullable' => true,
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'revision' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'text' => [
+            'type' => "varchar($maxLengths[text])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'update_api_key_id' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'update_api_key_name' => [
+            'type' => "varchar($maxLengths[update_api_key_name])",
+            'nullable' => true,
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+        'update_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/events.php
+++ www/scripts/ensure/table/events.php
@@ -3,54 +3,8 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Events/maxLengths.php';
-$maxLengths = Events\maxLengths();
+include_once '../../../fns/Events/ensure.php';
+echo Events\ensure($mysqli);
 
-include_once 'fns/ensure_table.php';
-ensure_table('events', [
-    'event_time' => [
-        'type' => 'bigint(20)',
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_api_key_id' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'insert_api_key_name' => [
-        'type' => "varchar($maxLengths[insert_api_key_name])",
-        'nullable' => true,
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'revision' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'text' => [
-        'type' => "varchar($maxLengths[text])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'update_api_key_id' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'update_api_key_name' => [
-        'type' => "varchar($maxLengths[update_api_key_name])",
-        'nullable' => true,
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'update_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
