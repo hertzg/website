57d9d8481765951c8e8d1bbd05b69ed364b6e84d
Archil Imnadze <archil@imnadze.ge>
2014-11-05 08:28:32 +0000

    ensure received_folder_files

---
+++ www/fns/ReceivedFolderFiles/ensure.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ReceivedFolderFiles;
+
+function ensure ($mysqli) {
+
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/Files/maxLengths.php";
+    $maxLengths = \Files\maxLengths();
+
+    include_once "$fnsDir/Table/ensure.php";
+    return \Table\ensure($mysqli, 'received_folder_files', [
+        'content_type' => [
+            'type' => "varchar($maxLengths[content_type])",
+            'characterSet' => 'ascii',
+            'collation' => 'ascii_bin',
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_received_folders' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'media_type' => [
+            'type' => "enum('archive','audio','image','text','unknown','video')",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+        'name' => [
+            'type' => "varchar($maxLengths[name])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'parent_id' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'size' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/received_folder_files.php
+++ www/scripts/ensure/table/received_folder_files.php
@@ -3,41 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Files/maxLengths.php';
-$maxLengths = Files\maxLengths();
-
-include_once 'fns/ensure_table.php';
-ensure_table('received_folder_files', [
-    'content_type' => [
-        'type' => "varchar($maxLengths[content_type])",
-        'characterSet' => 'ascii',
-        'collation' => 'ascii_bin',
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_received_folders' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'media_type' => [
-        'type' => "enum('archive','audio','image','text','unknown','video')",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'name' => [
-        'type' => "varchar($maxLengths[name])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'parent_id' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'size' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
+include_once '../../../fns/ReceivedFolderFiles/ensure.php';
+echo \ReceivedFolderFiles\ensure($mysqli);
