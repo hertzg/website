432172436589972e44fc04c7d420c899f7361435
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:35:31 +0000

    ensure task_tags

---
+++ www/fns/TaskTags/ensure.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace TaskTags;
+
+function ensure ($mysqli) {
+
+    $fnsDir = '../../../fns';
+
+    include_once "$fnsDir/Tasks/maxLengths.php";
+    $maxLengths = \Tasks\maxLengths();
+
+    include_once "$fnsDir/Table/ensure.php";
+    include_once "$fnsDir/Tag/maxLength.php";
+    return \Table\ensure($mysqli, 'task_tags', [
+        'deadline_time' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_tasks' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'tags' => [
+            'type' => "varchar($maxLengths[tags])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'tag_name' => [
+            'type' => 'varchar('.\Tag\maxLength().')',
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'text' => [
+            'type' => "varchar($maxLengths[text])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'top_priority' => [
+            'type' => 'tinyint(4) unsigned',
+        ],
+        'update_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/task_tags.php
+++ www/scripts/ensure/table/task_tags.php
@@ -3,49 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Tasks/maxLengths.php';
-$maxLengths = Tasks\maxLengths();
-
-include_once 'fns/ensure_table.php';
-include_once '../../../fns/Tag/maxLength.php';
-ensure_table('task_tags', [
-    'deadline_time' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_tasks' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'tags' => [
-        'type' => "varchar($maxLengths[tags])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'tag_name' => [
-        'type' => 'varchar('.Tag\maxLength().')',
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'text' => [
-        'type' => "varchar($maxLengths[text])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'top_priority' => [
-        'type' => 'tinyint(4) unsigned',
-    ],
-    'update_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
+include_once '../../../fns/TaskTags/ensure.php';
+echo \TaskTags\ensure($mysqli);
