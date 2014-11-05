664dab547bf5a4a107c4b287eb7caa2e5a18260f
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:23:18 +0000

    ensure folders

---
+++ www/fns/Folders/ensure.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Folders;
+
+function ensure ($mysqli) {
+
+    include_once __DIR__.'/maxLengths.php';
+    $maxLengths = maxLengths();
+
+    include_once __DIR__.'/../Table/ensure.php';
+    return \Table\ensure($mysqli, 'folders', [
+        'id_folders' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'insert_api_key_id' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'insert_api_key_name' => [
+            'type' => "varchar($maxLengths[insert_api_key_name])",
+            'nullable' => true,
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'name' => [
+            'type' => "varchar($maxLengths[name])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'parent_id_folders' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'rename_api_key_id' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'rename_api_key_name' => [
+            'type' => "varchar($maxLengths[rename_api_key_name])",
+            'nullable' => true,
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+        'rename_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/folders.php
+++ www/scripts/ensure/table/folders.php
@@ -3,51 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Folders/maxLengths.php';
-$maxLengths = Folders\maxLengths();
-
-include_once 'fns/ensure_table.php';
-ensure_table('folders', [
-    'id_folders' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_api_key_id' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'insert_api_key_name' => [
-        'type' => "varchar($maxLengths[insert_api_key_name])",
-        'nullable' => true,
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'name' => [
-        'type' => "varchar($maxLengths[name])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'parent_id_folders' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'rename_api_key_id' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'rename_api_key_name' => [
-        'type' => "varchar($maxLengths[rename_api_key_name])",
-        'nullable' => true,
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'rename_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
+include_once '../../../fns/Folders/ensure.php';
+echo Folders\ensure($mysqli);
