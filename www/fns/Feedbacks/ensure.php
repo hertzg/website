e20a1b195000bbba63e14d5d30be5d357a46372b
Archil Imnadze <archil@imnadze.ge>
2014-11-05 08:42:56 +0000

    ensure feedbacks

---
+++ www/fns/Feedbacks/ensure.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Feedbacks;
+
+function ensure ($mysqli) {
+
+    include_once __DIR__.'/maxLengths.php';
+    $maxLengths = maxLengths();
+
+    include_once __DIR__.'/../Table/ensure.php';
+    return \Table\ensure($mysqli, 'feedbacks', [
+        'idfeedbacks' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'text' => [
+            'type' => "varchar($maxLengths[text])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/feedbacks.php
+++ www/scripts/ensure/table/feedbacks.php
@@ -3,26 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Feedbacks/maxLengths.php';
-$maxLengths = Feedbacks\maxLengths();
-
-include_once 'fns/ensure_table.php';
-ensure_table('feedbacks', [
-    'idfeedbacks' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'text' => [
-        'type' => "varchar($maxLengths[text])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-]);
+include_once '../../../fns/Feedbacks/ensure.php';
+echo Feedbacks\ensure($mysqli);
