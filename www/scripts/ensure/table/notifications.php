cdd82fcb5003c5e5ad0c04310e7f9f5c433484c5
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:32:21 +0000

    ensure notifications

--- www/scripts/ensure/table/notifications.php
+++ www/scripts/ensure/table/notifications.php
@@ -3,48 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Notifications/maxLengths.php';
-$maxLengths = Notifications\maxLengths();
-
-include_once 'fns/ensure_table.php';
-ensure_table('notifications', [
-    'channel_name' => [
-        'type' => "varchar($maxLengths[channel_name])",
-        'characterSet' => 'ascii',
-        'collation' => 'ascii_general_ci',
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_channels' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'id_subscribed_channels' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_api_key_id' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'insert_api_key_name' => [
-        'type' => "varchar($maxLengths[insert_api_key_name])",
-        'nullable' => true,
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'text' => [
-        'type' => "varchar($maxLengths[text])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-]);
+include_once '../../../fns/Notifications/ensure.php';
+echo Notifications\ensure($mysqli);
