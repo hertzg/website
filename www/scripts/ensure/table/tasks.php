a03c78c5aa67fb623cc6477488d7a150c9414cf8
Archil Imnadze <archil@imnadze.ge>
2014-11-05 08:14:56 +0000

    ensure tasks

--- www/scripts/ensure/table/tasks.php
+++ www/scripts/ensure/table/tasks.php
@@ -3,71 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Tasks/maxLengths.php';
-$maxLengths = Tasks\maxLengths();
-
-include_once 'fns/ensure_table.php';
-ensure_table('tasks', [
-    'deadline_time' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_api_key_id' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'insert_api_key_name' => [
-        'type' => "varchar($maxLengths[insert_api_key_name])",
-        'nullable' => true,
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'num_tags' => [
-        'type' => 'tinyint(3) unsigned',
-    ],
-    'revision' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'tags' => [
-        'type' => "varchar($maxLengths[tags])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'tags_json' => [
-        'type' => "varchar($maxLengths[tags_json])",
-        'characterSet' => 'ascii',
-        'collation' => 'ascii_bin',
-    ],
-    'text' => [
-        'type' => "varchar($maxLengths[text])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'top_priority' => [
-        'type' => 'tinyint(4) unsigned',
-    ],
-    'update_api_key_id' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'update_api_key_name' => [
-        'type' => "varchar($maxLengths[update_api_key_name])",
-        'nullable' => true,
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_general_ci',
-    ],
-    'update_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
+include_once '../../../fns/Tasks/ensure.php';
+echo Tasks\ensure($mysqli);
