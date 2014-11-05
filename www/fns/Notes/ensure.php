c5827d41f8e01e98ed6b6c1478dc1adf740c2821
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:48:26 +0000

    ensure notes

---
+++ www/fns/Notes/ensure.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Notes;
+
+function ensure ($mysqli) {
+
+    include_once __DIR__.'/maxLengths.php';
+    $maxLengths = maxLengths();
+
+    include_once __DIR__.'/../Table/ensure.php';
+    return \Table\ensure($mysqli, 'notes', [
+        'encrypt' => [
+            'type' => 'tinyint(3) unsigned',
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'insert_api_key_id' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'insert_api_key_name' => [
+            'type' => "varchar($maxLengths[insert_api_key_name])",
+            'nullable' => true,
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'num_tags' => [
+            'type' => 'tinyint(3) unsigned',
+        ],
+        'revision' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'tags' => [
+            'type' => "varchar($maxLengths[tags])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'tags_json' => [
+            'type' => "varchar($maxLengths[tags_json])",
+            'characterSet' => 'ascii',
+            'collation' => 'ascii_bin',
+        ],
+        'text' => [
+            'type' => "varchar($maxLengths[text])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'update_api_key_id' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'update_api_key_name' => [
+            'type' => "varchar($maxLengths[update_api_key_name])",
+            'nullable' => true,
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+        'update_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
