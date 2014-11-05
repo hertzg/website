1dc9a5375653d8b574e635b9e470115ef1038ad6
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:57:06 +0000

    ensure deleted_folders

---
+++ www/fns/DeletedFolders/ensure.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DeletedFolders;
+
+function ensure ($mysqli) {
+
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/Folders/maxLengths.php";
+    $maxLengths = \Folders\maxLengths();
+
+    include_once "$fnsDir/Table/ensure.php";
+    return \Table\ensure($mysqli, 'deleted_folders', [
+        'id_deleted_items' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'id_folders' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'name' => [
+            'type' => "varchar($maxLengths[name])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'parent_id_folders' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'rename_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/deleted_folders.php
+++ www/scripts/ensure/table/deleted_folders.php
@@ -3,34 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Folders/maxLengths.php';
-$maxLengths = Folders\maxLengths();
-
-include_once 'fns/ensure_table.php';
-ensure_table('deleted_folders', [
-    'id_deleted_items' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'id_folders' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'name' => [
-        'type' => "varchar($maxLengths[name])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'parent_id_folders' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'rename_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-]);
+include_once '../../../fns/DeletedFolders/ensure.php';
+echo DeletedFolders\ensure($mysqli);
