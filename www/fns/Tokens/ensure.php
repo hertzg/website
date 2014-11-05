d3f3d705ac0890ec6ed2b74de12d28226c173fcf
Archil Imnadze <archil@imnadze.ge>
2014-11-05 08:08:53 +0000

    ensure tokens

---
+++ www/fns/Tokens/ensure.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tokens;
+
+
+function ensure ($mysqli) {
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/Table/ensure.php";
+    include_once "$fnsDir/Username/maxLength.php";
+    return \Table\ensure($mysqli, 'tokens', [
+        'access_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'token_text' => [
+            'type' => 'binary(16)',
+        ],
+        'username' => [
+            'type' => 'varchar('.\Username\maxLength().')',
+            'characterSet' => 'ascii',
+            'collation' => 'ascii_bin',
+        ],
+        'user_agent' => [
+            'type' => 'varchar(1024)',
+            'nullable' => true,
+            'characterSet' => 'ascii',
+            'collation' => 'ascii_bin',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/tokens.php
+++ www/scripts/ensure/table/tokens.php
@@ -3,35 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once 'fns/ensure_table.php';
-include_once '../../../fns/Username/maxLength.php';
-ensure_table('tokens', [
-    'access_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'token_text' => [
-        'type' => 'binary(16)',
-    ],
-    'username' => [
-        'type' => 'varchar('.Username\maxLength().')',
-        'characterSet' => 'ascii',
-        'collation' => 'ascii_bin',
-    ],
-    'user_agent' => [
-        'type' => 'varchar(1024)',
-        'nullable' => true,
-        'characterSet' => 'ascii',
-        'collation' => 'ascii_bin',
-    ],
-]);
+include_once '../../../fns/Tokens/ensure.php';
+echo \Tokens\ensure($mysqli);
