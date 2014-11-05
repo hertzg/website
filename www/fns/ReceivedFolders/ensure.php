c1ae590686f576db2e2215e2074c80cd7224f3f7
Archil Imnadze <archil@imnadze.ge>
2014-11-05 08:34:58 +0000

    ensure received_folders

---
+++ www/fns/ReceivedFolders/ensure.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ReceivedFolders;
+
+function ensure ($mysqli) {
+
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/Folders/maxLengths.php";
+    $maxLengths = \Folders\maxLengths();
+
+    include_once "$fnsDir/Table/ensure.php";
+    include_once "$fnsDir/Username/maxLength.php";
+    return \Table\ensure($mysqli, 'received_folders', [
+        'archived' => [
+            'type' => 'tinyint(3) unsigned',
+        ],
+        'committed' => [
+            'type' => 'tinyint(4)',
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'name' => [
+            'type' => "varchar($maxLengths[name])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'receiver_id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'sender_id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'sender_username' => [
+            'type' => 'varchar('.\Username\maxLength().')',
+            'characterSet' => 'ascii',
+            'collation' => 'ascii_bin',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/received_folders.php
+++ www/scripts/ensure/table/received_folders.php
@@ -3,40 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Folders/maxLengths.php';
-$maxLengths = Folders\maxLengths();
-
-include_once 'fns/ensure_table.php';
-include_once '../../../fns/Username/maxLength.php';
-ensure_table('received_folders', [
-    'archived' => [
-        'type' => 'tinyint(3) unsigned',
-    ],
-    'committed' => [
-        'type' => 'tinyint(4)',
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'name' => [
-        'type' => "varchar($maxLengths[name])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'receiver_id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'sender_id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'sender_username' => [
-        'type' => 'varchar('.Username\maxLength().')',
-        'characterSet' => 'ascii',
-        'collation' => 'ascii_bin',
-    ],
-]);
+include_once '../../../fns/ReceivedFolders/ensure.php';
+echo ReceivedFolders\ensure($mysqli);
