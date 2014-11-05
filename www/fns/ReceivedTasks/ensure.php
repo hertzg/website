3f580cffddc478d5981cafaa3a67948849e08310
Archil Imnadze <archil@imnadze.ge>
2014-11-05 07:45:12 +0000

    ensure received_tasks

---
+++ www/fns/ReceivedTasks/ensure.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ReceivedTasks;
+
+function ensure ($mysqli) {
+
+    $fnsDir = __DIR__.'/..';
+
+    include_once "$fnsDir/Tasks/maxLengths.php";
+    $maxLengths = \Tasks\maxLengths();
+
+    include_once "$fnsDir/Table/ensure.php";
+    include_once "$fnsDir/Username/maxLength.php";
+    return \Table\ensure($mysqli, 'received_tasks', [
+        'archived' => [
+            'type' => 'tinyint(3) unsigned',
+        ],
+        'deadline_time' => [
+            'type' => 'bigint(20) unsigned',
+            'nullable' => true,
+        ],
+        'id' => [
+            'type' => 'bigint(20) unsigned',
+            'primary' => true,
+        ],
+        'insert_time' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'receiver_id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'sender_id_users' => [
+            'type' => 'bigint(20) unsigned',
+        ],
+        'sender_username' => [
+            'type' => 'varchar('.\Username\maxLength().')',
+            'characterSet' => 'ascii',
+            'collation' => 'ascii_bin',
+        ],
+        'tags' => [
+            'type' => "varchar($maxLengths[tags])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'text' => [
+            'type' => "varchar($maxLengths[text])",
+            'characterSet' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ],
+        'top_priority' => [
+            'type' => 'tinyint(3) unsigned',
+        ],
+    ]);
+
+}
--- www/scripts/ensure/table/received_tasks.php
+++ www/scripts/ensure/table/received_tasks.php
@@ -3,49 +3,7 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../lib/mysqli.php';
 
-include_once '../../../fns/Tasks/maxLengths.php';
-$maxLengths = Tasks\maxLengths();
-
-include_once 'fns/ensure_table.php';
-include_once '../../../fns/Username/maxLength.php';
-ensure_table('received_tasks', [
-    'archived' => [
-        'type' => 'tinyint(3) unsigned',
-    ],
-    'deadline_time' => [
-        'type' => 'bigint(20) unsigned',
-        'nullable' => true,
-    ],
-    'id' => [
-        'type' => 'bigint(20) unsigned',
-        'primary' => true,
-    ],
-    'insert_time' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'receiver_id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'sender_id_users' => [
-        'type' => 'bigint(20) unsigned',
-    ],
-    'sender_username' => [
-        'type' => 'varchar('.Username\maxLength().')',
-        'characterSet' => 'ascii',
-        'collation' => 'ascii_bin',
-    ],
-    'tags' => [
-        'type' => "varchar($maxLengths[tags])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'text' => [
-        'type' => "varchar($maxLengths[text])",
-        'characterSet' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'top_priority' => [
-        'type' => 'tinyint(3) unsigned',
-    ],
-]);
+include_once '../../../fns/ReceivedTasks/ensure.php';
+echo ReceivedTasks\ensure($mysqli);
