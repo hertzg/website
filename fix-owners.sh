b07bbd7837e856cfba848c755fe0b4dbfc0c7d79
Archil Imnadze <archil@imnadze.ge>
2014-11-07 11:09:55 +0000

    script to fix ownerships of configuration files + default domain name localhost

---
+++ fix-owners.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+owner=www-data:www-data
+chown $owner www
+chown $owner www/admin/fns/get_admin.php
+chown $owner www/fns/get_domain_name.php
+chown $owner www/fns/get_mysqli_config.php
+chown $owner www/fns/get_site_base.php
+chown $owner www/fns/get_site_protocol.php
+chown $owner www/fns/installed.php
