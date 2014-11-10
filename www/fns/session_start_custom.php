19c8327a62f0dbbf4a3e8953658c8a291d6dc8d5
Archil Imnadze <archil@imnadze.ge>
2014-11-10 09:12:37 +0000

    set path to session cookies

--- www/fns/session_start_custom.php
+++ www/fns/session_start_custom.php
@@ -1,6 +1,9 @@
 <?php
 
 function session_start_custom () {
+    $lifetime = session_get_cookie_params()['lifetime'];
+    include_once __DIR__.'/SiteBase/get.php';
+    session_set_cookie_params($lifetime, SiteBase\get());
     session_name('zsid');
     session_start();
 }
