19040343e0b1a03d9a3206c960c28150af23098e
Archil Imnadze <archil@imnadze.ge>
2014-11-10 13:37:40 +0000

    check more installation problems in "Check Installation" admin section

---
+++ www/admin/check-installation/fns/assert.php
@@ -0,0 +1,58 @@
+<?php
+
+function assert_success ($text) {
+    return "<div class=\"status ok\">$text</div>";
+}
+
+function assert_enabled ($ok, $subject) {
+    if ($ok) return assert_success("$subject is enabled.");
+    return assert_failure("$subject is NOT enabled.");
+}
+
+function assert_failure ($text) {
+    return "<div class=\"status not_ok\">$text</div>";
+}
+
+function assert_file ($path) {
+    $subject = "\"$path\"";
+    if (is_file($path)) return assert_success("$subject is a file.");
+    return assert_failure("$subject is NOT a file.");
+}
+
+function assert_folder ($path) {
+    $subject = "\"$path\"";
+    if (is_dir($path)) return assert_success("$subject is a folder.");
+    return assert_failure("$subject is NOT a folder.");
+}
+
+function assert_installed ($ok, $subject) {
+    if ($ok) return assert_success("$subject is installed.");
+    return assert_failure("$subject is NOT installed.");
+}
+
+function assert_readable ($path) {
+    $subject = "\"$path\"";
+    if (is_readable($path)) return assert_success("$subject is readable.");
+    return assert_failure("$subject is NOT readable.");
+}
+
+function assert_readable_file ($path) {
+    $path = realpath($path);
+    return assert_file($path).assert_writable($path);
+}
+
+function assert_writable ($path) {
+    $subject = "\"$path\"";
+    if (is_writable($path)) return assert_success("$subject is writable.");
+    return assert_failure("$subject is NOT writable.");
+}
+
+function assert_writable_file ($path) {
+    $path = realpath($path);
+    return assert_file($path).assert_readable($path).assert_writable($path);
+}
+
+function assert_writable_folder ($path) {
+    $path = realpath($path);
+    return assert_folder($path).assert_writable($path);
+}
---
+++ www/admin/check-installation/fns/check_user_data.php
@@ -0,0 +1,58 @@
+<?php
+
+function check_user_data ($mysqli) {
+
+    $fnsDir = __DIR__.'/../../../fns';
+    include_once __DIR__.'/assert.php';
+
+    $content = '';
+
+    include_once "$fnsDir/mysqli_query_object.php";
+    $users = mysqli_query_object($mysqli, 'select * from users');
+    if ($users) {
+        include_once "$fnsDir/Files/File/dir.php";
+        include_once "$fnsDir/ReceivedFiles/File/dir.php";
+        include_once "$fnsDir/ReceivedFolderFiles/File/dir.php";
+        foreach ($users as $user) {
+            $id_users = $user->id_users;
+            $content .=
+                assert_writable_folder(Files\File\dir($id_users))
+                .assert_writable_folder(ReceivedFiles\File\dir($id_users))
+                .assert_writable_folder(ReceivedFolderFiles\File\dir($id_users));
+        }
+    }
+
+    $files = mysqli_query_object($mysqli, 'select * from files');
+    if ($files) {
+        include_once "$fnsDir/Files/File/path.php";
+        foreach ($files as $file) {
+            $path = Files\File\path($file->id_users, $file->id_files);
+            $content .= assert_readable_file($path);
+        }
+    }
+
+    $receivedFiles = mysqli_query_object($mysqli, 'select * from received_files');
+    if ($receivedFiles) {
+        include_once "$fnsDir/ReceivedFiles/File/path.php";
+        foreach ($receivedFiles as $receivedFile) {
+            $receiver_id_users = $receivedFile->receiver_id_users;
+            $path = ReceivedFiles\File\path($receiver_id_users, $receivedFile->id);
+            $content .= assert_readable_file($path);
+        }
+    }
+
+    $sql = 'select * from received_folder_files';
+    $receivedFolderFiles = mysqli_query_object($mysqli, $sql);
+    if ($receivedFolderFiles) {
+        include_once "$fnsDir/ReceivedFolderFiles/File/path.php";
+        foreach ($receivedFolderFiles as $receivedFolderFile) {
+            $id_users = $receivedFolderFile->id_users;
+            $id = $receivedFolderFile->id;
+            $path = ReceivedFolderFiles\File\path($id_users, $id);
+            $content .= assert_readable_file($path);
+        }
+    }
+
+    return $content;
+
+}
