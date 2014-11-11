3744046a4dfdb0fe63003b07ba0c2639f3c91ab5
Archil Imnadze <archil@imnadze.ge>
2014-11-11 07:34:53 +0000

    extend wizard layout height

--- www/install/fns/echo_html.php
+++ www/install/fns/echo_html.php
@@ -14,7 +14,7 @@ function echo_html ($title, $content) {
                 .'<link rel="icon" type="image/png"
                 href="../icons/32.png" sizes="32x32" />'
                 .'<link rel="stylesheet" type="text/css"'
-                .' href="../css/compressed.css?3" />'
+                .' href="../css/compressed.css?4" />'
             .'</head>'
             .'<body>'
                 .'<div class="backgroundGradient">'
