66eaa619084ad77f55fa487523253b15a0ae5c43
Archil Imnadze <archil@imnadze.ge>
2014-11-15 17:19:45 +0400

    script to recompute birthday days and months of contacts

---
+++ www/scripts/update-birthdays.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+
+chdir(__DIR__);
+include_once '../../lib/cli.php';
+include_once '../fns/mysqli_query_object.php';
+include_once '../lib/mysqli.php';
+
+$microtime = microtime(true);
+
+$sql = 'select * from contacts where birthday_time is not null';
+$contacts = mysqli_query_object($mysqli, $sql);
+foreach ($contacts as $contact) {
+    $birthday_time = $contact->birthday_time;
+    $birthday_day = date('j', $birthday_time);
+    $birthday_month = date('n', $birthday_time);
+    $sql = "update contacts set birthday_day = $birthday_day,"
+        ." birthday_month = $birthday_month where id = $contact->id";
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+}
+
+$elapsedSeconds = number_format(microtime(true) - $microtime, 3);
+echo "Done in $elapsedSeconds seconds.\n";
