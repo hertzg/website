600990847bec1a1bb17e3117db3fd3bb1bd79e85
Archil Imnadze <archil@imnadze.ge>
2014-12-16 07:29:54 +0000

    use Session\remember function in account/tokens/submit-remember

--- www/account/tokens/submit-remember.php
+++ www/account/tokens/submit-remember.php
@@ -13,27 +13,8 @@ include_once '../../lib/mysqli.php';
 $token = request_valid_token($mysqli);
 
 if (!$token) {
-
-    $token_text = md5(uniqid(), true);
-
-    $key = 'HTTP_USER_AGENT';
-    if (array_key_exists($key, $_SERVER)) $user_agent = $_SERVER[$key];
-    else $user_agent = null;
-
-    include_once "$fnsDir/Users/Tokens/add.php";
-    $id = Users\Tokens\add($mysqli, $user->id_users,
-        $user->username, $token_text, $user_agent);
-
-    include_once "$fnsDir/Tokens/get.php";
-    $token = Tokens\get($mysqli, $id);
-
-    if ($token) {
-        $expires = time() + 60 * 60 * 24 * 30;
-        include_once "$fnsDir/Cookie/set.php";
-        Cookie\set('token', bin2hex($token_text));
-        $_SESSION['token'] = $token;
-    }
-
+    include_once "$fnsDir/Session/remember.php";
+    Session\remember($mysqli, $user);
 }
 
 unset($_SESSION['account/tokens/errors']);
