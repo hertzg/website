62124666128664d7157fe6b4ace334d1a7594871
Archil Imnadze <archil@imnadze.ge>
2014-12-24 10:00:55 +0000

    more space for clock

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = [
-        'css/common/compressed.css' => 78,
+        'css/common/compressed.css' => 79,
         'css/confirmDialog/compressed.css' => 1,
         'css/contact/compressed.css' => 2,
         'css/icons/compressed.css' => 59,
