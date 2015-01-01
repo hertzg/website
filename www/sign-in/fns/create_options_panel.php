8ce75bcd7fcbc7191805762512e8d9de898573f0
Archil Imnadze <archil@imnadze.ge>
2015-01-01 16:36:34 +0400

    imageArrowLink to email-reset-password

--- www/sign-in/fns/create_options_panel.php
+++ www/sign-in/fns/create_options_panel.php
@@ -2,14 +2,16 @@
 
 function create_options_panel () {
 
-    include_once __DIR__.'/../../fns/Page/imageLinkWithDescription.php';
+    $fnsDir = __DIR__.'/../../fns';
 
     $options = [];
 
-    $options[] = Page\imageLinkWithDescription('Forgot password?',
+    include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
+    $options[] = Page\imageArrowLinkWithDescription('Forgot password?',
         'Reset your account password here.', '../email-reset-password/',
         'reset-password', ['id' => 'email-reset-password']);
 
+    include_once "$fnsDir/Page/imageLinkWithDescription.php";
     $options[] = Page\imageLinkWithDescription('Don\'t have an account?',
         'Sign up here.', '../sign-up/', 'new-password');
 
