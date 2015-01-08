8668f067e32640a2c5a824004bc38210a178ea1a
Archil Imnadze <archil@imnadze.ge>
2015-01-08 16:23:21 +0000

    update titles of deleted notes and tasks

--- www/scripts/update-titles.php
+++ www/scripts/update-titles.php
@@ -1,14 +1,36 @@
 #!/usr/bin/php
 <?php
 
-function process ($mysqli, $table, $maxLengths) {
+function process ($mysqli, $table, $length) {
     $rows = mysqli_query_object($mysqli, "select * from $table");
     foreach ($rows as $row) {
-        $title = text_title($row->text, $maxLengths['title']);
+        $title = text_title($row->text, $length);
         $title = $mysqli->real_escape_string($title);
         $sql = "update $table set title = '$title' where id = $row->id";
         $mysqli->query($sql) || trigger_error($mysqli->error);
     }
+}
+
+function process_deleted ($mysqli, $types, $length) {
+
+    $types = array_map(function ($type) {
+        return "'$type'";
+    }, $types);
+
+    $sql = 'select * from deleted_items'
+        .' where data_type in ('.join(',', $types).')';
+    $rows = mysqli_query_object($mysqli, $sql);
+
+    foreach ($rows as $row) {
+        $data_json = json_decode($row->data_json);
+        $title = text_title($data_json->text, $length);
+        $data_json = json_encode($data_json);
+        $data_json = $mysqli->real_escape_string($data_json);
+        $sql = "update $table set data_json = '$data_json'"
+            ." where id = $row->id";
+        $mysqli->query($sql) || trigger_error($mysqli->error);
+    }
+
 }
 
 chdir(__DIR__);
@@ -20,16 +42,18 @@ include_once '../lib/mysqli.php';
 $microtime = microtime(true);
 
 include_once '../fns/Notes/maxLengths.php';
-$maxLengths = Notes\maxLengths();
-process($mysqli, 'notes', $maxLengths);
-process($mysqli, 'note_tags', $maxLengths);
-process($mysqli, 'received_notes', $maxLengths);
+$length = Notes\maxLengths()['title'];
+process($mysqli, 'notes', $length);
+process($mysqli, 'note_tags', $length);
+process($mysqli, 'received_notes', $length);
+process_deleted($mysqli, ['note', 'receivedNote'], $length);
 
 include_once '../fns/Tasks/maxLengths.php';
-$maxLengths = Tasks\maxLengths();
-process($mysqli, 'tasks', $maxLengths);
-process($mysqli, 'task_tags', $maxLengths);
-process($mysqli, 'received_tasks', $maxLengths);
+$length = Tasks\maxLengths()['title'];
+process($mysqli, 'tasks', $length);
+process($mysqli, 'task_tags', $length);
+process($mysqli, 'received_tasks', $length);
+process_deleted($mysqli, ['task', 'receivedTask'], $length);
 
 $elapsedSeconds = number_format(microtime(true) - $microtime, 3);
 echo "Done in $elapsedSeconds seconds.\n";
