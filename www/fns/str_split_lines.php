d9e0db556de40c7582320c9694f6fcb5fbaf75a4
Archil Imnadze <archil@imnadze.ge>
2015-01-08 11:34:41 +0000

    str_split_lines limit param and test cases

--- www/fns/str_split_lines.php
+++ www/fns/str_split_lines.php
@@ -1,9 +1,7 @@
 <?php
 
-function str_split_lines ($string) {
-    $lines = preg_split("/\r\n|\r|\n/", $string);
-    if (count($lines) == 1 && !$lines[0]) {
-        return [];
-    }
+function str_split_lines ($string, $limit = -1) {
+    $lines = preg_split("/\r\n|\r|\n/", $string, $limit);
+    if (count($lines) == 1 && $lines[0] === '' && $limit != 2) return [];
     return $lines;
 }
---
+++ www/tests/str_split_lines.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+
+include_once __DIR__.'/../fns/str_split_lines.php';
+assert(str_split_lines("") === []);
+assert(str_split_lines("\n") === ['', '']);
+assert(str_split_lines("\n", 1) === ["\n"]);
+assert(str_split_lines("\n", 2) === ['', '']);
+assert(str_split_lines("\n\n") === ['', '', '']);
+assert(str_split_lines("\n\n", 1) === ["\n\n"]);
+assert(str_split_lines("\n\n", 2) === ['', "\n"]);
+assert(str_split_lines(" \n ") === [' ', ' ']);
