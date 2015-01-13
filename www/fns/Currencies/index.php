05e7b6b5a79304332d26947d787e81fe9acbcfc6
Archil Imnadze <archil@imnadze.ge>
2015-01-13 14:11:29 +0000

    list of popular currencies

---
+++ www/fns/Currencies/index.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Currencies;
+
+function index () {
+    return [
+        [
+            'code' => 'AUD',
+            'name' => 'Australian dollar',
+            'base' => 100,
+        ],
+        [
+            'code' => 'CAD',
+            'name' => 'Canadian dollar',
+            'base' => 100,
+        ],
+        [
+            'code' => 'CHF',
+            'name' => 'Swiss franc',
+            'base' => 100,
+        ],
+        [
+            'code' => 'CNY',
+            'name' => 'Chinese yuan',
+            'base' => 100,
+        ],
+        [
+            'code' => 'DKK',
+            'name' => 'Danish krone',
+            'base' => 100,
+        ],
+        [
+            'code' => 'EUR',
+            'name' => 'Euro',
+            'base' => 100,
+        ],
+        [
+            'code' => 'GBP',
+            'name' => 'Pound sterling',
+            'base' => 100,
+        ],
+        [
+            'code' => 'GEL',
+            'name' => 'Georgian lari',
+            'base' => 100,
+        ],
+        [
+            'code' => 'HKD',
+            'name' => 'Hong Kong dollar',
+            'base' => 100,
+        ],
+        [
+            'code' => 'NOK',
+            'name' => 'Norwegian krone',
+            'base' => 100,
+        ],
+        [
+            'code' => 'NZD',
+            'name' => 'New Zealand dollar',
+            'base' => 100,
+        ],
+        [
+            'code' => 'PLN',
+            'name' => 'Polish złoty',
+            'base' => 100,
+        ],
+        [
+            'code' => 'SGD',
+            'name' => 'Singapore dollar',
+            'base' => 100,
+        ],
+        [
+            'code' => 'SEK',
+            'name' => 'Swedish krona',
+            'base' => 100,
+        ],
+        [
+            'code' => 'THB',
+            'name' => 'Thai baht',
+            'base' => 100,
+        ],
+        [
+            'code' => 'USD',
+            'name' => 'United States dollar',
+            'base' => 100,
+        ],
+        [
+            'code' => 'ZAR',
+            'name' => 'South African rand',
+            'base' => 100,
+        ],
+    ];
+}
