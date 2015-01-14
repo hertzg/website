7c7d4b2e6545b9c4bd659cfb9f27ed052e037809
Archil Imnadze <archil@imnadze.ge>
2015-01-14 11:44:42 +0000

    script to set content_revision to 0 to deleted files

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -37,5 +37,20 @@ process($mysqli, 'contact');
 process($mysqli, 'note');
 process($mysqli, 'task');
 
+$sql = "select * from deleted_items where data_type = 'file'";
+$rows = mysqli_query_object($mysqli, $sql);
+
+foreach ($rows as $row) {
+    $data_json = json_decode($row->data_json);
+    if (!property_exists($data_json, 'content_revision')) {
+        $data_json->content_revision = 0;
+        $data_json = json_encode($data_json);
+        $data_json = $mysqli->real_escape_string($data_json);
+        $sql = "update deleted_items set data_json = '$data_json'"
+            ." where id = $row->id";
+        $mysqli->query($sql) || trigger_error($mysqli->error);
+    }
+}
+
 $elapsedSeconds = number_format(microtime(true) - $microtime, 3);
 echo "Done in $elapsedSeconds seconds.\n";
