c8438bcb506fca917ee9af7b9cda543a5aaec35d
Archil Imnadze <archil@imnadze.ge>
2015-01-19 22:40:06 +0400

    script to set access_times of old accounts

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -5,29 +5,12 @@ chdir(__DIR__);
 include_once '../../lib/cli.php';
 include_once '../lib/mysqli.php';
 
-$sql = 'update users set show_wallets = 1';
+$sql = 'update users set access_time = last_login_time'
+    .' where last_login_time <> 0';
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
-include_once '../fns/mysqli_query_object.php';
-$users = mysqli_query_object($mysqli, 'select * from users');
-
-foreach ($users as $user) {
-
-    $order_home_items = json_decode($user->order_home_items);
-
-    $index = array_search('wallets', $order_home_items);
-    if ($index !== false) array_splice($order_home_items, $index, 1);
-
-    $index = array_search('new-wallet', $order_home_items);
-    if ($index !== false) array_splice($order_home_items, $index, 1);
-
-    $index = array_search('schedules', $order_home_items);
-    array_splice($order_home_items, $index + 1, 0, ['wallets', 'new-wallet']);
-
-    $order_home_items = json_encode($order_home_items);
-    $order_home_items = $mysqli->real_escape_string($order_home_items);
-    $sql = "update users set order_home_items = '$order_home_items'"
-        ." where id_users = $user->id_users";
-    $mysqli->query($sql) || trigger_error($mysqli->error);
-
-}
+include_once '../fns/Users/emailExpireDays.php';
+$access_time = time() - Users\emailExpireDays() * 24 * 60 * 60;
+$sql = "update users set access_time = $access_time"
+    .' where access_time is null';
+$mysqli->query($sql) || trigger_error($mysqli->error);
