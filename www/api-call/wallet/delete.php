b47658870f768f1847775ac438df96a524ab9aa7
Archil Imnadze <archil@imnadze.ge>
2015-01-21 09:53:08 +0000

    wallet/delete and wallet/deleteAll methods

---
+++ www/api-call/wallet/delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once '../fns/require_api_key.php';
+list($apiKey, $user, $mysqli) = require_api_key('can_write_wallets');
+
+include_once 'fns/require_wallet.php';
+$wallet = require_wallet($mysqli, $user->id_users);
+
+include_once '../../fns/Users/Wallets/delete.php';
+Users\Wallets\delete($mysqli, $wallet);
+
+header('Content-Type: application/json');
+echo 'true';
