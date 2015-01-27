3d239a0bb2724d0c601e49a1c4fb569f5c0d154e
Archil Imnadze <archil@imnadze.ge>
2015-01-27 11:14:24 +0000

    honor email and phone1 params in contact/new

--- www/contacts/new/index.php
+++ www/contacts/new/index.php
@@ -1,20 +1,25 @@
 <?php
 
 $base = '../../';
+$fnsDir = '../../fns';
 
-include_once '../../fns/require_user.php';
+include_once "$fnsDir/require_user.php";
 $user = require_user($base);
 
 $key = 'contacts/new/values';
 if (array_key_exists($key, $_SESSION)) {
     $values = $_SESSION[$key];
 } else {
+
+    include_once "$fnsDir/request_strings.php";
+    list($email, $phone1) = request_strings('email', 'phone1');
+
     $values = [
         'full_name' => '',
         'alias' => '',
         'address' => '',
-        'email' => '',
-        'phone1' => '',
+        'email' => $email,
+        'phone1' => $phone1,
         'phone2' => '',
         'birthday_day' => 0,
         'birthday_month' => 0,
@@ -36,5 +41,5 @@ unset(
 include_once 'fns/create_content.php';
 $content = create_content($values);
 
-include_once '../../fns/echo_page.php';
+include_once "$fnsDir/echo_page.php";
 echo_page($user, 'New Contact', $content, $base);
