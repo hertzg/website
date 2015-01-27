f924eab8ace898f8d4e3a594ca2079c72285246a
Archil Imnadze <archil@imnadze.ge>
2015-01-27 11:08:50 +0000

    protocol handler page

---
+++ www/handle-protocol/index.php
@@ -0,0 +1,83 @@
+<?php
+
+$base = '../';
+$fnsDir = '../fns';
+
+include_once "$fnsDir/require_user.php";
+$user = require_user($base);
+
+include_once "$fnsDir/request_strings.php";
+list($value) = request_strings('value');
+
+$boldValue = '"<b>'.htmlspecialchars($value).'</b>"';
+$items = [];
+$errors = [];
+$warnings = [];
+
+$url = parse_url($value);
+
+if (array_key_exists('scheme', $url) && array_key_exists('path', $url)) {
+
+    $scheme = $url['scheme'];
+    $path = $url['path'];
+
+    if ($scheme === 'mailto' || $scheme === 'sms' || $scheme === 'tel') {
+
+        $escapedPath = htmlspecialchars($path);
+        $pathParam = rawurlencode($path);
+        $warnings[] = "What would you like to do with \"<b>$escapedPath</b>\"?";
+
+        if ($scheme === 'mailto') {
+            $type = 'email';
+            $property = 'email';
+        } else {
+            $type = 'phone';
+            $property = 'phone1';
+        }
+
+        include_once "$fnsDir/Page/imageLinkWithDescription.php";
+        $items[] = Page\imageLinkWithDescription('Search',
+            "Search for \"$escapedPath\".", "../search/?keyword=$pathParam",
+            'search');
+
+        $items[] = Page\imageLinkWithDescription('New Contact',
+            "Create a new contact with the $type \"$escapedPath\".",
+            "../contacts/new/?$property=$pathParam", 'create-contact');
+
+    } else {
+        $errors[] = "The link $boldValue cannot be opened.";
+    }
+
+} else {
+    include_once "$fnsDir/Page/info.php";
+    $errors[] = "The link $boldValue is invalid.";
+}
+
+if ($errors) {
+    include_once "$fnsDir/Page/errors.php";
+    $errorsHtml = Page\errors($errors);
+} else {
+    $errorsHtml = '';
+}
+
+if ($warnings) {
+    include_once "$fnsDir/Page/warnings.php";
+    $warningsHtml = Page\warnings($warnings);
+} else {
+    $warningsHtml = '';
+}
+
+include_once "$fnsDir/Page/tabs.php";
+$content = Page\tabs(
+    [
+        [
+            'title' => 'Home',
+            'href' => '..',
+        ],
+    ],
+    'Handle Protocol',
+    $errorsHtml.$warningsHtml.join('<div class="hr"></div>', $items)
+);
+
+include_once "$fnsDir/echo_page.php";
+echo_page($user, 'Handle Protocol', $content, $base);
