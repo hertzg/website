30e6a269fc8aab8535219fbffe0ce53c73ddf492
Archil Imnadze <archil@imnadze.ge>
2015-02-02 09:57:40 +0000

    do not use a reset password key that is 24 hours old

--- www/fns/Users/getByResetPasswordKey.php
+++ www/fns/Users/getByResetPasswordKey.php
@@ -6,10 +6,11 @@ function getByResetPasswordKey ($mysqli, $id_users, $reset_password_key) {
 
     $reset_password_key = hex2bin($reset_password_key);
     $reset_password_key = $mysqli->real_escape_string($reset_password_key);
+    $reset_password_key_time = time() - 24 * 60 * 60;
 
-    // TODO check for expired keys
     $sql = "select * from users where id_users = $id_users"
-        ." and reset_password_key = '$reset_password_key'";
+        ." and reset_password_key = '$reset_password_key'"
+        ." and reset_password_key_time > $reset_password_key_time";
 
     include_once __DIR__.'/../mysqli_single_object.php';
     return mysqli_single_object($mysqli, $sql);
