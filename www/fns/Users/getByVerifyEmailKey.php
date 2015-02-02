5ab7798f913199ed8dca8100c166836b7cfc4c91
Archil Imnadze <archil@imnadze.ge>
2015-02-02 10:26:22 +0000

    do not use verify email key that is older than 24 hours

--- www/fns/Users/getByVerifyEmailKey.php
+++ www/fns/Users/getByVerifyEmailKey.php
@@ -6,9 +6,11 @@ function getByVerifyEmailKey ($mysqli, $id_users, $verify_email_key) {
 
     $verify_email_key = hex2bin($verify_email_key);
     $verify_email_key = $mysqli->real_escape_string($verify_email_key);
+    $verify_email_key_time = time() - 24 * 60 * 60;
 
     $sql = "select * from users where id_users = $id_users"
-        ." and email != '' and verify_email_key = '$verify_email_key'";
+        ." and email != '' and verify_email_key = '$verify_email_key'"
+        ." and verify_email_key_time > $verify_email_key_time";
 
     include_once __DIR__.'/../mysqli_single_object.php';
     return mysqli_single_object($mysqli, $sql);
