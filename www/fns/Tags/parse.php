4dfd466a5d0ee69fa7dae0b8873409b7051fc716
Archil Imnadze <archil@imnadze.ge>
2015-02-07 17:33:54 +0400

    sort tags

--- www/fns/Tags/parse.php
+++ www/fns/Tags/parse.php
@@ -2,16 +2,23 @@
 
 namespace Tags;
 
-function parse ($tag_names) {
+function parse (&$tags) {
 
     include_once __DIR__.'/../str_collapse_spaces.php';
-    $tag_names = str_collapse_spaces($tag_names);
+    $tag_names = str_collapse_spaces($tags);
 
     if ($tag_names === '') return [];
 
     $tag_names = explode(' ', $tag_names);
     $tag_names = array_unique($tag_names);
     $tag_names = array_values($tag_names);
+
+    usort($tag_names, function ($a, $b) {
+        return strcasecmp($a, $b);
+    });
+
+    $tags = join(' ', $tag_names);
+
     return $tag_names;
 
 }
