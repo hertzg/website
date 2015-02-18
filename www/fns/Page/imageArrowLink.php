66008b6140bd5471bffe2c2240bca8bd61b543fc
Archil Imnadze <archil@imnadze.ge>
2015-02-18 09:16:22 +0000

    remove array typed options paramter from Page\image* functions

--- www/fns/Page/imageArrowLink.php
+++ www/fns/Page/imageArrowLink.php
@@ -2,8 +2,7 @@
 
 namespace Page;
 
-function imageArrowLink ($title, $href, $iconName,
-    array $options = []) {
+function imageArrowLink ($title, $href, $iconName, $options = []) {
     $options['class'] = 'withArrow';
     include_once __DIR__.'/imageLink.php';
     return imageLink($title, $href, $iconName, $options);
--- www/fns/Page/imageArrowLinkWithDescription.php
+++ www/fns/Page/imageArrowLinkWithDescription.php
@@ -2,13 +2,13 @@
 
 namespace Page;
 
-function imageArrowLinkWithDescription ($title, $description, $href,
-    $iconName, array $options = []) {
+function imageArrowLinkWithDescription ($title,
+    $description, $href, $iconName, $options = []) {
 
     $options['class'] = 'withArrow';
 
     include_once __DIR__.'/imageLinkWithDescription.php';
-    return imageLinkWithDescription($title, $description, $href,
-        $iconName, $options);
+    return imageLinkWithDescription($title,
+        $description, $href, $iconName, $options);
 
 }
