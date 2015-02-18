fc219dc9650e48f1a159ab98298319ae09a442bb
Archil Imnadze <archil@imnadze.ge>
2015-02-18 07:24:10 +0000

    script to restore single points of places

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -7,16 +7,18 @@ include_once '../lib/mysqli.php';
 
 $microtime = microtime(true);
 
-$mysqli->query('delete from place_points') || trigger_error($mysqli->error);
-
-$sql = 'update places set num_points = 1';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
 include_once '../fns/PlacePoints/add.php';
-$places = mysqli_query_object($mysqli, 'select * from places');
+$places = mysqli_query_object($mysqli, 'select * from places where num_points = 0');
 foreach ($places as $place) {
-    PlacePoints\add($mysqli, $place->id_users, $place->id,
+
+    $id = $place->id;
+
+    PlacePoints\add($mysqli, $place->id_users, $id,
         $place->latitude, $place->longitude, $place->altitude);
+
+    $sql = "update places set num_points = 1 where id = $id";
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+
 }
 
 $elapsedSeconds = number_format(microtime(true) - $microtime, 3);
