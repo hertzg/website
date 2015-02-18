1ea16702f0426a4f05429dde8b96ac78e61ac476
Archil Imnadze <archil@imnadze.ge>
2015-02-18 23:09:29 +0400

    show UTC date on calendar icons

--- www/js/calendarIcon/calendarIcon.js
+++ www/js/calendarIcon/calendarIcon.js
@@ -1,6 +1,6 @@
 (function (batteryAndClock) {
     var dayElement = document.querySelector('.calendarIcon-day')
     batteryAndClock.onClockUpdate(function (date) {
-        dayElement.innerHTML = date.getDate()
+        dayElement.innerHTML = date.getUTCDate()
     })
 })(batteryAndClock)
