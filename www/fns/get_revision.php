c825afa6167faba9b334f4476a6202d14b94f170
Archil Imnadze <archil@imnadze.ge>
2015-02-19 08:15:05 +0000

    remove unnecessary calendar icon text-shadow

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -2,7 +2,7 @@
 
 function get_revision ($key) {
     static $revisions = [
-        'css/calendarIcon/compressed.css' => 1,
+        'css/calendarIcon/compressed.css' => 2,
         'css/common/compressed.css' => 83,
         'css/confirmDialog/compressed.css' => 1,
         'css/contact/compressed.css' => 3,
