062d0846ff1b4746958738d6d04de4df8be2cb32
Archil Imnadze <archil@imnadze.ge>
2015-02-20 11:53:38 +0000

    use empty href for geolocation link

--- www/places/fns/create_geolocation_panel.php
+++ www/places/fns/create_geolocation_panel.php
@@ -7,7 +7,7 @@ function create_geolocation_panel ($base,
 
     include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
     $content = Page\imageArrowLinkWithDescription('Use Geolocation',
-        'Use GPS to detect the current location.', '../geolocation',
+        'Use GPS to detect the current location.', '',
         'locate', ['id' => 'geolocationLink']);
 
     include_once "$fnsDir/create_panel.php";
