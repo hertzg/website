afe8147991a4ea80adad46491457226cf8ba83ff
Archil Imnadze <archil@imnadze.ge>
2015-02-23 12:38:49 +0000

    require same domain referer in delete-point submit page

--- www/places/delete-point/submit.php
+++ www/places/delete-point/submit.php
@@ -1,5 +1,10 @@
 <?php
 
+$fnsDir = '../../fns';
+
+include_once "$fnsDir/require_same_domain_referer.php";
+require_same_domain_referer('..');
+
 include_once '../fns/require_point.php';
 include_once '../../lib/mysqli.php';
 list($point, $id, $user, $place) = require_point($mysqli);
@@ -7,10 +12,10 @@ list($point, $id, $user, $place) = require_point($mysqli);
 $_SESSION['places/view/messages'] = ['The point has been removed.'];
 
 if ($place->num_points > 1) {
-    include_once '../../fns/Users/Places/Points/delete.php';
+    include_once "$fnsDir/Users/Places/Points/delete.php";
     Users\Places\Points\delete($mysqli, $point);
 }
 
-include_once '../../fns/redirect.php';
-include_once '../../fns/ItemList/itemQuery.php';
+include_once "$fnsDir/redirect.php";
+include_once "$fnsDir/ItemList/itemQuery.php";
 redirect('../view/'.ItemList\itemQuery($place->id));
