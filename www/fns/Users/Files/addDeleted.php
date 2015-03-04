d8dc6b94ec22c87aa6cb0ff30f02017f39ff4bee
Archil Imnadze <archil@imnadze.ge>
2015-03-04 07:48:12 +0000

    set unique name to files restored from trash

--- www/fns/Users/Files/addDeleted.php
+++ www/fns/Users/Files/addDeleted.php
@@ -6,6 +6,7 @@ function addDeleted ($mysqli, $user, $data) {
 
     $id_folders = $data->id_folders;
     $id_users = $user->id_users;
+    $name = $data->name;
     $size = $data->size;
     $fnsDir = __DIR__.'/../..';
 
@@ -13,9 +14,12 @@ function addDeleted ($mysqli, $user, $data) {
     $folder = \Users\Folders\get($mysqli, $user, $id_folders);
     if (!$folder) $id_folders = 0;
 
+    include_once "$fnsDir/Files/getUniqueName.php";
+    $name = \Files\getUniqueName($mysqli, $id_users, $id_folders, $name);
+
     include_once "$fnsDir/Files/addDeleted.php";
     \Files\addDeleted($mysqli, $data->id, $id_users,
-        $id_folders, $data->content_type, $data->media_type, $data->name,
+        $id_folders, $data->content_type, $data->media_type, $name,
         $size, $data->insert_time, $data->rename_time,
         $data->content_revision, $data->revision);
 
