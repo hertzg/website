b5e36483e06b69bf25101068122f27e94c256db3
Archil Imnadze <archil@imnadze.ge>
2015-03-04 07:42:56 +0000

    use Files\getUniqueName in Users\Files\Received\importCopy

--- www/fns/Users/Files/Received/importCopy.php
+++ www/fns/Users/Files/Received/importCopy.php
@@ -12,22 +12,8 @@ function importCopy ($mysqli, $receivedFile, $parent_id, $insertApiKey = null) {
     include_once "$fnsDir/ReceivedFiles/File/path.php";
     $filePath = \ReceivedFiles\File\path($id_users, $receivedFile->id);
 
-    include_once "$fnsDir/Files/getByName.php";
-    while (\Files\getByName($mysqli, $id_users, $parent_id, $name)) {
-        $extension = '';
-        if (preg_match('/\..*?$/', $name, $match)) {
-            $name = preg_replace('/\..*?$/', '', $name);
-            $extension = $match[0];
-        }
-        if (preg_match('/_(\d+)$/', $name, $match)) {
-            $name = preg_replace('/_\d+$/', '_'.($match[1] + 1), $name);
-        } else {
-            $name .= '_1';
-        }
-        if ($extension) {
-            $name = "$name$extension";
-        }
-    }
+    include_once "$fnsDir/Files/getUniqueName.php";
+    $name = \Files\getUniqueName($mysqli, $id_users, $parent_id, $name);
 
     include_once "$fnsDir/Users/Files/add.php";
     return \Users\Files\add($mysqli, $id_users,
