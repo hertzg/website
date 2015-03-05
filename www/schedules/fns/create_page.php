193ce442c345194efd4109150586c0e9e299e3e7
Archil Imnadze <archil@imnadze.ge>
2015-03-05 13:41:11 +0000

    implement paging in schedules

--- www/schedules/fns/create_page.php
+++ www/schedules/fns/create_page.php
@@ -11,14 +11,32 @@ function create_page ($mysqli, $user, $base = '') {
     include_once "$fnsDir/Paging/requestOffset.php";
     $offset = Paging\requestOffset();
 
+    $items = [];
+
+    $searchAction = "{$base}search/";
+    $searchPlaceholder = 'Search schedules...';
+
+    include_once "$fnsDir/Paging/limit.php";
+    $limit = Paging\limit();
+
+    $searchForm = false;
+
     if ($tag === '') {
 
         $filterMessage = '';
 
         include_once "$fnsDir/Schedules/indexOnUser.php";
         $schedules = Schedules\indexOnUser($mysqli, $id_users);
+        $total = count($schedules);
 
-        if (count($schedules) > 1) {
+        if ($total > 1) {
+
+            include_once "$fnsDir/SearchForm/emptyContent.php";
+            $formContent = SearchForm\emptyContent($searchPlaceholder);
+
+            include_once "$fnsDir/SearchForm/create.php";
+            $items[] = SearchForm\create($searchAction, $formContent);
+            $searchForm = true;
 
             include_once "$fnsDir/ScheduleTags/indexOnUser.php";
             $tags = \ScheduleTags\indexOnUser($mysqli, $id_users);
@@ -32,61 +50,53 @@ function create_page ($mysqli, $user, $base = '') {
 
     } else {
 
+        include_once "$fnsDir/ScheduleTags/indexOnTagName.php";
+        $schedules = ScheduleTags\indexOnTagName($mysqli, $id_users, $tag);
+        $total = count($schedules);
+
+        if ($total > 1) {
+
+            include_once "$fnsDir/Form/hidden.php";
+            include_once "$fnsDir/SearchForm/emptyContent.php";
+            $formContent =
+                SearchForm\emptyContent($searchPlaceholder)
+                .Form\hidden('tag', $tag);
+
+            include_once "$fnsDir/SearchForm/create.php";
+            $items[] = SearchForm\create($searchAction, $formContent);
+            $searchForm = true;
+
+        }
+
         include_once "$fnsDir/create_clear_filter_bar.php";
         $filterMessage = create_clear_filter_bar($tag, $base);
 
-        include_once "$fnsDir/ScheduleTags/indexOnTagName.php";
-        $schedules = ScheduleTags\indexOnTagName($mysqli, $id_users, $tag);
-
     }
 
-    $scripts = '';
+    include_once __DIR__.'/sort_schedules.php';
+    sort_schedules($user, $schedules);
+    $schedules = array_slice($schedules, $offset, $limit);
 
-    $items = [];
-    if ($schedules) {
+    $params = [];
+    if ($tag !== '') $params['tag'] = $tag;
+    if ($offset) $params['offset'] = $offset;
 
-        if (count($schedules) > 1) {
+    include_once "$fnsDir/check_offset_overflow.php";
+    check_offset_overflow($offset, $limit, $total, $params);
 
-            include_once "$fnsDir/SearchForm/emptyContent.php";
-            $formContent = SearchForm\emptyContent('Search schedules...');
+    include_once __DIR__.'/render_prev_button.php';
+    render_prev_button($offset, $limit, $total, $items, $tag);
 
-            if ($tag !== '') {
-                include_once "$fnsDir/Form/hidden.php";
-                $formContent .= Form\hidden('tag', $tag);
-            }
+    include_once __DIR__.'/render_schedules.php';
+    render_schedules($user, $schedules, $items, $params, $base);
 
-            include_once "$fnsDir/SearchForm/create.php";
-            $items[] = SearchForm\create("{$base}search/", $formContent);
-
-            include_once "$fnsDir/compressed_js_script.php";
-            $scripts = compressed_js_script('searchForm', "$base../");
-
-        }
-
-        include_once __DIR__.'/sort_schedules.php';
-        sort_schedules($user, $schedules);
-
-        include_once __DIR__.'/format_days_left.php';
-        include_once "$fnsDir/ItemList/escapedItemQuery.php";
-        include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
-        foreach ($schedules as $schedule) {
-            $id = $schedule->id;
-            $title = htmlspecialchars($schedule->text);
-            $description = format_days_left($user, $schedule->days_left);
-            $href = "{$base}view/".ItemList\escapedItemQuery($id);
-            $items[] = Page\imageArrowLinkWithDescription($title,
-                $description, $href, 'schedule', ['id' => $id]);
-        }
-
-    } else {
-        include_once "$fnsDir/Page/info.php";
-        $items[] = Page\info('No schedules');
-    }
+    include_once __DIR__.'/render_next_button.php';
+    render_next_button($offset, $limit, $total, $items, $tag);
 
     include_once __DIR__.'/unset_session_vars.php';
     unset_session_vars();
 
     include_once __DIR__.'/create_content.php';
-    return create_content($user, $filterMessage, $items, $base, $scripts);
+    return create_content($user, $filterMessage, $items, $base, $searchForm);
 
 }
---
+++ www/schedules/fns/render_next_button.php
@@ -0,0 +1,13 @@
+<?php
+
+function render_next_button ($offset, $limit, $total, &$items, $tag) {
+
+    if ($offset + $limit >= $total) return;
+
+    $args = [];
+    if ($tag !== '') $args['tag'] = $tag;
+
+    include_once __DIR__.'/../../fns/Paging/nextButton.php';
+    $items[] = Paging\nextButton($offset, $limit, 'Schedules', $args);
+
+}
