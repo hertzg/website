7e96d676aef0ae5875cab672b8964f85d0562511
Archil Imnadze <archil@imnadze.ge>
2015-03-05 10:32:23 +0000

    remove unused max length values

--- www/fns/SubscribedChannels/maxLengths.php
+++ www/fns/SubscribedChannels/maxLengths.php
@@ -12,13 +12,11 @@ function maxLengths () {
     include_once "$fnsDir/ApiKeyName/maxLength.php";
     $apiKeyNameMaxLength = \ApiKeyName\maxLength();
 
-    include_once "$fnsDir/Username/maxLength.php";
     return [
         'channel_name' => $channelNameMaxLength,
         'insert_api_key_name' => $apiKeyNameMaxLength,
         'lowercase_name' => $channelNameMaxLength,
         'update_api_key_name' => $apiKeyNameMaxLength,
-        'username' => \Username\maxLength(),
     ];
 
 }
--- www/fns/Tokens/maxLengths.php
+++ www/fns/Tokens/maxLengths.php
@@ -3,9 +3,5 @@
 namespace Tokens;
 
 function maxLengths () {
-    include_once __DIR__.'/../Username/maxLength.php';
-    return [
-        'username' => \Username\maxLength(),
-        'token_text' => 16,
-    ];
+    return ['token_text' => 16];
 }
