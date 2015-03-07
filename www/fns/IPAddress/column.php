f8f4465cc0738cd84e761c553e4eb027e44dc05d
Archil Imnadze <archil@imnadze.ge>
2015-03-07 20:42:02 +0400

    limit ip address columns to 45 characters

---
+++ www/fns/IPAddress/column.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace IPAddress;
+
+function column () {
+    return [
+        'type' => 'varchar(45)',
+        'characterSet' => 'ascii',
+        'collation' => 'ascii_bin',
+    ];
+}
