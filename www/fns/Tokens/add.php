453bc48b2724de9be617b2fd91b37884264a30fb
Archil Imnadze <archil@imnadze.ge>
2015-03-07 21:32:41 +0400

    save access_remote_address on token insert

--- www/fns/Tokens/add.php
+++ www/fns/Tokens/add.php
@@ -2,19 +2,22 @@
 
 namespace Tokens;
 
-function add ($mysqli, $id_users, $username, $token_text, $user_agent) {
+function add ($mysqli, $id_users, $username,
+    $token_text, $access_remote_address, $user_agent) {
 
     $username = $mysqli->real_escape_string($username);
     $token_text = $mysqli->real_escape_string($token_text);
     $insert_time = $access_time = time();
+    $access_remote_address = $mysqli->real_escape_string(
+        $access_remote_address);
 
     if ($user_agent === null) $user_agent = 'null';
     else $user_agent = "'".$mysqli->real_escape_string($user_agent)."'";
 
     $sql = 'insert into tokens (id_users, username, token_text,'
-        .' user_agent, insert_time, access_time)'
+        .' access_remote_address, user_agent, insert_time, access_time)'
         ." values ($id_users, '$username', '$token_text',"
-        ." $user_agent, $insert_time, $access_time)";
+        ." '$access_remote_address', $user_agent, $insert_time, $access_time)";
 
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
