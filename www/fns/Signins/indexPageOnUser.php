c805e092d57bb99aa0381e79ce5c4effe1dea8a9
Archil Imnadze <archil@imnadze.ge>
2015-03-07 20:32:20 +0400

    fix task #513 paging in successful signins

---
+++ www/fns/Signins/indexPageOnUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Signins;
+
+function indexPageOnUser ($mysqli, $id_users, $offset, $limit, &$total) {
+
+    $fnsDir = __DIR__.'/..';
+
+    $fromWhere = "from signins where id_users = $id_users";
+
+    $sql = "select count(*) total $fromWhere";
+    include_once "$fnsDir/mysqli_single_object.php";
+    $total = mysqli_single_object($mysqli, $sql)->total;
+
+    if ($offset >= $total) return [];
+
+    $sql = "select * $fromWhere order by insert_time desc"
+        ." limit $limit offset $offset";
+    include_once "$fnsDir/mysqli_query_object.php";
+    return mysqli_query_object($mysqli, $sql);
+
+}
