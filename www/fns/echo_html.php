6c48710240c70e97a9e5a56bbb883463799f9b27
Archil Imnadze <archil@imnadze.ge>
2015-04-05 14:16:08 +0400

    integrate allsize icons in web manifest and pages

--- www/fns/echo_html.php
+++ www/fns/echo_html.php
@@ -19,6 +19,30 @@ function echo_html ($title, $head, $body, $theme, $base) {
                 ." href=\"{$base}themes/$theme/images/icon16.png?1\" />"
                 .'<link rel="icon" type="image/png" sizes="32x32"'
                 ." href=\"{$base}themes/$theme/images/icon32.png?1\" />"
+                .'<link rel="icon" type="image/png" sizes="48x48"'
+                ." href=\"{$base}zvini-icons/48.png\" />"
+                .'<link rel="icon" type="image/png" sizes="60x60"'
+                ." href=\"{$base}zvini-icons/60.png\" />"
+                .'<link rel="icon" type="image/png" sizes="64x64"'
+                ." href=\"{$base}zvini-icons/64.png\" />"
+                .'<link rel="icon" type="image/png" sizes="84x84"'
+                ." href=\"{$base}zvini-icons/84.png\" />"
+                .'<link rel="icon" type="image/png" sizes="90x90"'
+                ." href=\"{$base}zvini-icons/90.png\" />"
+                .'<link rel="icon" type="image/png" sizes="120x120"'
+                ." href=\"{$base}zvini-icons/120.png\" />"
+                .'<link rel="icon" type="image/png" sizes="126x126"'
+                ." href=\"{$base}zvini-icons/126.png\" />"
+                .'<link rel="icon" type="image/png" sizes="128x128"'
+                ." href=\"{$base}zvini-icons/128.png\" />"
+                .'<link rel="icon" type="image/png" sizes="142x142"'
+                ." href=\"{$base}zvini-icons/142.png\" />"
+                .'<link rel="icon" type="image/png" sizes="189x189"'
+                ." href=\"{$base}zvini-icons/189.png\" />"
+                .'<link rel="icon" type="image/png" sizes="256x256"'
+                ." href=\"{$base}zvini-icons/256.png\" />"
+                .'<link rel="icon" type="image/png" sizes="512x512"'
+                ." href=\"{$base}zvini-icons/512.png\" />"
                 .'<meta http-equiv="Content-Type"'
                 .' content="text/html; charset=UTF-8" />'
                 .'<meta name="viewport"'
--- www/webapp-manifest/index.php
+++ www/webapp-manifest/index.php
@@ -24,18 +24,22 @@ echo json_encode([
         'url' => "http://$domainName/",
     ],
     'icons' => [
-        16 => "{$siteBase}zvini-icons/16.png?"
+        '16' => "{$siteBase}zvini-icons/16.png?"
             .get_revision('zvini-icons/16.png'),
-        32 => "{$siteBase}zvini-icons/32.png?"
+        '32' => "{$siteBase}zvini-icons/32.png?"
             .get_revision('zvini-icons/32.png'),
-        48 => "{$siteBase}zvini-icons/48.png?1",
-        60 => "{$siteBase}zvini-icons/60.png?1",
-        64 => "{$siteBase}zvini-icons/64.png?1",
-        90 => "{$siteBase}zvini-icons/90.png?1",
-        120 => "{$siteBase}zvini-icons/120.png?1",
-        128 => "{$siteBase}zvini-icons/128.png?1",
-        256 => "{$siteBase}zvini-icons/256.png?1",
-        512 => "{$siteBase}zvini-icons/512.png",
+        '48' => "{$siteBase}zvini-icons/48.png?1",
+        '60' => "{$siteBase}zvini-icons/60.png?1",
+        '64' => "{$siteBase}zvini-icons/64.png?1",
+        '84' => "{$siteBase}zvini-icons/84.png",
+        '90' => "{$siteBase}zvini-icons/90.png?1",
+        '120' => "{$siteBase}zvini-icons/120.png?1",
+        '126' => "{$siteBase}zvini-icons/126.png",
+        '128' => "{$siteBase}zvini-icons/128.png?1",
+        '142' => "{$siteBase}zvini-icons/142.png",
+        '189' => "{$siteBase}zvini-icons/189.png",
+        '256' => "{$siteBase}zvini-icons/256.png?1",
+        '512' => "{$siteBase}zvini-icons/512.png",
     ],
     'permissions' => [
         'geolocation' => [
