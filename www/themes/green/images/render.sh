588e0d37bdd251a31df1467f18306467d608bf0c
Archil Imnadze <archil@imnadze.ge>
2015-04-07 13:55:22 +0000

    fix unrendered icons

--- www/themes/green/images/render.sh
+++ www/themes/green/images/render.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-function render () {
-    inkscape --export-png=$1.png $1.svg > /dev/null
-    optipng -q -o 7 -strip all $1.png
-}
-
 cd `dirname $BASH_SOURCE`
-render icon16
-render icon32
+for i in icon*.svg
+do
+    name=`basename $i .svg`
+    inkscape --export-png=$name.png $name.svg > /dev/null
+    optipng -q -o 7 -strip all $name.png
+done
