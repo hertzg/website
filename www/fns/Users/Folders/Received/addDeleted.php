76fb3bc46757a64c490fb08b917ec4e66f9d7887
Archil Imnadze <archil@imnadze.ge>
2015-04-19 06:30:00 +0000

    mark reeived folder subfolders and files as deleted

--- www/fns/Users/Folders/Received/addDeleted.php
+++ www/fns/Users/Folders/Received/addDeleted.php
@@ -4,12 +4,20 @@ namespace Users\Folders\Received;
 
 function addDeleted ($mysqli, $receiver_id_users, $data) {
 
+    $id = $data->id;
     $archived = $data->archived;
+    $fnsDir = __DIR__.'/../../..';
 
-    include_once __DIR__.'/../../../ReceivedFolders/addDeleted.php';
-    \ReceivedFolders\addDeleted($mysqli, $data->id, $data->sender_id_users,
+    include_once "$fnsDir/ReceivedFolders/addDeleted.php";
+    \ReceivedFolders\addDeleted($mysqli, $id, $data->sender_id_users,
         $data->sender_username, $receiver_id_users, $data->name,
         $archived, $data->insert_time);
+
+    include_once "$fnsDir/ReceivedFolderFiles/setDeletedOnFolder.php";
+    \ReceivedFolderFiles\setDeletedOnFolder($mysqli, $id, false);
+
+    include_once "$fnsDir/ReceivedFolderSubfolders/setDeletedOnFolder.php";
+    \ReceivedFolderSubfolders\setDeletedOnFolder($mysqli, $id, false);
 
     include_once __DIR__.'/addNumber.php';
     addNumber($mysqli, $receiver_id_users, 1);
--- www/fns/Users/Folders/Received/delete.php
+++ www/fns/Users/Folders/Received/delete.php
@@ -7,7 +7,16 @@ function delete ($mysqli, $receivedFolder, $apiKey = null) {
     include_once __DIR__.'/purge.php';
     purge($mysqli, $receivedFolder);
 
-    include_once __DIR__.'/../../DeletedItems/addReceivedFolder.php';
+    $id = $receivedFolder->id;
+    $fnsDir = __DIR__.'/../../..';
+
+    include_once "$fnsDir/ReceivedFolderFiles/setDeletedOnFolder.php";
+    \ReceivedFolderFiles\setDeletedOnFolder($mysqli, $id, true);
+
+    include_once "$fnsDir/ReceivedFolderSubfolders/setDeletedOnFolder.php";
+    \ReceivedFolderSubfolders\setDeletedOnFolder($mysqli, $id, true);
+
+    include_once "$fnsDir/Users/DeletedItems/addReceivedFolder.php";
     \Users\DeletedItems\addReceivedFolder($mysqli, $receivedFolder, $apiKey);
 
 }
