2f8a025b59945c11ebbb44581c26e11dd8c80592
Archil Imnadze <archil@imnadze.ge>
2015-04-20 16:49:10 +0000

    restore wallets from trash

---
+++ www/fns/Users/Wallets/addDeleted.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Users\Wallets;
+
+function addDeleted ($mysqli, $id_users, $data) {
+
+    $id = $data->id;
+    $balance = $data->balance;
+    $num_transactions = $data->num_transactions;
+
+    $fnsDir = __DIR__.'/../..';
+
+    include_once "$fnsDir/Wallets/addDeleted.php";
+    \Wallets\addDeleted($mysqli, $id, $id_users, $data->name,
+        $balance, $num_transactions, $data->insert_time,
+        $data->update_time, $data->revision);
+
+    if ($num_transactions) {
+        include_once "$fnsDir/WalletTransactions/setDeletedOnWallet.php";
+        \WalletTransactions\setDeletedOnWallet($mysqli, $id, false);
+    }
+
+    include_once __DIR__.'/addNumber.php';
+    addNumber($mysqli, $id_users, 1);
+
+    if ($balance) {
+        include_once __DIR__.'/../addBalanceTotal.php';
+        \Users\addBalanceTotal($mysqli, $id_users, $balance);
+    }
+
+}
---
+++ www/fns/Wallets/addDeleted.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wallets;
+
+function addDeleted ($mysqli, $id, $id_users, $name, $balance,
+    $num_transactions, $insert_time, $update_time, $revision) {
+
+    $name = $mysqli->real_escape_string($name);
+
+    $sql = 'insert into wallets (id, id_users, name, balance,'
+        .' num_transactions, insert_time, update_time)'
+        ." values ($id, $id_users, '$name', $balance,"
+        ." $num_transactions, $insert_time, $update_time)";
+
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+
+    return $mysqli->insert_id;
+
+}
