4ba59e9231fb2608bde22bf19f903c6b2bf64222
Archil Imnadze <archil@imnadze.ge>
2015-04-20 17:59:58 +0000

    script to update balance_total columns of all users

---
+++ www/scripts/update-balance-total.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+
+chdir(__DIR__);
+include_once '../../lib/cli.php';
+include_once '../fns/mysqli_query_object.php';
+include_once '../fns/mysqli_single_object.php';
+include_once '../lib/mysqli.php';
+
+$microtime = microtime(true);
+
+$users = mysqli_query_object($mysqli, 'select * from users');
+
+foreach ($users as $user) {
+
+    $id_users = $user->id_users;
+
+    $sql = 'select sum(balance) balance_total'
+        ." from wallets where id_users = $id_users";
+    $balance_total = mysqli_single_object($mysqli, $sql)->balance_total;
+    if ($balance_total === null) $balance_total = 0;
+
+    $sql = "update users set balance_total = $balance_total"
+        ." where id_users = $id_users";
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+
+}
+
+$elapsedSeconds = number_format(microtime(true) - $microtime, 3);
+echo "Done in $elapsedSeconds seconds.\n";
