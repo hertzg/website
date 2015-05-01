94b45bd9efe5c33ab1c6e01e41eae989154b3236
Archil Imnadze <archil@imnadze.ge>
2015-05-01 17:55:06 +0000

    error page descriptions and return links

--- www/fns/ErrorPage/forbidden.php
+++ www/fns/ErrorPage/forbidden.php
@@ -3,6 +3,11 @@
 namespace ErrorPage;
 
 function forbidden () {
+
+    $description = "Access to the page "
+        .'<em>'.htmlspecialchars($_SERVER['REQUEST_URI']).'</em> denied.';
+
     include_once __DIR__.'/create.php';
-    create(403, 'Forbidden');
+    create(403, 'Forbidden', $description);
+
 }
--- www/fns/ErrorPage/notFound.php
+++ www/fns/ErrorPage/notFound.php
@@ -3,6 +3,12 @@
 namespace ErrorPage;
 
 function notFound () {
+
+    $description = 'The page '
+        .'<em>'.htmlspecialchars($_SERVER['REQUEST_URI']).'</em>'
+        .' was not found.';
+
     include_once __DIR__.'/create.php';
-    create(404, 'Not Found');
+    create(404, 'Not Found', $description);
+
 }
