a2042a98b9d304da7d4674aaf614b3ce0289b392
Archil Imnadze <archil@imnadze.ge>
2015-05-05 10:45:58 +0000

    clear income and expense on all transaction delete

--- www/fns/Wallets/removeAllTransactions.php
+++ www/fns/Wallets/removeAllTransactions.php
@@ -4,6 +4,6 @@ namespace Wallets;
 
 function removeAllTransactions ($mysqli, $id) {
     $sql = 'update wallets set num_transactions = 0,'
-        ." balance = 0 where id = $id";
+        ." income = 0, expense = 0, balance = 0 where id = $id";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 }
