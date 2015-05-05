e564e9c89956c7d186c6f724eb1cf2dab2e4c0e6
Archil Imnadze <archil@imnadze.ge>
2015-05-05 10:47:50 +0000

    reduce wallet income and expense on transaction delete

--- www/fns/Wallets/removeTransaction.php
+++ www/fns/Wallets/removeTransaction.php
@@ -3,7 +3,18 @@
 namespace Wallets;
 
 function removeTransaction ($mysqli, $id, $amount) {
+
+    if ($amount > 0) {
+        $income = $amount;
+        $expense = 0;
+    } else {
+        $expense = -$amount;
+        $income = 0;
+    }
+
     $sql = 'update wallets set num_transactions = num_transactions - 1,'
+        ." income = income - $income, expense = expense - $expense,"
         ." balance = balance - $amount where id = $id";
     $mysqli->query($sql) || trigger_error($mysqli->error);
+
 }
