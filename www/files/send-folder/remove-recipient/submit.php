c672a0734b59757914085e8f7a5b4acd7cee525b
Archil Imnadze <archil@imnadze.ge>
2015-05-06 10:03:20 +0000

    require same domain referer when removing a recipient

--- www/files/send-folder/remove-recipient/submit.php
+++ www/files/send-folder/remove-recipient/submit.php
@@ -1,5 +1,10 @@
 <?php
 
+$fnsDir = '../../../fns';
+
+include_once "$fnsDir/require_same_domain_referer.php";
+require_same_domain_referer('../../');
+
 include_once 'fns/require_recipient.php';
 include_once '../../../lib/mysqli.php';
 list($file, $id, $username, $user) = require_recipient($mysqli);
@@ -13,5 +18,5 @@ if (array_key_exists($key, $_SESSION)) {
 
 $_SESSION['files/send-folder/messages'] = ['The recipient has been removed.'];
 
-include_once '../../../fns/redirect.php';
+include_once "$fnsDir/redirect.php";
 redirect("../?id_folders=$id");
