f89c73d26d6ecf160164f19546307eaeaaebd0f2
Archil Imnadze <archil@imnadze.ge>
2015-05-11 08:41:09 +0000

    Page\newItemMenu

---
+++ www/css/newItemMenu/build.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+process.chdir(__dirname)
+
+var fs = require('fs'),
+    uglifyCss = require('uglifycss')
+
+var source = fs.readFileSync('newItemMenu.css', 'utf-8')
+
+var compressCss = uglifyCss.processString(source)
+fs.writeFileSync('compressed.css', compressCss)
