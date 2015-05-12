5aea1cdfc98277010570f6cff1835504e30fd02b
Archil Imnadze <archil@imnadze.ge>
2015-05-12 10:09:33 +0000

    separate ViewPage\renderLines function

--- www/bar-charts/fns/ViewPage/chart.php
+++ www/bar-charts/fns/ViewPage/chart.php
@@ -63,41 +63,6 @@ function chart ($mysqli, $bar_chart) {
         $renderBar = $createNegative;
     }
 
-    $step = 1;
-    $a = 2.5;
-    $b = 2;
-    while (true) {
-
-        $c = $a;
-        $a = $b;
-        $b = $c;
-
-        $numLines = $range / $step;
-        if ($numLines > 5) $step *= $a;
-        elseif ($numLines < 2) $step /= $a;
-        else break;
-
-    }
-
-    $createLine = function ($value, $class) use ($max, $range) {
-        $top = (($max - $value) * 100 / $range).'%';
-        include_once __DIR__.'/../../../fns/short_number.php';
-        return
-            "<div class=\"barChart-line\" style=\"top: $top\">"
-                ."<div class=\"barChart-lineValue $class\">"
-                    .short_number($value)
-                .'</div>'
-            .'</div>';
-    };
-    $linesHtml = $createLine(0, 'positive');
-    for ($i = $step; $i < $max; $i += $step) {
-        $linesHtml .= $createLine($i, 'positive');
-    }
-    for ($i = -$step; $i > $min; $i -= $step) {
-        $linesHtml .= $createLine($i, 'negative');
-    }
-    $linesHtml .= $createLine($min, 'negative').$createLine($max, 'positive');
-
     $barsHtml = '';
     foreach ($bars as $bar) {
         $value = $bar->value;
@@ -107,10 +72,11 @@ function chart ($mysqli, $bar_chart) {
             .'</div>';
     }
 
+    include_once __DIR__.'/renderLines.php';
     return
         "<div class=\"barChart\">"
             .'<div class="barChart-content">'
-                .$linesHtml.$barsHtml
+                .renderLines($range, $min, $max).$barsHtml
             .'</div>'
         .'</div>'
         .'<div class="hr"></div>';
---
+++ www/bar-charts/fns/ViewPage/renderLines.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ViewPage;
+
+function renderLines ($range, $min, $max) {
+
+    $step = 1;
+    $a = 2.5;
+    $b = 2;
+    while (true) {
+
+        $c = $a;
+        $a = $b;
+        $b = $c;
+
+        $numLines = $range / $step;
+        if ($numLines > 8) $step *= $a;
+        elseif ($numLines < 2) $step /= $a;
+        else break;
+
+    }
+
+    $createLine = function ($value, $class) use ($max, $range) {
+        $top = (($max - $value) * 100 / $range).'%';
+        include_once __DIR__.'/../../../fns/short_number.php';
+        return
+            "<div class=\"barChart-line\" style=\"top: $top\">"
+                ."<div class=\"barChart-lineValue $class\">"
+                    .short_number($value)
+                .'</div>'
+            .'</div>';
+    };
+
+    $html = $createLine(0, 'positive');
+    for ($i = $step; $i < $max; $i += $step) {
+        $html .= $createLine($i, 'positive');
+    }
+    for ($i = -$step; $i > $min; $i -= $step) {
+        $html .= $createLine($i, 'negative');
+    }
+    $html .= $createLine($min, 'negative').$createLine($max, 'positive');
+    return $html;
+
+}
