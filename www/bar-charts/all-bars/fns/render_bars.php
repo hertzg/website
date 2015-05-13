1dd382158408d80ef85a81adb2ffb37101b8424e
Archil Imnadze <archil@imnadze.ge>
2015-05-13 12:59:00 +0000

    render bar labels in bar lists

--- www/bar-charts/all-bars/fns/render_bars.php
+++ www/bar-charts/all-bars/fns/render_bars.php
@@ -7,11 +7,25 @@ function render_bars ($bars, &$items, $base = '') {
     include_once "$fnsDir/export_date_ago.php";
     include_once "$fnsDir/ItemList/escapedItemQuery.php";
     include_once "$fnsDir/Page/imageArrowLink.php";
+    include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
     foreach ($bars as $bar) {
+ 
         $id = $bar->id;
-        $escapedItemQuery = ItemList\escapedItemQuery($id);
-        $items[] = Page\imageArrowLink($bar->value,
-            "{$base}view/$escapedItemQuery", 'bar', ['id' => $id]);
+        $title = $bar->value;
+        $label = $bar->label;
+        $href = "{$base}view/".ItemList\escapedItemQuery($id);
+        $icon = 'bar';
+        $options =  ['id' => $id];
+
+        if ($label === '') {
+            $link = Page\imageArrowLink($title, $href, $icon, $options);
+        } else {
+            $link = Page\imageArrowLinkWithDescription($title,
+                htmlspecialchars($label), $href, $icon, $options);
+        }
+
+        $items[] = $link;
+
     }
 
 }
--- www/bar-charts/fns/render_bars.php
+++ www/bar-charts/fns/render_bars.php
@@ -6,10 +6,25 @@ function render_bars ($bars, &$items) {
 
     include_once "$fnsDir/export_date_ago.php";
     include_once "$fnsDir/Page/imageArrowLink.php";
+    include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
     foreach ($bars as $bar) {
+
         $id = $bar->id;
-        $items[] = Page\imageArrowLink($bar->value,
-            "../view-bar/?id=$id", 'bar', ['id' => $id]);
+        $title = $bar->value;
+        $label = $bar->label;
+        $href = "../view-bar/?id=$id";
+        $icon = 'bar';
+        $options =  ['id' => $id];
+
+        if ($label === '') {
+            $link = Page\imageArrowLink($title, $href, $icon, $options);
+        } else {
+            $link = Page\imageArrowLinkWithDescription($title,
+                htmlspecialchars($label), $href, $icon, $options);
+        }
+
+        $items[] = $link;
+
     }
 
 }
