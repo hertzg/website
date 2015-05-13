c0137f0e4ddd8a85316b6a601f72e140a6d0e5ef
Archil Imnadze <archil@imnadze.ge>
2015-05-13 11:27:07 +0000

    bring confirm dialogs over other elements using z-index: 1

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -5,7 +5,7 @@ function get_revision ($key) {
         'css/barChart/compressed.css' => 2,
         'css/calendarIcon/compressed.css' => 2,
         'css/common/compressed.css' => 89,
-        'css/confirmDialog/compressed.css' => 1,
+        'css/confirmDialog/compressed.css' => 2,
         'css/contact/compressed.css' => 3,
         'css/icons/compressed.css' => 77,
         'css/index/compressed.css' => 1,
