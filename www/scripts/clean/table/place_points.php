b74008767a63b4e191388a3a5bed5cd68a19c79e
Archil Imnadze <archil@imnadze.ge>
2015-05-14 09:51:59 +0000

    do not clean deleted wallet transactions and place points

--- www/scripts/clean/table/place_points.php
+++ www/scripts/clean/table/place_points.php
@@ -3,15 +3,33 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../fns/mysqli_query_object.php';
 include_once '../../../lib/mysqli.php';
 
 $microtime = microtime(true);
 
 $sql = 'delete from place_points'
     .' where id_users not in (select id_users from users)'
-    .' or id_places not in (select id_places from places)';
+    .' or (!deleted and id_places not in (select id from places))';
 $mysqli->query($sql) || trigger_error($mysqli->error);
+$deleted = $mysqli->affected_rows;
+
+$ids = [];
+$sql = "select data_json from deleted_items where data_type = 'place'";
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) $ids[] = json_decode($row->data_json)->id;
+
+$sql = 'select id, id_places from place_points'
+    .' where deleted and id_places not in (select id from places)';
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) {
+    if (!in_array($row->id_places, $ids)) {
+        $sql = "delete from place_points where id = $row->id";
+        $mysqli->query($sql) || trigger_error($mysqli->error);
+        $deleted++;
+    }
+}
 
 $elapsedSeconds = number_format(microtime(true) - $microtime, 3);
 echo "Done in $elapsedSeconds seconds.\n"
-    ." $mysqli->affected_rows row(s) deleted.\n";
+    ." $deleted row(s) deleted.\n";
--- www/scripts/clean/table/wallet_transactions.php
+++ www/scripts/clean/table/wallet_transactions.php
@@ -3,15 +3,33 @@
 
 chdir(__DIR__);
 include_once '../../../../lib/cli.php';
+include_once '../../../fns/mysqli_query_object.php';
 include_once '../../../lib/mysqli.php';
 
 $microtime = microtime(true);
 
 $sql = 'delete from wallet_transactions'
     .' where id_users not in (select id_users from users)'
-    .' or id_wallets not in (select id_wallets from wallets)';
+    .' or (!deleted and id_wallets not in (select id from wallets))';
 $mysqli->query($sql) || trigger_error($mysqli->error);
+$deleted = $mysqli->affected_rows;
+
+$ids = [];
+$sql = "select data_json from deleted_items where data_type = 'wallet'";
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) $ids[] = json_decode($row->data_json)->id;
+
+$sql = 'select id, id_wallets from wallet_transactions'
+    .' where deleted and id_wallets not in (select id from wallets)';
+$rows = mysqli_query_object($mysqli, $sql);
+foreach ($rows as $row) {
+    if (!in_array($row->id_wallets, $ids)) {
+        $sql = "delete from wallet_transactions where id = $row->id";
+        $mysqli->query($sql) || trigger_error($mysqli->error);
+        $deleted++;
+    }
+}
 
 $elapsedSeconds = number_format(microtime(true) - $microtime, 3);
 echo "Done in $elapsedSeconds seconds.\n"
-    ." $mysqli->affected_rows row(s) deleted.\n";
+    ." $deleted row(s) deleted.\n";
