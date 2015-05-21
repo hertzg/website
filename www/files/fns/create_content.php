30fbe3183da602209e24e1c22c0554f55f13cffc
Archil Imnadze <archil@imnadze.ge>
2015-05-21 12:56:23 +0000

    split files/fns/create_page.php file

---
+++ www/files/fns/create_content.php
@@ -0,0 +1,39 @@
+<?php
+
+function create_content ($mysqli, $folder,
+    $user, $files, $items, $infoText, $scripts) {
+
+    $fnsDir = __DIR__.'/../../fns';
+    $id = $folder ? $folder->id_folders : 0;
+
+    include_once __DIR__.'/create_options_panel.php';
+    include_once __DIR__.'/create_location_bar.php';
+    include_once "$fnsDir/Page/sessionErrors.php";
+    include_once "$fnsDir/Page/sessionMessages.php";
+    include_once "$fnsDir/Page/tabs.php";
+    $content =
+        Page\tabs(
+            [
+                [
+                    'title' => 'Home',
+                    'href' => '../home/#files',
+                ],
+            ],
+            'Files',
+            Page\sessionErrors('files/errors')
+            .Page\sessionMessages('files/messages')
+            .create_location_bar($mysqli, $folder)
+            .join('<div class="hr"></div>', $items)
+            .$infoText
+        )
+        .create_options_panel($user, $id, $files)
+        .$scripts;
+
+    if ($id) {
+        include_once __DIR__.'/create_folder_options_panel.php';
+        $content .= create_folder_options_panel($id);
+    }
+
+    return $content;
+
+}
--- www/files/fns/create_page.php
+++ www/files/fns/create_page.php
@@ -69,34 +69,8 @@ function create_page ($mysqli, $user, $folder, &$scripts, $base = '') {
     include_once __DIR__.'/unset_session_vars.php';
     unset_session_vars();
 
-    include_once __DIR__.'/create_options_panel.php';
-    include_once __DIR__.'/create_location_bar.php';
-    include_once "$fnsDir/Page/sessionErrors.php";
-    include_once "$fnsDir/Page/sessionMessages.php";
-    include_once "$fnsDir/Page/tabs.php";
-    $content =
-        Page\tabs(
-            [
-                [
-                    'title' => 'Home',
-                    'href' => '../home/#files',
-                ],
-            ],
-            'Files',
-            Page\sessionErrors('files/errors')
-            .Page\sessionMessages('files/messages')
-            .create_location_bar($mysqli, $folder)
-            .join('<div class="hr"></div>', $items)
-            .$infoText
-        )
-        .create_options_panel($user, $id, $files)
-        .$scripts;
-
-    if ($id) {
-        include_once __DIR__.'/create_folder_options_panel.php';
-        $content .= create_folder_options_panel($id);
-    }
-
-    return $content;
+    include_once __DIR__.'/create_content.php';
+    return create_content($mysqli, $folder,
+        $user, $files, $items, $infoText, $scripts);
 
 }
