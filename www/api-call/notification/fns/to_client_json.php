e7854d9f00c5bb2954b778bcf720445b3b5e2072
Archil Imnadze <archil@imnadze.ge>
2015-05-25 08:21:37 +0000

    fix notification id type to int returned from api

--- www/api-call/notification/fns/to_client_json.php
+++ www/api-call/notification/fns/to_client_json.php
@@ -2,7 +2,7 @@
 
 function to_client_json ($notification) {
     return [
-        'id' => $notification->id,
+        'id' => (int)$notification->id,
         'text' => $notification->text,
         'channel_name' => $notification->channel_name,
         'insert_time' => (int)$notification->insert_time,
