e2b852837c8d277a6ceba915d87c6eafd0653f86
Archil Imnadze <archil@imnadze.ge>
2015-05-26 09:51:50 +0000

    forbid opening of new transaction page if no wallets are present

--- www/wallets/quick-new-transaction/index.php
+++ www/wallets/quick-new-transaction/index.php
@@ -1,10 +1,10 @@
 <?php
 
+include_once '../fns/require_wallets.php';
+$user = require_wallets();
+
 $base = '../../';
 $fnsDir = '../../fns';
-
-include_once "$fnsDir/require_user.php";
-$user = require_user($base);
 
 unset(
     $_SESSION['home/messages'],
