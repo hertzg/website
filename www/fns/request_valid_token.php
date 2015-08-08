591a89d1140edb5ad889e05fb44825c754ee06bf
Archil Imnadze <archil@imnadze.ge>
2015-08-08 06:07:29 +0000

    page to restore session from GET parameters

--- www/fns/request_valid_token.php
+++ www/fns/request_valid_token.php
@@ -1,12 +1,14 @@
 <?php
 
-function request_valid_token ($mysqli) {
+function request_valid_token ($mysqli, $array = null) {
 
-    if (!array_key_exists('username', $_COOKIE) ||
-        !array_key_exists('token', $_COOKIE)) return;
+    if ($array === null) $array = $_COOKIE;
 
-    $username = $_COOKIE['username'];
-    $token_text = $_COOKIE['token'];
+    if (!array_key_exists('username', $array) ||
+        !array_key_exists('token', $array)) return;
+
+    $username = $array['username'];
+    $token_text = $array['token'];
 
     if (!is_string($username) && !is_string($token_text)) return;
 
---
+++ www/restore-session/index.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once '../fns/request_valid_token.php';
+include_once '../lib/mysqli.php';
+$token = request_valid_token($mysqli, $_GET);
+
+if ($token) {
+
+    include_once '../fns/Cookie/set.php';
+    \Cookie\set('token', bin2hex($token->token_text));
+    \Cookie\set('username', $token->username);
+
+    include_once '../fns/redirect.php';
+    redirect('../home/');
+
+}
+
+include_once '../fns/echo_alert_page.php';
+echo_alert_page('Link Invalid', 'The link is no longer valid.', '..', '../');
