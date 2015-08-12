b34b702457e7d39f68088249971064bed8d6efda
Archil Imnadze <archil@imnadze.ge>
2015-08-12 12:10:43 +0000

    panel title border on top

--- www/fns/echo_html.php
+++ www/fns/echo_html.php
@@ -50,7 +50,7 @@ function echo_html ($title, $head, $body, $theme, $base) {
                 .compressed_css_link('common', $base)
                 .compressed_css_link('iconsets', $base)
                 .'<link rel="stylesheet" type="text/css"'
-                ." href=\"{$base}themes/$theme/common.css?19\" />"
+                ." href=\"{$base}themes/$theme/common.css?20\" />"
                 .$head
             .'</head>'
             .'<body>'
