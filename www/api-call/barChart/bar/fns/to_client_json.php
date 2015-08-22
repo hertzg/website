d91078e1b844c9ac7a5f50644b5fbbc2d91b9458
Archil Imnadze <archil@imnadze.ge>
2015-08-22 08:38:08 +0000

    return bar chart bar value as number from api

--- www/api-call/barChart/bar/fns/to_client_json.php
+++ www/api-call/barChart/bar/fns/to_client_json.php
@@ -3,7 +3,7 @@
 function to_client_json ($bar) {
     return [
         'id' => (int)$bar->id,
-        'value' => $bar->value,
+        'value' => (float)$bar->value,
         'label' => $bar->label,
         'insert_time' => (int)$bar->insert_time,
         'update_time' => (int)$bar->update_time,
