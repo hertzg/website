b1ed31c290b1fa0587f422f0fbc70b8beffb4315
Archil Imnadze <archil@imnadze.ge>
2015-08-25 14:48:12 +0000

    phishing warning on pages where there are password inputs

---
+++ www/fns/phishing_warning.php
@@ -0,0 +1,11 @@
+<?php
+
+function phishing_warning () {
+    include_once __DIR__.'/DomainName/get.php';
+    include_once __DIR__.'/Page/infoText.php';
+    include_once __DIR__.'/SiteBase/get.php';
+    include_once __DIR__.'/SiteProtocol/get.php';
+    return Page\infoText('The data will be sent to "<code>'
+        .SiteProtocol\get() .'://'.DomainName\get().SiteBase\get().'</code>".'
+        .' The same address should appear in your browser\'s address bar.');
+}
