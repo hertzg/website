c7df809de3226605148dbc404a74f9251e756c4d
Archil Imnadze <archil@imnadze.ge>
2015-08-25 14:53:33 +0000

    shorten phishing warning

--- www/fns/phishing_warning.php
+++ www/fns/phishing_warning.php
@@ -5,7 +5,7 @@ function phishing_warning () {
     include_once __DIR__.'/Page/infoText.php';
     include_once __DIR__.'/SiteBase/get.php';
     include_once __DIR__.'/SiteProtocol/get.php';
-    return Page\infoText('The data will be sent to "<code>'
-        .SiteProtocol\get() .'://'.DomainName\get().SiteBase\get().'</code>".'
+    return Page\infoText('The are accessing "<code>'
+        .SiteProtocol\get().'://'.DomainName\get().SiteBase\get().'</code>".'
         .' The address in your browser\'s address bar should start with it.');
 }
