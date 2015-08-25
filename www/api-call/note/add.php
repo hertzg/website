b34769528b6959e9d83efb62fdff6004abc6f347
Archil Imnadze <archil@imnadze.ge>
2015-08-25 18:34:59 +0000

    drop support of password-protecting using api key

--- www/api-call/note/add.php
+++ www/api-call/note/add.php
@@ -4,12 +4,11 @@ include_once '../fns/require_api_key.php';
 list($apiKey, $user, $mysqli) = require_api_key('can_write_notes');
 
 include_once 'fns/request_note_params.php';
-list($text, $tags, $tag_names, $encrypt_in_listings,
-    $password_protect) = request_note_params();
+list($text, $tags, $tag_names, $encrypt_in_listings) = request_note_params();
 
 include_once '../../fns/Users/Notes/add.php';
 $id = Users\Notes\add($mysqli, $user->id_users, $text, $tags,
-    $tag_names, $encrypt_in_listings, $password_protect, $apiKey);
+    $tag_names, $encrypt_in_listings, false, $apiKey);
 
 header('Content-Type: application/json');
 echo $id;
--- www/api-call/note/fns/request_note_params.php
+++ www/api-call/note/fns/request_note_params.php
@@ -3,8 +3,7 @@
 function request_note_params () {
 
     include_once __DIR__.'/../../../fns/Notes/request.php';
-    list($text, $tags, $encrypt_in_listings,
-        $password_protect) = Notes\request();
+    list($text, $tags, $encrypt_in_listings) = Notes\request();
 
     if ($text === '') {
         include_once __DIR__.'/../../fns/bad_request.php';
@@ -14,6 +13,6 @@ function request_note_params () {
     include_once __DIR__.'/../../fns/require_tags.php';
     list($tags, $tag_names) = require_tags();
 
-    return [$text, $tags, $tag_names, $encrypt_in_listings, $password_protect];
+    return [$text, $tags, $tag_names, $encrypt_in_listings];
 
 }
