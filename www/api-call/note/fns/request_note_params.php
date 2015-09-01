446f5a1e0a4a44731c87ca33409f08789b604aa5
Archil Imnadze <archil@imnadze.ge>
2015-09-01 12:47:13 +0000

    allow password-protecting using api if using session authentication

--- www/api-call/note/fns/request_note_params.php
+++ www/api-call/note/fns/request_note_params.php
@@ -2,8 +2,11 @@
 
 function request_note_params () {
 
-    include_once __DIR__.'/../../../fns/Notes/request.php';
-    list($text, $tags, $encrypt_in_listings) = Notes\request();
+    $fnsDir = __DIR__.'/../../../fns';
+
+    include_once "$fnsDir/Notes/request.php";
+    list($text, $tags, $encrypt_in_listings,
+        $password_protect) = Notes\request();
 
     if ($text === '') {
         include_once __DIR__.'/../../fns/bad_request.php';
@@ -13,6 +16,18 @@ function request_note_params () {
     include_once __DIR__.'/../../fns/require_tags.php';
     list($tags, $tag_names) = require_tags();
 
-    return [$text, $tags, $tag_names, $encrypt_in_listings];
+    if ($password_protect) {
+        include_once "$fnsDir/Session/EncryptionKey/get.php";
+        $encryption_key = Session\EncryptionKey\get();
+        if ($encryption_key === null) {
+            include_once __DIR__.'/../../fns/bad_request.php';
+            bad_request('CANNOT_PASSWORD_PROTECT');
+        }
+    } else {
+        $encryption_key = null;
+    }
+
+    return [$text, $tags, $tag_names,
+        $encrypt_in_listings, $password_protect, $encryption_key];
 
 }
