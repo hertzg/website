b1bdf1260288eb82f3fbb5c156565016e62f3d2c
Archil Imnadze <archil@imnadze.ge>
2015-09-03 16:26:53 +0000

    rename encrypt property of deleted notes to encrypt_in_listings

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -17,4 +17,19 @@ $sql = 'alter table received_notes change encrypt'
     .' encrypt_in_listings tinyint unsigned not null';
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
+$sql = 'select * from deleted_items'
+    ." where data_type in ('note', 'received_note')";
+include_once '../fns/mysqli_query_object.php';
+$deleted_items = mysqli_query_object($mysqli, $sql);
+
+foreach ($deleted_items as $deleted_item) {
+    $data = json_decode($deleted_item->data_json);
+    $data->encrypt_in_listings = $data->encrypt;
+    unset($data->encrypt);
+    $data = $mysqli->real_escape_string(json_encode($data));
+    $sql = "update deleted_items set data_json = '$data'"
+        ." where id = $deleted_item->id";
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+}
+
 echo "Done\n";
