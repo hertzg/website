9b868b0eb452dd53ae04b7181b4bc0127a0c08d1
Archil Imnadze <archil@imnadze.ge>
2015-09-03 16:39:58 +0000

    set empty fields to deleted notes to seamlessly upgrade

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -17,8 +17,24 @@ $sql = 'alter table received_notes change encrypt'
     .' encrypt_in_listings tinyint unsigned not null';
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
-$sql = 'select * from deleted_items'
-    ." where data_type in ('note', 'received_note')";
+$sql = "select * from deleted_items where data_type = 'note'";
+include_once '../fns/mysqli_query_object.php';
+$deleted_items = mysqli_query_object($mysqli, $sql);
+
+foreach ($deleted_items as $deleted_item) {
+    $data = json_decode($deleted_item->data_json);
+    $data->encrypt_in_listings = $data->encrypt;
+    $data->password_protect = false;
+    $data->encrypted_text = $data->encrypted_text_iv = null;
+    $data->encrypted_title = $data->encrypted_title_iv = null;
+    unset($data->encrypt);
+    $data = $mysqli->real_escape_string(json_encode($data));
+    $sql = "update deleted_items set data_json = '$data'"
+        ." where id = $deleted_item->id";
+    $mysqli->query($sql) || trigger_error($mysqli->error);
+}
+
+$sql = "select * from deleted_items where data_type = 'receivedNote'";
 include_once '../fns/mysqli_query_object.php';
 $deleted_items = mysqli_query_object($mysqli, $sql);
 
