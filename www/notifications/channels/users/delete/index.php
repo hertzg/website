06fcd3d607ac084862e3b4bbee855017dcb40d83
Archil Imnadze <archil@imnadze.ge>
2015-09-07 05:26:43 +0000

    correct page title on remove channel user page

--- www/notifications/channels/users/delete/index.php
+++ www/notifications/channels/users/delete/index.php
@@ -10,20 +10,25 @@ $fnsDir = '../../../../fns';
 unset($_SESSION['notifications/channels/users/messages']);
 
 $id_channels = $subscribedChannel->id_channels;
+$escaped_subscriber_username = htmlspecialchars(
+    $subscribedChannel->subscriber_username);
 
 include_once "$fnsDir/Page/confirmDialog.php";
 include_once '../fns/create_page.php';
 $content =
     create_page($mysqli, $id_channels, '../')
     .Page\confirmDialog('Are you sure you want to remove the user "<b>'
-        .htmlspecialchars($subscribedChannel->subscriber_username)
+        .$escaped_subscriber_username
         .'</b>" from the channel "<b>'
         .htmlspecialchars($subscribedChannel->channel_name)
         .'</b>"?', 'Yes, remove user', "submit.php?id=$id",
         "../?id=$id_channels");
 
+$title = "Remove User \"$escaped_subscriber_username\""
+    ." from Channel #$id_channels?";
+
 include_once "$fnsDir/compressed_css_link.php";
 include_once "$fnsDir/echo_user_page.php";
-echo_user_page($user, "Remove Channel User #$id?", $content, $base, [
+echo_user_page($user, $title, $content, $base, [
     'head' => compressed_css_link('confirmDialog', $base),
 ]);
