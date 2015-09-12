9af20b358bd86bece63dfcf464c8a02fd6794cd7
Archil Imnadze <archil@imnadze.ge>
2015-09-12 18:14:42 +0000

    shorten code

--- www/fns/Users/Files/add.php
+++ www/fns/Users/Files/add.php
@@ -16,8 +16,7 @@ function add ($mysqli, $id_users, $id_folders,
         $name, $size, $md5_sum, $sha256_sum, $insertApiKey);
 
     include_once "$fnsDir/Files/File/path.php";
-    $storagePath = \Files\File\path($id_users, $id);
-    copy($filePath, $storagePath);
+    copy($filePath, \Files\File\path($id_users, $id));
 
     include_once __DIR__.'/../addStorageUsed.php';
     \Users\addStorageUsed($mysqli, $id_users, $size);
