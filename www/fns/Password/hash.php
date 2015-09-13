3ce68eada551e2959c8909e4ab680688957ad95a
Archil Imnadze <archil@imnadze.ge>
2015-09-13 17:51:09 +0000

    fix password hashes not saved as binary strings

--- www/fns/Password/hash.php
+++ www/fns/Password/hash.php
@@ -4,6 +4,6 @@ namespace Password;
 
 function hash ($password) {
     $sha512_key = openssl_random_pseudo_bytes(64);
-    $sha512_hash = hash_hmac('sha512', $password, $sha512_key);
+    $sha512_hash = hash_hmac('sha512', $password, $sha512_key, true);
     return [$sha512_hash, $sha512_key];
 }
