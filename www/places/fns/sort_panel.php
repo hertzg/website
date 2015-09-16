f08b82ba840e76a397a2d857b1c105ace6220ff7
Archil Imnadze <archil@imnadze.ge>
2015-09-16 17:34:56 +0000

    implement sorting in places

---
+++ www/places/fns/sort_panel.php
@@ -0,0 +1,25 @@
+<?php
+
+function sort_panel ($user, $base = '') {
+
+    if ($user->num_places < 2) return;
+
+    $fnsDir = __DIR__.'/../../fns';
+
+    include_once "$fnsDir/ItemList/escapedPageQuery.php";
+    $escapedPageQuery = ItemList\escapedPageQuery();
+
+    include_once "$fnsDir/Page/imageLink.php";
+    $insertTimeLink = Page\imageLink('Created time',
+        "{$base}submit-sort-created.php$escapedPageQuery", 'sort-time');
+
+    $updateTimeLink = Page\imageLink('Last modified time',
+        "{$base}submit-sort-last-modified.php$escapedPageQuery", 'sort-time');
+
+    include_once "$fnsDir/Page/twoColumns.php";
+    $content = Page\twoColumns($updateTimeLink, $insertTimeLink);
+
+    include_once "$fnsDir/create_panel.php";
+    return create_panel('Sort Places By', $content);
+
+}
---
+++ www/places/submit-sort-created.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once '../fns/require_same_domain_referer.php';
+require_same_domain_referer('./');
+
+include_once '../fns/require_user.php';
+$user = require_user('../');
+
+include_once '../fns/Users/Places/editOrderBy.php';
+include_once '../lib/mysqli.php';
+Users\Places\editOrderBy($mysqli, $user->id_users, 'insert_time desc');
+
+unset($_SESSION['places/errors']);
+$_SESSION['places/messages'] = ['The list is now sorted by created time.'];
+
+include_once '../fns/redirect.php';
+include_once '../fns/ItemList/listUrl.php';
+redirect(ItemList\listUrl('./'));
