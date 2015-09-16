977fce77438e15541b26741bff65a84e918a61e9
Archil Imnadze <archil@imnadze.ge>
2015-09-16 09:09:24 +0000

    submit pages to change notes order by column

--- www/fns/Users/Notes/editOrderBy.php
+++ www/fns/Users/Notes/editOrderBy.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Users;
+namespace Users\Notes;
 
-function editNotesOrderBy ($mysqli, $id, $notes_order_by) {
+function editOrderBy ($mysqli, $id, $notes_order_by) {
     $sql = "update users set notes_order_by = '$notes_order_by'"
         ." where id_users = $id";
     $mysqli->query($sql) || trigger_error($mysqli->error);
