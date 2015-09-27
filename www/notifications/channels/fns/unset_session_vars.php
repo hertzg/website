a2b4cc71defdef356c7c13e3fe7c2da61bc2ea5f
Archil Imnadze <archil@imnadze.ge>
2015-09-27 13:26:21 +0000

    fix new channel page session keys

--- www/notifications/channels/fns/unset_session_vars.php
+++ www/notifications/channels/fns/unset_session_vars.php
@@ -2,8 +2,8 @@
 
 function unset_session_vars () {
     unset(
-        $_SESSION['notifications/channels/add/errors'],
-        $_SESSION['notifications/channels/add/values'],
+        $_SESSION['notifications/channels/new/errors'],
+        $_SESSION['notifications/channels/new/values'],
         $_SESSION['notifications/channels/view/messages'],
         $_SESSION['notifications/in-channel/messages'],
         $_SESSION['notifications/in-subscribed-channel/messages'],
