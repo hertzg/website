ec431165d9aad2da1715ea4c3f9710a7a5de2a2a
Archil Imnadze <archil@imnadze.ge>
2015-09-27 07:06:46 +0000

    return md5 and sha256 sums of files from api methods

--- www/api-call/file/fns/to_client_json.php
+++ www/api-call/file/fns/to_client_json.php
@@ -5,6 +5,8 @@ function to_client_json ($file) {
         'id' => (int)$file->id_files,
         'name' => $file->name,
         'size' => (int)$file->size,
+        'md5_sum' => $file->md5_sum,
+        'sha256_sum' => $file->sha256_sum,
         'insert_time' => (int)$file->insert_time,
         'rename_time' => (int)$file->rename_time,
     ];
--- www/api-call/file/received/fns/to_client_json.php
+++ www/api-call/file/received/fns/to_client_json.php
@@ -6,6 +6,8 @@ function to_client_json ($receivedFile) {
         'sender_username' => $receivedFile->sender_username,
         'name' => $receivedFile->name,
         'size' => (int)$receivedFile->size,
+        'md5_sum' => $receivedFile->md5_sum,
+        'sha256_sum' => $receivedFile->sha256_sum,
         'insert_time' => (int)$receivedFile->insert_time,
     ];
 }
