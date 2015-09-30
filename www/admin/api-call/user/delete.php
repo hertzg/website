f588191d8b8a6d645f98509a3d94a0a2bfdb051f
Archil Imnadze <archil@imnadze.ge>
2015-09-30 11:18:38 +0000

    implement user/delete admin api method

---
+++ www/admin/api-call/user/delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once '../fns/require_admin_api_key.php';
+require_admin_api_key('can_write_users', $apiKey, $mysqli);
+
+include_once 'fns/require_user.php';
+$user = require_user($mysqli);
+
+include_once '../../../fns/Users/Account/Close/close.php';
+Users\Account\Close\close($mysqli, $user);
+
+header('Content-Type: application/json');
+echo 'true';
