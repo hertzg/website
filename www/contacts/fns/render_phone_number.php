1581bb27cd858fd29c810d26cc8dc8e2a965a9d7
Archil Imnadze <archil@imnadze.ge>
2015-10-01 14:44:25 +0000

    do not remove spaces from contacts phones, thus fixing search highlighting issue

--- www/contacts/fns/render_phone_number.php
+++ www/contacts/fns/render_phone_number.php
@@ -4,13 +4,13 @@ function render_phone_number ($label, $number, &$items, $keyword = '') {
 
     if ($number === '') return;
 
-    $number = preg_replace('/\s+/', '', $number);
-
     $title = htmlspecialchars($number);
     if ($keyword !== '') {
         $regex = '/('.preg_quote(htmlspecialchars($keyword), '/').')+/i';
         $title = preg_replace($regex, '<mark>$0</mark>', $title);
     }
+
+    $number = preg_replace('/\s+/', '', $number);
 
     include_once __DIR__.'/../../fns/Form/phoneNumber.php';
     $items[] = Form\phoneNumber($label, $number, $title);
