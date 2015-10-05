24d4e3e4a185ae6501dc4701881c312b01e5174e
Archil Imnadze <archil@imnadze.ge>
2015-10-05 14:23:11 +0000

    do not query database for files when sending folder if user has no files

--- www/fns/Users/Folders/sendRenamed.php
+++ www/fns/Users/Folders/sendRenamed.php
@@ -4,24 +4,23 @@ namespace Users\Folders;
 
 function sendRenamed ($mysqli, $user, $receiver_id_users, $folder, $name) {
 
-    $id_users = $user->id_users;
     $fnsDir = __DIR__.'/../..';
 
     include_once "$fnsDir/ReceivedFolders/add.php";
-    $id_received_folders = \ReceivedFolders\add($mysqli, $id_users,
-        $user->username, $receiver_id_users, $name);
+    $id_received_folders = \ReceivedFolders\add($mysqli,
+        $user->id_users, $user->username, $receiver_id_users, $name);
 
     $copy = function ($id, $parent_id, $copy) use ($mysqli,
-        $id_received_folders, $name, $id_users, $receiver_id_users, $fnsDir) {
+        $id_received_folders, $name, $user, $receiver_id_users, $fnsDir) {
 
-        include_once "$fnsDir/Files/indexInFolder.php";
-        $files = \Files\indexInFolder($mysqli, $id);
+        include_once "$fnsDir/Users/Files/index.php";
+        $files = \Users\Files\index($mysqli, $user, $id);
 
         if ($files) {
             include_once __DIR__.'/Received/Files/add.php';
             include_once "$fnsDir/Files/File/path.php";
             foreach ($files as $file) {
-                $path = \Files\File\path($id_users, $file->id_files);
+                $path = \Files\File\path($user->id_users, $file->id_files);
                 \Users\Folders\Received\Files\add($mysqli, $id_received_folders,
                     $name, $receiver_id_users, $parent_id, $file->name,
                     $file->size, $path);
