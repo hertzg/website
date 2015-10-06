608e6730bdff71b256b15b61d80282011341d71d
Archil Imnadze <archil@imnadze.ge>
2015-10-06 12:00:44 +0000

    quiet update

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 cd `dirname $BASH_SOURCE`
 git fetch
 current=`git describe --tags --abbrev=0`
-git checkout $current
+git checkout -q $current
 next=`git tag | sort -V | grep -A 1 $current | tail -1`
 echo "Current tag: $current"
 echo "Next tag: $next"
-git checkout $next
+git checkout -q $next
 
 file=www/scripts/update.php
 if [ -f $file ]
