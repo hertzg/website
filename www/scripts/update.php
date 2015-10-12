7f514278a5cca6e4ab0b7ef129c78dbefb7b4775
Archil Imnadze <archil@imnadze.ge>
2015-10-12 12:14:07 +0000

    ensure all tables in update script

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -5,6 +5,9 @@ chdir(__DIR__);
 include_once '../../lib/cli.php';
 include_once '../lib/mysqli.php';
 
+include_once '../fns/Table/ensureAll.php';
+echo Table\ensureAll($mysqli);
+
 $sql = "update users set api_keys_order_by = 'name'";
 $mysqli->query($sql) || trigger_error($mysqli->error);
 
