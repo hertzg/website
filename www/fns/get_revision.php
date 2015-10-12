b6781ccb98ecc35ca7124b7da60dd5872ce74726
Archil Imnadze <archil@imnadze.ge>
2015-10-12 14:32:35 +0000

    fix page code item margins

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -4,7 +4,7 @@ function get_revision ($key) {
     static $revisions = [
         'css/barChart/compressed.css' => 6,
         'css/calendarIcon/compressed.css' => 3,
-        'css/common/compressed.css' => 119,
+        'css/common/compressed.css' => 120,
         'css/confirmDialog/compressed.css' => 3,
         'css/contact/compressed.css' => 4,
         'css/iconsets/compressed.css' => 93,
