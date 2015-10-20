423c22ef617f8995382cf59ec44148ea9768c342
Archil Imnadze <archil@imnadze.ge>
2015-10-20 10:52:30 +0000

    show id of deleted folder in message

--- www/files/delete-folder/submit.php
+++ www/files/delete-folder/submit.php
@@ -5,7 +5,7 @@ require_same_domain_referer('..');
 
 include_once '../fns/require_folder.php';
 include_once '../../lib/mysqli.php';
-list($folder, $id_folders, $user) = require_folder($mysqli);
+list($folder, $id, $user) = require_folder($mysqli);
 
 include_once '../../fns/Users/Folders/delete.php';
 Users\Folders\delete($mysqli, $user, $folder);
@@ -14,7 +14,7 @@ $parent_id = $folder->parent_id;
 
 unset($_SESSION['files/errors']);
 $_SESSION['files/id_folders'] = $parent_id;
-$_SESSION['files/messages'] = ['The folder has been deleted.'];
+$_SESSION['files/messages'] = ["Folder #$id has been deleted."];
 
 include_once '../../fns/create_folder_link.php';
 include_once '../../fns/redirect.php';
