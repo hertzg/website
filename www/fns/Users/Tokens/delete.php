c36f7000b9cc3f02d06472b52914d38432bd7502
Archil Imnadze <archil@imnadze.ge>
2015-10-22 10:13:41 +0000

    delete token auths on token delete

--- www/fns/Users/Tokens/delete.php
+++ www/fns/Users/Tokens/delete.php
@@ -7,6 +7,9 @@ function delete ($mysqli, $token) {
     include_once __DIR__.'/../../Tokens/delete.php';
     \Tokens\delete($mysqli, $token->id);
 
+    include_once __DIR__.'/../../TokenAuths/deleteOnToken.php';
+    \TokenAuths\deleteOnToken($mysqli, $token->id);
+
     include_once __DIR__.'/addNumber.php';
     addNumber($mysqli, $token->id_users, -1);
 
--- www/fns/Users/Tokens/deleteAll.php
+++ www/fns/Users/Tokens/deleteAll.php
@@ -7,6 +7,9 @@ function deleteAll ($mysqli, $id_users) {
     include_once __DIR__.'/../../Tokens/deleteOnUser.php';
     \Tokens\deleteOnUser($mysqli, $id_users);
 
+    include_once __DIR__.'/../../TokenAuths/deleteOnUser.php';
+    \TokenAuths\deleteOnUser($mysqli, $id_users);
+
     $sql = "update users set num_tokens = 0 where id_users = $id_users";
     $mysqli->query($sql) || trigger_error($mysqli->error);
 
