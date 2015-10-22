10beede55429260f07c656193d4b8ac0c9b21e2b
Archil Imnadze <archil@imnadze.ge>
2015-10-22 10:28:32 +0000

    save token authentication on new token creation

--- www/fns/Users/Tokens/add.php
+++ www/fns/Users/Tokens/add.php
@@ -3,11 +3,14 @@
 namespace Users\Tokens;
 
 function add ($mysqli, $id_users, $username,
-    $token_text, $access_remote_address, $user_agent) {
+    $token_text, $remote_address, $user_agent) {
 
     include_once __DIR__.'/../../Tokens/add.php';
     $id = \Tokens\add($mysqli, $id_users, $username,
-        $token_text, $access_remote_address, $user_agent);
+        $token_text, $remote_address, $user_agent);
+
+    include_once __DIR__.'/../../TokenAuths/add.php';
+    \TokenAuths\add($mysqli, $id, $id_users, $remote_address);
 
     include_once __DIR__.'/addNumber.php';
     addNumber($mysqli, $id_users, 1);
