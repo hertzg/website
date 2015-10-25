ba1cf5bd98086f471235ddf16c063ee30363777c
Archil Imnadze <archil@imnadze.ge>
2015-10-25 10:31:11 +0000

    more precise css selectors for source codes

--- www/fns/get_revision.php
+++ www/fns/get_revision.php
@@ -4,7 +4,7 @@ function get_revision ($key) {
     static $revisions = [
         'css/barChart/compressed.css' => 6,
         'css/calendarIcon/compressed.css' => 3,
-        'css/common/compressed.css' => 125,
+        'css/common/compressed.css' => 126,
         'css/confirmDialog/compressed.css' => 3,
         'css/contact/compressed.css' => 4,
         'css/iconsets/compressed.css' => 93,
