7b45eba1f9afa63cfc536400b893921bc91b9ab3
Archil Imnadze <archil@imnadze.ge>
2015-11-02 19:41:48 +0000

    fix: do not unset session vars of unreachable pages

--- www/admin/api-doc/fns/unset_session_vars.php
+++ www/admin/api-doc/fns/unset_session_vars.php
@@ -5,11 +5,6 @@ function unset_session_vars () {
     include_once __DIR__.'/../../../fns/session_start_custom.php';
     session_start_custom($new);
 
-    unset(
-        $_SESSION['admin/messages'],
-        $_SESSION['admin/api-keys/new/errors'],
-        $_SESSION['admin/api-keys/new/values'],
-        $_SESSION['admin/api-keys/view/messages']
-    );
+    unset($_SESSION['admin/messages']);
 
 }
