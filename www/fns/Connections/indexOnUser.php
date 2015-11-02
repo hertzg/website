dc01dc613c0eeff48d298a34d5d224f6e8ac28d2
Archil Imnadze <archil@imnadze.ge>
2015-11-02 19:27:58 +0000

    sort connections by address too

--- www/fns/Connections/indexOnUser.php
+++ www/fns/Connections/indexOnUser.php
@@ -4,7 +4,7 @@ namespace Connections;
 
 function indexOnUser ($mysqli, $id_users) {
     $sql = "select * from connections where id_users = $id_users"
-        .' order by username';
+        .' order by username, address';
     include_once __DIR__.'/../mysqli_query_object.php';
     return mysqli_query_object($mysqli, $sql);
 }
