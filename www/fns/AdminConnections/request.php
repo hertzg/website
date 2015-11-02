bab153681361ffba34250aa6d400e99464811203
Archil Imnadze <archil@imnadze.ge>
2015-11-02 19:31:21 +0000

    remove all spaces from admin connection address

--- www/fns/AdminConnections/request.php
+++ www/fns/AdminConnections/request.php
@@ -10,8 +10,7 @@ function request () {
     list($address, $their_exchange_api_key) = request_strings(
         'address', 'their_exchange_api_key');
 
-    include_once "$fnsDir/str_collapse_spaces.php";
-    $address = str_collapse_spaces($address);
+    $address = preg_replace('/\s+/', '', $address);
 
     include_once "$fnsDir/ConnectionAddress/maxLength.php";
     $address = mb_substr($address, 0, \ConnectionAddress\maxLength(), 'UTF-8');
