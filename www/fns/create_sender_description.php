1ed4619429c17e9bff518664b3b76cf2b3dcf0f3
Archil Imnadze <archil@imnadze.ge>
2015-11-02 10:22:28 +0000

    show sender address in received* lists

--- www/fns/create_sender_description.php
+++ www/fns/create_sender_description.php
@@ -2,6 +2,10 @@
 
 function create_sender_description ($receivedItem) {
     $username = htmlspecialchars($receivedItem->sender_username);
+    $sender_address = $receivedItem->sender_address;
+    if ($sender_address !== null) {
+        $username .= '@'.htmlspecialchars($sender_address);
+    }
     include_once __DIR__.'/export_date_ago.php';
     return "From $username ".export_date_ago($receivedItem->insert_time).'.';
 }
