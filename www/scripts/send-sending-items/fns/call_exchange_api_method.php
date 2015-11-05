68b9c6950a6a1a4caad3f9d9de4ae311350897c7
Archil Imnadze <archil@imnadze.ge>
2015-11-05 11:15:47 +0000

    ignore RECEIVER_NOT_RECEIVING error when sending

--- www/scripts/send-sending-items/fns/call_exchange_api_method.php
+++ www/scripts/send-sending-items/fns/call_exchange_api_method.php
@@ -16,12 +16,24 @@ function call_exchange_api_method ($method, $receiver_address, $params) {
     }
 
     if (curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
+        $object = json_decode($response);
+        if ($object === 'RECEIVER_NOT_RECEIVING') {
+            // TODO do not ignore this event
+            return;
+        } else {
+            echo "ERROR: $response\n";
+            exit(1);
+        }
+    }
+
+    $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    if ($contentType !== 'application/json') {
         echo "ERROR: $response\n";
         exit(1);
     }
 
-    $response = json_decode($response);
-    if ($response !== true) {
+    $object = json_decode($response);
+    if ($object !== true) {
         echo "ERROR: $response\n";
         exit(1);
     }
