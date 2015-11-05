e8d8384676038037d546b97678d2246b937da865
Archil Imnadze <archil@imnadze.ge>
2015-11-05 12:37:46 +0000

    script to create next version for publishing

---
+++ tag-next-version.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php
+
+chdir(__DIR__);
+exec('git tag | grep ^v | sort --version-sort | tail -1', $lines);
+preg_match('/^v(\d+)$/', $lines[0], $match);
+$next = 'v'.($match[1] + 1);
+exec("git tag -a -m $next $next");
