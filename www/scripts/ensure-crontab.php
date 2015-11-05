f5bf775e398185df69a8b60ac9e1e6c3c81dc2be
Archil Imnadze <archil@imnadze.ge>
2015-11-05 10:31:17 +0000

    update script to write new crontab

---
+++ www/scripts/ensure-crontab.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once '../fns/write_crontab.php';
+write_crontab();
+
+echo "Done\n";
--- www/scripts/update.php
+++ www/scripts/update.php
@@ -8,4 +8,13 @@ include_once '../lib/mysqli.php';
 include_once '../fns/Table/ensureAll.php';
 echo Table\ensureAll($mysqli);
 
+include_once '../fns/DomainName/get.php';
+include_once '../fns/SiteBase/get.php';
+include_once '../fns/SiteProtocol/get.php';
+$url = SiteProtocol\get().'://'.DomainName\get().SiteBase\get().'scripts/ensure-crontab.php';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_exec($ch);
+
 echo "Done\n";
