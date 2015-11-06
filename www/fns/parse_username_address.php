7eb2336808487b4270383478bd1a30889fb398d1
Archil Imnadze <archil@imnadze.ge>
2015-11-06 08:55:28 +0000

    do not consider empty username and empty address as a username-address pair

--- www/fns/parse_username_address.php
+++ www/fns/parse_username_address.php
@@ -1,7 +1,7 @@
 <?php
 
 function parse_username_address ($raw_username, &$username, &$address) {
-    if (preg_match('/^(.*?)@(.*)$/', $raw_username, $match)) {
+    if (preg_match('/^(.+?)@(.+)$/', $raw_username, $match)) {
         $username = $match[1];
         $address = $match[2];
     } else {
