80f36bc9cbe907a371d1fc713d052953124c5255
Archil Imnadze <archil@imnadze.ge>
2015-11-07 17:26:53 +0000

    better layout of form notes

--- www/fns/Form/notes.php
+++ www/fns/Form/notes.php
@@ -4,7 +4,12 @@ namespace Form;
 
 function notes ($notes) {
     $ul = '<ul class="form-notes">';
-    foreach ($notes as $note) $ul .= "<li>$note</li>";
+    foreach ($notes as $note) {
+        $ul .=
+            '<li class="form-notes-item">'
+                ."<span class=\"form-notes-item-bullet\"></span>$note"
+            .'</li>';
+    }
     $ul .= '</ul>';
     include_once __DIR__.'/association.php';
     return association($ul, '');
