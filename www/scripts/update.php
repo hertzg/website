ae0eccd86fd0443087c8543a9f211754e8a99bf4
Archil Imnadze <archil@imnadze.ge>
2015-11-11 12:17:39 +0000

    update old sort fields with new

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -8,6 +8,34 @@ include_once '../lib/mysqli.php';
 include_once '../fns/Table/ensureAll.php';
 echo Table\ensureAll($mysqli);
 
+$sql = "update users set bar_charts_order_by = 'name, insert_time desc'"
+    ." where bar_charts_order_by = 'name'";
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
+$sql = "update users set bookmarks_order_by = 'title, insert_time desc'"
+    ." where bookmarks_order_by = 'title'";
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
+$sql = "update users set contacts_order_by = 'full_name, insert_time desc'"
+    ." where contacts_order_by = 'full_name'";
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
+$sql = "update users set events_order_by = 'event_time desc, start_hour, start_minute, insert_time desc'"
+    ." where events_order_by = 'event_time desc'";
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
+$sql = "update users set places_order_by = 'name, insert_time desc'"
+    ." where places_order_by = 'name'";
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
+$sql = "update users set wallets_order_by = 'name, insert_time desc'"
+    ." where wallets_order_by = 'name'";
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
+$sql = "update users set wallets_order_by = 'balance, insert_time desc'"
+    ." where wallets_order_by = 'balance'";
+$mysqli->query($sql) || trigger_error($mysqli->error);
+
 /*
 include_once '../fns/DomainName/get.php';
 include_once '../fns/SiteBase/get.php';
