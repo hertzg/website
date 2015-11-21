5f8caa3e713d39770ddb00948bce2b4391ca6561
Archil Imnadze <archil@imnadze.ge>
2015-11-21 16:25:52 +0000

    treat recipients with local connection address as local users

--- www/fns/SendForm/requestUsernameAddress.php
+++ www/fns/SendForm/requestUsernameAddress.php
@@ -13,4 +13,22 @@ function requestUsernameAddress (&$username, &$parsed_username, &$address) {
     include_once "$fnsDir/parse_username_address.php";
     parse_username_address($username, $parsed_username, $address);
 
+    if ($address !== null) {
+
+        include_once "$fnsDir/SiteProtocol/get.php";
+        $siteProtocol = \SiteProtocol\get();
+
+        include_once "$fnsDir/DomainName/get.php";
+        $domainName = \DomainName\get();
+
+        include_once "$fnsDir/SiteBase/get.php";
+        $siteBase = \SiteBase\get();
+
+        if ($address === "$siteProtocol://$domainName$siteBase") {
+            $username = $parsed_username;
+            $address = null;
+        }
+
+    }
+
 }
