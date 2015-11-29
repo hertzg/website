0e8e2b00159f297dfa0ea86790c8ac4bade5c1b9
Archil Imnadze <archil@imnadze.ge>
2015-11-29 11:20:50 +0000

    cleanup update script

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -8,18 +8,6 @@ include_once '../lib/mysqli.php';
 include_once '../fns/Table/ensureAll.php';
 echo Table\ensureAll($mysqli);
 
-include_once '../fns/Users/index.php';
-$users = Users\index($mysqli);
-
-foreach ($users as $user) {
-    $order_home_items = json_decode($user->order_home_items);
-    $index = array_search('notifications', $order_home_items);
-    array_splice($order_home_items, $index + 1, 0, 'post-notification');
-    $order_home_items = $mysqli->real_escape_string(json_encode($order_home_items));
-    $sql = "update users set order_home_items = '$order_home_items'"
-        ." where id_users = $user->id_users";
-    $mysqli->query($sql) || trigger_error($mysqli->error);
-}
 /*
 include_once '../fns/get_absolute_base.php';
 $url = get_absolute_base().'scripts/ensure-crontab.php';
