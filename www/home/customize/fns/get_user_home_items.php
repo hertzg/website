8f9b49b7ea2c5c2cedc070971c0139ac94a3ad1d
Archil Imnadze <archil@imnadze.ge>
2015-12-09 14:23:27 +0000

    ignore unknown home items to ease up developing new home items

--- www/home/customize/fns/get_user_home_items.php
+++ www/home/customize/fns/get_user_home_items.php
@@ -4,7 +4,9 @@ function get_user_home_items ($homeItems, $user) {
     $order_home_items = json_decode($user->order_home_items);
     $userHomeItems = [];
     foreach ($order_home_items as $key) {
-        $userHomeItems[$key] = $homeItems[$key];
+        if (array_key_exists($key, $homeItems)) {
+            $userHomeItems[$key] = $homeItems[$key];
+        }
     }
     return $userHomeItems;
 }
