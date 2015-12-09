9a4a45e92765e2f4c4ace97f8917c74db126d0f0
Archil Imnadze <archil@imnadze.ge>
2015-12-09 09:06:21 +0000

    write calculation description creation on one function

--- www/calculations/fns/SearchPage/renderCalculations.php
+++ www/calculations/fns/SearchPage/renderCalculations.php
@@ -10,6 +10,7 @@ function renderCalculations ($calculations, &$items, $params, $keyword) {
 
         $regex = '/('.preg_quote(htmlspecialchars($keyword), '/').')+/i';
 
+        include_once __DIR__.'/../create_description.php';
         include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
         foreach ($calculations as $calculation) {
 
@@ -25,13 +26,7 @@ function renderCalculations ($calculations, &$items, $params, $keyword) {
             $title = htmlspecialchars($calculation->title);
             $title = preg_replace($regex, '<mark>$0</mark>', $title);
 
-            $value = $calculation->value;
-            if ($value === null) {
-                $description =
-                    '<span class="colorText red">Uncomputable.</span>';
-            } else {
-                $description = number_format($value, 2);
-            }
+            $description = create_description($calculation);
 
             $items[] = \Page\imageArrowLinkWithDescription(
                 $title, $description, $href, 'calculation', $options);
--- www/calculations/fns/render_calculations.php
+++ www/calculations/fns/render_calculations.php
@@ -5,6 +5,7 @@ function render_calculations ($calculations, &$items, $params, $base = '') {
     $fnsDir = __DIR__.'/../../fns';
 
     if ($calculations) {
+        include_once __DIR__.'/create_description.php';
         include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
         $icon = 'calculation';
         foreach ($calculations as $calculation) {
@@ -22,13 +23,8 @@ function render_calculations ($calculations, &$items, $params, $base = '') {
             if ($title === '') $title = $calculation->expression;
             $title = htmlspecialchars($title);
 
-            $value = $calculation->value;
-            if ($value === null) {
-                $description =
-                    '<span class="colorText red">Uncomputable.</span>';
-            } else {
-                $description = number_format($value, 2);
-            }
+            $description = create_description($calculation);
+
             $items[] = Page\imageArrowLinkWithDescription(
                 $title, $description, $href, $icon, $options);
 
