2f0d0e74fe6bec5879d7eb8055b9bec618dbb776
Archil Imnadze <archil@imnadze.ge>
2015-12-11 10:36:44 +0000

    assume default value of $value_of null in test cases

--- www/tests/evaluate/fns/visual_assert.php
+++ www/tests/evaluate/fns/visual_assert.php
@@ -2,13 +2,20 @@
 
 function visual_assert ($config) {
 
-    extract($config);
+    $expression = $config['expression'];
+    $expected_value = $config['expected_value'];
+    $expected_pretty_expression = $config['expected_pretty_expression'];
+
+    if (array_key_exists('value_of', $config)) $value_of = $config['value_of'];
+    else $value_of = null;
 
     $result = evaluate($expression, $error,
         $error_char, $pretty_expression, $value_of);
 
     $failed = $result !== $expected_value;
-    echo 'evaluate('.var_export($expression, true).') returned '.var_export($result, true);
+    if ($failed) echo 'FAIL: ';
+    echo 'evaluate('.var_export($expression, true).')'
+        .' returned '.var_export($result, true);
     if ($failed) echo ' instead of '.var_export($expected_value, true);
     echo "\n";
     if ($pretty_expression !== $expected_pretty_expression) {
--- www/tests/evaluate/index.php
+++ www/tests/evaluate/index.php
@@ -8,349 +8,291 @@ visual_assert([
     'expression' => '',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => ' ',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '  ',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '   ',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => 'a',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '+',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '-',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '*',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '/',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '(',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '(2',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => ')',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2)',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '()',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + ()',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + (( ))',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 3',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 +',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + 3 +',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2(3)',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 (3)',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + (3 (4))',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + (3) (4)',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '0',
     'expected_value' => 0.0,
     'expected_pretty_expression' => '0',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2',
     'expected_value' => 2.0,
     'expected_pretty_expression' => '2',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2.5',
     'expected_value' => 2.5,
     'expected_pretty_expression' => '2.5',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2.5 + 1.5',
     'expected_value' => 4.0,
     'expected_pretty_expression' => '2.5 + 1.5',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '23',
     'expected_value' => 23.0,
     'expected_pretty_expression' => '23',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '-2',
     'expected_value' => -2.0,
     'expected_pretty_expression' => '-2',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '- 2',
     'expected_value' => -2.0,
     'expected_pretty_expression' => '-2',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '+2',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '+ 2',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '3 * (-2)',
     'expected_value' => -6.0,
     'expected_pretty_expression' => '3 * (-2)',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '3 * (+2)',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + + 3',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + - 3',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + - - 3',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 * / 3',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '3 * - 2',
     'expected_value' => -6.0,
     'expected_pretty_expression' => '3 * -2',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '1 + 2 + 3 - 4 - 5',
     'expected_value' => -3.0,
     'expected_pretty_expression' => '1 + 2 + 3 - 4 - 5',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 * 3',
     'expected_value' => 6.0,
     'expected_pretty_expression' => '2 * 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '3 * (- 2 - 3)',
     'expected_value' => -15.0,
     'expected_pretty_expression' => '3 * (-2 - 3)',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '1 + 2 + 3 + 4 + 5',
     'expected_value' => 15.0,
     'expected_pretty_expression' => '1 + 2 + 3 + 4 + 5',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '5 - 2',
     'expected_value' => 3.0,
     'expected_pretty_expression' => '5 - 2',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '1 + 2 * 3',
     'expected_value' => 7.0,
     'expected_pretty_expression' => '1 + 2 * 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 * 3 + 4',
     'expected_value' => 10.0,
     'expected_pretty_expression' => '2 * 3 + 4',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '6 / 3',
     'expected_value' => 2.0,
     'expected_pretty_expression' => '6 / 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '5 - 6 / 3',
     'expected_value' => 3.0,
     'expected_pretty_expression' => '5 - 6 / 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '10 / 2 * 3',
     'expected_value' => 15.0,
     'expected_pretty_expression' => '10 / 2 * 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 * (3 + 4)',
     'expected_value' => 14.0,
     'expected_pretty_expression' => '2 * (3 + 4)',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 * (3 + 4) + 3',
     'expected_value' => 17.0,
     'expected_pretty_expression' => '2 * (3 + 4) + 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 * (3 + (7 - 1) / 2) + 3',
     'expected_value' => 15.0,
     'expected_pretty_expression' => '2 * (3 + (7 - 1) / 2) + 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '   2  *    (  3  +  ( 7-1)/2)+ 3',
     'expected_value' => 15.0,
     'expected_pretty_expression' => '2 * (3 + (7 - 1) / 2) + 3',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '40 / ((7 - 3) * 2)',
     'expected_value' => 5.0,
     'expected_pretty_expression' => '40 / ((7 - 3) * 2)',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '40 / (4 * (5 - 3))',
     'expected_value' => 5.0,
     'expected_pretty_expression' => '40 / (4 * (5 - 3))',
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + 1 / 0 - 5',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 / (4 - 4)',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + #',
     'expected_value' => false,
     'expected_pretty_expression' => null,
-    'value_of' => null,
 ]);
 visual_assert([
     'expression' => '2 + #123',
