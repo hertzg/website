9d15b3e1bbef27f570c3c2d4f1a68dcb45610b94
Archil Imnadze <archil@imnadze.ge>
2015-12-23 08:32:46 +0000

    the theme based icons in webapp manifest

--- www/fns/page_icon_links.php
+++ www/fns/page_icon_links.php
@@ -1,23 +1,29 @@
 <?php
 
 function page_icon_links ($theme_color, $base = '') {
-    $theme_images = "{$base}theme/color/$theme_color/images";
+
+    include_once __DIR__.'/get_revision.php';
+    $get_icon_href = function ($size) use ($base, $theme_color) {
+        $href = "theme/color/$theme_color/images/icon$size.png";
+        return "$base$href?".get_revision($href);
+    };
+
     return '<link rel="icon" type="image/png"'
-        ." href=\"$theme_images/icon16.png?3\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="32x32"'
-        ." href=\"$theme_images/icon32.png?3\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="48x48"'
-        ." href=\"$theme_images/icon48.png?2\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="64x64"'
-        ." href=\"$theme_images/icon64.png?2\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="90x90"'
-        ." href=\"$theme_images/icon90.png?2\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="120x120"'
-        ." href=\"$theme_images/icon120.png?2\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="128x128"'
-        ." href=\"$theme_images/icon128.png?2\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="256x256"'
-        ." href=\"$theme_images/icon256.png?2\" />"
+        .' href="'.$get_icon_href('16').'" />'
         .'<link rel="icon" type="image/png" sizes="512x512"'
-        ." href=\"$theme_images/icon512.png?2\" />";
+        .' href="'.$get_icon_href('16').'" />';
 }
--- www/webapp-manifest/index.php
+++ www/webapp-manifest/index.php
@@ -7,10 +7,18 @@ $description =
     'Save your data in Zvini. Save your files, contacts, notes and more.'
     .' It\'s free and easy.';
 
+include_once '../fns/Theme/Color/getDefault.php';
+$theme_color = Theme\Color\getDefault();
+
+include_once '../fns/get_revision.php';
+$get_icon_href = function ($size) use ($siteBase, $theme_color) {
+    $src = "theme/color/$theme_color/images/icon$size.png";
+    return $src.get_revision($src);
+};
+
 header('Content-Type: application/x-web-app-manifest+json');
 
 include_once '../fns/get_absolute_base.php';
-include_once '../fns/get_revision.php';
 include_once '../fns/SiteTitle/get.php';
 echo json_encode([
     'name' => SiteTitle\get(),
@@ -23,17 +31,15 @@ echo json_encode([
         'url' => get_absolute_base(),
     ],
     'icons' => [
-        '16' => "{$siteBase}images/icons/16.png?"
-            .get_revision('images/icons/16.png'),
-        '32' => "{$siteBase}images/icons/32.png?"
-            .get_revision('images/icons/32.png'),
-        '48' => "{$siteBase}images/icons/48.png?2",
-        '64' => "{$siteBase}images/icons/64.png?2",
-        '90' => "{$siteBase}images/icons/90.png?2",
-        '120' => "{$siteBase}images/icons/120.png?2",
-        '128' => "{$siteBase}images/icons/128.png?2",
-        '256' => "{$siteBase}images/icons/256.png?2",
-        '512' => "{$siteBase}images/icons/512.png?1",
+        '16' => $get_icon_href('16'),
+        '32' => $get_icon_href('32'),
+        '48' => $get_icon_href('48'),
+        '64' => $get_icon_href('64'),
+        '90' => $get_icon_href('90'),
+        '120' => $get_icon_href('120'),
+        '128' => $get_icon_href('128'),
+        '256' => $get_icon_href('256'),
+        '512' => $get_icon_href('512'),
     ],
     'permissions' => [
         'geolocation' => [
