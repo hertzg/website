f2f12ba8ce5610eac640851b6a25c39b0ad153a3
Archil Imnadze <archil@imnadze.ge>
2015-12-24 07:59:35 +0000

    invert theme colors for <mark> backgrounds

--- www/fns/page_theme_links.php
+++ www/fns/page_theme_links.php
@@ -1,8 +1,10 @@
 <?php
 
 function page_theme_links ($theme_color, $theme_brightness, $base = '') {
+    include_once __DIR__.'/get_revision.php';
+    $color_href = "theme/color/$theme_color/common.css";
     return '<link rel="stylesheet" type="text/css"'
-        ." href=\"{$base}theme/color/$theme_color/common.css?32\" />"
+        ." href=\"{$base}$color_href?".get_revision($color_href).'" />'
         .'<link rel="stylesheet" type="text/css"'
         ." href=\"{$base}theme/brightness/$theme_brightness/common.css?7\" />";
 }
