9d91cd247f4d50bc77d33fdc654e8992b318d67d
Archil Imnadze <archil@imnadze.ge>
2015-12-30 13:48:21 +0000

    cleanup update script

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -8,9 +8,4 @@ include_once '../lib/mysqli.php';
 include_once '../fns/Table/ensureAll.php';
 echo Table\ensureAll($mysqli);
 
-$sql = 'update users set events_order_by = '
-    ."'event_time desc, start_hour, start_minute, insert_time desc'"
-    ." where events_order_by = 'event_time desc'";
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
 echo "Done\n";
