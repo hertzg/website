2161fc99f0ae294aa62fe334f480ac734910e8d4
Archil Imnadze <archil@imnadze.ge>
2016-01-21 01:24:05 -0900

    show that history is empty for old items

--- www/tasks/history/index.php
+++ www/tasks/history/index.php
@@ -13,15 +13,21 @@ include_once "$fnsDir/TaskRevisions/indexOnTask.php";
 include_once '../../lib/mysqli.php';
 $revisions = TaskRevisions\indexOnTask($mysqli, $id);
 
-$items = [];
-include_once "$fnsDir/export_date_ago.php";
-include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
-foreach ($revisions as $revision) {
-    $item_id = $revision->id;
-    $items[] = Page\imageArrowLinkWithDescription(
-        export_date_ago($revision->insert_time, true),
-        'R'.($revision->revision + 1), "view/?id=$item_id",
-        'restore-defaults', ['id' => $item_id]);
+if ($revisions) {
+    $items = [];
+    include_once "$fnsDir/export_date_ago.php";
+    include_once "$fnsDir/Page/imageArrowLinkWithDescription.php";
+    foreach ($revisions as $revision) {
+        $item_id = $revision->id;
+        $items[] = Page\imageArrowLinkWithDescription(
+            export_date_ago($revision->insert_time, true),
+            'R'.($revision->revision + 1), "view/?id=$item_id",
+            'restore-defaults', ['id' => $item_id]);
+    }
+    $content = join('<div class="hr"></div>', $items);
+} else {
+    include_once "$fnsDir/Page/info.php";
+    $content = Page\info('History is empty');
 }
 
 include_once "$fnsDir/Page/create.php";
@@ -31,7 +37,7 @@ $content = \Page\create(
         'href' => "../view/?id=$id#history",
     ],
     'History',
-    join('<div class="hr"></div>', $items)
+    $content
 );
 
 include_once "$fnsDir/compressed_js_script.php";
