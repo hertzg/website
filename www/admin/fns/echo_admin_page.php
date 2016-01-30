f8b51848d1b1e78405fbf511a9ffd966c217442e
Archil Imnadze <archil@imnadze.ge>
2016-01-30 11:33:12 +0000

    return to home page from admin panel when signed in with session

--- www/admin/fns/echo_admin_page.php
+++ www/admin/fns/echo_admin_page.php
@@ -3,7 +3,9 @@
 function echo_admin_page ($user, $title,
     $content, $admin_base, $options = []) {
 
-    $options['logo_href'] = $admin_base;
+    $logo_href = $admin_base;
+    if ($user) $logo_href .= '../home/';
+    $options['logo_href'] = $logo_href;
 
     include_once __DIR__.'/../../fns/echo_page.php';
     echo_page($user, $title, $content, "$admin_base../", $options);
--- www/admin/index.php
+++ www/admin/index.php
@@ -22,31 +22,42 @@ include_once 'fns/create_general_info_link.php';
 include_once 'fns/create_mysql_link.php';
 include_once '../fns/create_panel.php';
 include_once '../fns/Page/sessionMessages.php';
-include_once '../fns/Page/title.php';
-$content =
-    Page\title(
+$content = Page\sessionMessages('admin/messages')
+    .create_database_links($mysqli)
+    .create_general_info_link()
+    .'<div class="hr"></div>'
+    .create_mysql_link($mysqli)
+    .'<div class="hr"></div>'
+    .Page\imageArrowLink('Ensure Crontab',
+        'ensure-crontab/', 'generic', ['id' => 'ensure-crontab'])
+    .'<div class="hr"></div>'
+    .Page\imageArrowLink('Ensure Data Folder',
+        'ensure-data-dir/', 'generic', ['id' => 'ensure-data-dir'])
+    .'<div class="hr"></div>'
+    .Page\imageArrowLink('Check Installation',
+        'check-installation/', 'generic', ['id' => 'check-installation'])
+    .'<div class="hr"></div>'
+    .Page\imageArrowLink('Set New Username/Password',
+        'username-password/', 'generic', ['id' => 'username-password']);
+
+if ($admin_user) {
+    include_once '../fns/Page/create.php';
+    $content = Page\create(
+        [
+            'title' => 'Home',
+            'href' => '../home/#admin',
+        ],
         'Administration',
-        Page\sessionMessages('admin/messages')
-        .create_database_links($mysqli)
-        .create_general_info_link()
-        .'<div class="hr"></div>'
-        .create_mysql_link($mysqli)
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Ensure Crontab',
-            'ensure-crontab/', 'generic', ['id' => 'ensure-crontab'])
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Ensure Data Folder',
-            'ensure-data-dir/', 'generic', ['id' => 'ensure-data-dir'])
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Check Installation',
-            'check-installation/', 'generic', ['id' => 'check-installation'])
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Set New Username/Password',
-            'username-password/', 'generic', ['id' => 'username-password'])
-    )
-    .create_panel('Options', $helpLink);
+        $content
+    );
+} else {
+    include_once '../fns/Page/title.php';
+    $content = Page\title('Administration', $content);
+}
+
+$content .= create_panel('Options', $helpLink);
 
 include_once '../fns/echo_page.php';
 echo_page($admin_user, 'Administration', $content, '../', [
-    'logo_href' => './',
+    'logo_href' => $admin_user ? '../home/' : './',
 ]);
