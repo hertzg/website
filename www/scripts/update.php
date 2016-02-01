2b96277c8d8aa527d72c8df424048de493a23ac1
Archil Imnadze <archil@imnadze.ge>
2016-02-01 18:26:59 +0400

    cleanup update script

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -5,22 +5,7 @@ chdir(__DIR__);
 include_once '../../lib/cli.php';
 include_once '../lib/mysqli.php';
 
-include_once '../fns/Table/ensureAll.php';
-echo Table\ensureAll($mysqli);
-
-$sql = 'update users set show_admin = 1';
-$mysqli->query($sql) || trigger_error($mysqli->error);
-
-include_once '../fns/Users/index.php';
-$users = Users\index($mysqli);
-
-foreach ($users as $user) {
-    $order_home_items = json_decode($user->order_home_items);
-    array_shift($order_home_items);
-    $order_home_items = $mysqli->real_escape_string(json_encode($order_home_items));
-    $sql = "update users set order_home_items = '$order_home_items'"
-        ." where id_users = $user->id_users";
-    $mysqli->query($sql) || trigger_error($mysqli->error);
-}
+//include_once '../fns/Table/ensureAll.php';
+//echo Table\ensureAll($mysqli);
 
 echo "Done\n";
