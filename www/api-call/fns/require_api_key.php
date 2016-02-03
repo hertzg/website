02a5f1dcfc454896839dca451da8a7b3bf868bef
Archil Imnadze <archil@imnadze.ge>
2016-02-03 18:51:40 +0400

    ApiCall\requireUser

--- www/api-call/fns/require_api_key.php
+++ www/api-call/fns/require_api_key.php
@@ -12,24 +12,8 @@ function require_api_key ($permission_field, &$apiKey, &$user, &$mysqli) {
 
     if ($session_auth) {
 
-        include_once "$fnsDir/is_same_domain_referer.php";
-        if (!is_same_domain_referer()) {
-            include_once "$fnsDir/ErrorJson/forbidden.php";
-            ErrorJson\forbidden('"CROSS_DOMAIN_REQUEST"');
-        }
-
-        include_once "$fnsDir/signed_user.php";
-        $user = signed_user();
-
-        if (!$user) {
-            include_once "$fnsDir/ErrorJson/forbidden.php";
-            ErrorJson\forbidden('"NOT_SIGNED_IN"');
-        }
-
-        if ($user->disabled) {
-            include_once "$fnsDir/ErrorJson/forbidden.php";
-            ErrorJson\forbidden('"USER_DISABLED"');
-        }
+        include_once "$fnsDir/ApiCall/requireUser.php";
+        $user = ApiCall\requireUser();
 
         $apiKey = null;
 
