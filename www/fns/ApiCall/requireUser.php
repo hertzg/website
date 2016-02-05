8fa3c6aac3be73f836bcb6269776653da5671891
Archil Imnadze <archil@imnadze.ge>
2016-02-05 15:42:47 +0400

    fix showing of errors from ApiCall\requireUser

--- www/fns/ApiCall/requireUser.php
+++ www/fns/ApiCall/requireUser.php
@@ -9,7 +9,7 @@ function requireUser () {
     include_once "$fnsDir/is_same_domain_referer.php";
     if (!is_same_domain_referer()) {
         include_once "$fnsDir/ErrorJson/forbidden.php";
-        ErrorJson\forbidden('"CROSS_DOMAIN_REQUEST"');
+        \ErrorJson\forbidden('"CROSS_DOMAIN_REQUEST"');
     }
 
     include_once "$fnsDir/signed_user.php";
@@ -17,12 +17,12 @@ function requireUser () {
 
     if (!$user) {
         include_once "$fnsDir/ErrorJson/forbidden.php";
-        ErrorJson\forbidden('"NOT_SIGNED_IN"');
+        \ErrorJson\forbidden('"NOT_SIGNED_IN"');
     }
 
     if ($user->disabled) {
         include_once "$fnsDir/ErrorJson/forbidden.php";
-        ErrorJson\forbidden('"USER_DISABLED"');
+        \ErrorJson\forbidden('"USER_DISABLED"');
     }
 
     return $user;
