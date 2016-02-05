4b5260f40969c23053cf793afeacd75c810c52be
Archil Imnadze <archil@imnadze.ge>
2016-02-05 16:42:22 +0400

    fix aborting of script loading

--- www/js/localNavigation/LoadScript.js
+++ www/js/localNavigation/LoadScript.js
@@ -1,15 +1,17 @@
 function LoadScript (src, loadCallback, errorCallback) {
 
     console.log('LoadScript', src)
+
     var script = document.createElement('script')
     script.src = src
     script.onload = loadCallback
     script.onerror = errorCallback
+
     document.body.appendChild(script)
 
     return {
         abort: function () {
-            script.onload = null
+            script.onload = script.onerror = null
         },
     }
 
--- www/js/localNavigation/combined.js
+++ www/js/localNavigation/combined.js
@@ -51,15 +51,17 @@ function LoadPage (base, href, loader, callback) {
 function LoadScript (src, loadCallback, errorCallback) {
 
     console.log('LoadScript', src)
+
     var script = document.createElement('script')
     script.src = src
     script.onload = loadCallback
     script.onerror = errorCallback
+
     document.body.appendChild(script)
 
     return {
         abort: function () {
-            script.onload = null
+            script.onload = script.onerror = null
         },
     }
 
