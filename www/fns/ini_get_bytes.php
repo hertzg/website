3c7251409fc3e7d82166c16d3b29b3379ae47c40
Archil Imnadze <archil@imnadze.ge>
2016-02-08 16:40:23 +0400

    support case-insensitive parsing of php.ini byte values

--- www/fns/ini_get_bytes.php
+++ www/fns/ini_get_bytes.php
@@ -2,15 +2,12 @@
 
 function ini_get_bytes ($varname) {
     $value = ini_get($varname);
-    if (preg_match('/^(\d+)([KMG])$/', $value, $match)) {
-        if ($match[2] == 'K') {
-            $multiplier = 1024;
-        } elseif ($match[2] == 'M') {
-            $multiplier = 1024 * 1024;
-        } else {
-            $multiplier = 1024 * 1024 * 1024;
-        }
+    if (preg_match('/^(\d+)([kmg])$/i', $value, $match)) {
+        $suffix = strtolower($match[2]);
+        if ($suffix === 'k') $multiplier = 1024;
+        elseif ($suffix === 'm') $multiplier = 1024 * 1024;
+        else $multiplier = 1024 * 1024 * 1024;
         $value = $match[1] * $multiplier;
     }
-    return $value + 0;
+    return (int)$value;
 }
