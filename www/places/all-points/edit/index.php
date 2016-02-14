e44fc8ed82152f44893055a9902991c214b111c4
Archil Imnadze <archil@imnadze.ge>
2016-02-14 06:59:33 -0500

    add geolocation dialogs on point forms under all-points page

--- www/places/all-points/edit/index.php
+++ www/places/all-points/edit/index.php
@@ -4,6 +4,7 @@ include_once '../../fns/require_point.php';
 include_once '../../../lib/mysqli.php';
 list($point, $id, $user) = require_point($mysqli, '../');
 
+$base = '../../../';
 $fnsDir = '../../../fns';
 
 include_once '../../fns/request_edit_point_values.php';
@@ -11,6 +12,7 @@ $values = request_edit_point_values($point, 'places/all-points/edit/values');
 
 unset($_SESSION['places/all-points/view/messages']);
 
+include_once '../../fns/create_geolocation_panel.php';
 include_once '../../fns/create_point_form_items.php';
 include_once "$fnsDir/Form/button.php";
 include_once "$fnsDir/ItemList/escapedItemQuery.php";
@@ -18,23 +20,26 @@ include_once "$fnsDir/ItemList/itemHiddenInputs.php";
 include_once "$fnsDir/Page/create.php";
 include_once "$fnsDir/Page/sessionErrors.php";
 include_once "$fnsDir/Page/warnings.php";
-$content = Page\create(
-    [
-        'title' => "Point #$id",
-        'href' => '../view/'.ItemList\escapedItemQuery($id).'#edit',
-    ],
-    'Edit',
-    Page\sessionErrors('places/all-points/edit/errors')
-    .Page\warnings(['Editing the point will update the latitude,'
-        .' the longitude and the altitude of the place'
-        .' to the avarage of all the points.'])
-    .'<form action="submit.php" method="post">'
-        .create_point_form_items($values)
-        .'<div class="hr"></div>'
-        .Form\button('Save Changes')
-        .ItemList\itemHiddenInputs($id)
-    .'</form>'
-);
+$content =
+    Page\create(
+        [
+            'title' => "Point #$id",
+            'href' => '../view/'.ItemList\escapedItemQuery($id).'#edit',
+        ],
+        'Edit',
+        Page\sessionErrors('places/all-points/edit/errors')
+        .Page\warnings(['Editing the point will update the latitude,'
+            .' the longitude and the altitude of the place'
+            .' to the avarage of all the points.'])
+        .'<form action="submit.php" method="post">'
+            .create_point_form_items($values)
+            .'<div class="hr"></div>'
+            .Form\button('Save Changes')
+            .ItemList\itemHiddenInputs($id)
+        .'</form>'
+    )
+    .create_geolocation_panel($scripts, $base, (float)$point->latitude,
+        (float)$point->longitude, (float)$point->altitude);
 
 include_once "$fnsDir/echo_user_page.php";
-echo_user_page($user, "Edit Point #$id", $content, '../../../');
+echo_user_page($user, "Edit Point #$id", $content, $base, ['scripts' => $scripts]);
--- www/places/all-points/new/index.php
+++ www/places/all-points/new/index.php
@@ -15,6 +15,7 @@ $values = request_new_point_values('places/all-points/new/values');
 $base = '../../../';
 $fnsDir = '../../../fns';
 
+include_once '../../fns/create_geolocation_panel.php';
 include_once '../../fns/create_point_form_items.php';
 include_once "$fnsDir/Form/button.php";
 include_once "$fnsDir/ItemList/escapedItemQuery.php";
@@ -22,23 +23,27 @@ include_once "$fnsDir/ItemList/itemHiddenInputs.php";
 include_once "$fnsDir/Page/create.php";
 include_once "$fnsDir/Page/sessionErrors.php";
 include_once "$fnsDir/Page/warnings.php";
-$content = Page\create(
-    [
-        'title' => 'All Points',
-        'href' => '../'.ItemList\escapedItemQuery($id),
-    ],
-    'Add New',
-    Page\sessionErrors('places/all-points/new/errors')
-    .Page\warnings(['Adding a new point will update the latitude,'
-        .' the longitude and the altitude of the place'
-        .' to the avarage of all the points.'])
-    .'<form action="submit.php" method="post">'
-        .create_point_form_items($values)
-        .'<div class="hr"></div>'
-        .Form\button('Save Point')
-        .ItemList\itemHiddenInputs($id)
-    .'</form>'
-);
+$content =
+    Page\create(
+        [
+            'title' => 'All Points',
+            'href' => '../'.ItemList\escapedItemQuery($id),
+        ],
+        'Add New',
+        Page\sessionErrors('places/all-points/new/errors')
+        .Page\warnings(['Adding a new point will update the latitude,'
+            .' the longitude and the altitude of the place'
+            .' to the avarage of all the points.'])
+        .'<form action="submit.php" method="post">'
+            .create_point_form_items($values)
+            .'<div class="hr"></div>'
+            .Form\button('Save Point')
+            .ItemList\itemHiddenInputs($id)
+        .'</form>'
+    )
+    .create_geolocation_panel($scripts, $base, (float)$place->latitude,
+        (float)$place->longitude, (float)$place->altitude);
 
 include_once "$fnsDir/echo_user_page.php";
-echo_user_page($user, "Add New Point to Place #$place->id", $content, $base);
+echo_user_page($user, "Add New Point to Place #$place->id",
+    $content, $base, ['scripts' => $scripts]);
