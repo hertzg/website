429c1e4ac794d983a7d63f6ba58e232dfdb2f7c8
Archil Imnadze <archil@imnadze.ge>
2016-02-15 09:30:07 +0000

    remove className parameter of compressed_js_script js function

--- www/js/ui/combined.js
+++ www/js/ui/combined.js
@@ -79,14 +79,13 @@ function CompressedCssLink (revisions) {
 }
 ;
 function CompressedJsScript (revisions) {
-    return function (parentNode, name, base, className) {
+    return function (parentNode, name, base) {
 
         var fullName = 'js/' + name + '/compressed.js'
 
         Element(parentNode, 'script', function (script) {
             script.type = 'text/javascript'
             script.src = base + fullName + '?' + revisions[fullName]
-            if (className !== undefined) script.className = className
         })
 
     }
