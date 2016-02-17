ccf1307c81ce9ca40b17597ab7a508f36d8d6b44
Archil Imnadze <archil@imnadze.ge>
2016-02-17 08:31:00 +0000

    remove extra newlines

--- www/wallets/quick-transfer-amount/fns/get_values.php
+++ www/wallets/quick-transfer-amount/fns/get_values.php
@@ -5,7 +5,6 @@ function get_values ($wallets) {
     $key = 'wallets/quick-transfer-amount/values';
     if (array_key_exists($key, $_SESSION)) return $_SESSION[$key];
 
-
     return [
         'focus' => 'from_id_wallets',
         'from_id_wallets' => $wallets[0]->id,
