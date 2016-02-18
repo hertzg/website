872e70389bfc1721b409f5e0ff02b9018da32862
Archil Imnadze <archil@imnadze.ge>
2016-02-18 11:03:57 +0000

    prepare home item move page for implementing local navigation

---
+++ www/home/customize/reorder/move/fns/unset_session_vars.php
@@ -0,0 +1,5 @@
+<?php
+
+function unset_session_vars () {
+    unset($_SESSION['home/customize/reorder/messages']);
+}
--- www/home/customize/reorder/move/index.php
+++ www/home/customize/reorder/move/index.php
@@ -3,12 +3,10 @@
 include_once 'fns/require_item.php';
 list($item, $key, $user) = require_item();
 
+include_once 'fns/unset_session_vars.php';
+unset_session_vars();
+
 $title = $item['title'];
-
-unset($_SESSION['home/customize/reorder/messages']);
-
-$topHref = "submit-to-top.php?key=$key";
-$bottomHref = "submit-to-bottom.php?key=$key";
 
 $fnsDir = '../../../../fns';
 
@@ -24,13 +22,15 @@ $content = Page\create(
     'Move Item',
     Page\text("Where would you like to move \"<b>$title</b>\"?")
     .'<div class="hr"></div>'
-    .Page\imageLink('Move to the Top', $topHref, 'move-to-top')
+    .Page\imageLink('Move to the Top',
+        "submit-to-top.php?key=$key", 'move-to-top')
     .'<div class="hr"></div>'
     .Page\imageLink('Move Up', "submit-up.php?key=$key", 'move-up')
     .'<div class="hr"></div>'
     .Page\imageLink('Move Down', "submit-down.php?key=$key", 'move-down')
     .'<div class="hr"></div>'
-    .Page\imageLink('Move to the Bottom', $bottomHref, 'move-to-bottom')
+    .Page\imageLink('Move to the Bottom',
+        "submit-to-bottom.php?key=$key", 'move-to-bottom')
 );
 
 include_once "$fnsDir/echo_user_page.php";
