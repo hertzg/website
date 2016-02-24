42f89ec9cf8f7efe845433b0934b91e7e1c82995
Archil Imnadze <archil@imnadze.ge>
2016-02-24 20:21:32 +0000

    pack links on account page in Page\twoColumns

--- www/account/fns/create_options_panel.php
+++ www/account/fns/create_options_panel.php
@@ -13,21 +13,24 @@ function create_options_panel ($user) {
         $content = '';
     }
 
+    include_once "$fnsDir/Page/twoColumns.php";
     $content .=
-        Page\imageArrowLink('Change Password', 'change-password/',
-            'edit-password', ['id' => 'change-password'])
+        Page\twoColumns(
+            Page\imageArrowLink('Change Password', 'change-password/',
+                'edit-password', ['id' => 'change-password']),
+            Page\imageArrowLink('Edit Profile',
+                'edit-profile/', 'edit-profile', ['id' => 'edit-profile'])
+        )
         .'<div class="hr"></div>'
-        .Page\imageArrowLink('Edit Profile',
-            'edit-profile/', 'edit-profile', ['id' => 'edit-profile'])
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Edit Theme', 'edit-theme/',
-            "edit-$user->theme_color-$user->theme_brightness-theme",
-            ['id' => 'edit-theme'])
-        .'<div class="hr"></div>'
-        .Page\imageArrowLink('Close Account', 'close/', 'trash-bin', [
-            'id' => 'close',
-            'localNavigation' => true,
-        ]);
+        .Page\twoColumns(
+            Page\imageArrowLink('Edit Theme', 'edit-theme/',
+                "edit-$user->theme_color-$user->theme_brightness-theme",
+                ['id' => 'edit-theme']),
+            Page\imageArrowLink('Close Account', 'close/', 'trash-bin', [
+                'id' => 'close',
+                'localNavigation' => true,
+            ])
+        );
 
     include_once "$fnsDir/create_panel.php";
     return create_panel('Options', $content);
