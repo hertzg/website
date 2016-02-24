79d19f647b5d2b0c7807793154112e9d726a4b61
Archil Imnadze <archil@imnadze.ge>
2016-02-24 19:16:24 +0000

    create acount page options using string concatenation instead of array+join combination

--- www/account/fns/create_options_panel.php
+++ www/account/fns/create_options_panel.php
@@ -5,40 +5,41 @@ function create_options_panel ($user) {
     $fnsDir = __DIR__.'/../../fns';
     include_once "$fnsDir/Page/imageArrowLink.php";
 
-    $options = [];
     if ($user->email !== '' && !$user->email_verified) {
-        $options[] = Page\imageArrowLink('Verify Email',
-            'verify-email/', 'yes', ['id' => 'verify-email']);
+        $content = Page\imageArrowLink('Verify Email',
+            'verify-email/', 'yes', ['id' => 'verify-email'])
+            .'<div class="hr"></div>';
+    } else {
+        $content = '';
     }
 
-    $options[] = Page\imageArrowLink('Change Password',
-        'change-password/', 'edit-password', ['id' => 'change-password']);
-
-    $options[] = Page\imageArrowLink('Edit Profile',
-        'edit-profile/', 'edit-profile', ['id' => 'edit-profile']);
-
-    $options[] = Page\imageArrowLink('Edit Theme', 'edit-theme/',
-        "edit-$user->theme_color-$user->theme_brightness-theme",
-        ['id' => 'edit-theme']);
-
-    include_once __DIR__.'/create_tokens_link.php';
-    $options[] = create_tokens_link($user);
-
-    include_once __DIR__.'/create_connections_link.php';
-    $options[] = create_connections_link($user);
-
     include_once __DIR__.'/create_api_keys_link.php';
-    $options[] = create_api_keys_link($user);
-
-    $options[] = Page\imageArrowLink('Authentication History',
-        'signins/', 'sign-ins', ['id' => 'signins']);
-
-    $options[] = Page\imageArrowLink('Close Account', 'close/', 'trash-bin', [
-        'id' => 'close',
-        'localNavigation' => true,
-    ]);
-
-    $content = join('<div class="hr"></div>', $options);
+    include_once __DIR__.'/create_connections_link.php';
+    include_once __DIR__.'/create_tokens_link.php';
+    $content .=
+        Page\imageArrowLink('Change Password', 'change-password/',
+            'edit-password', ['id' => 'change-password'])
+        .'<div class="hr"></div>'
+        .Page\imageArrowLink('Edit Profile',
+            'edit-profile/', 'edit-profile', ['id' => 'edit-profile'])
+        .'<div class="hr"></div>'
+        .Page\imageArrowLink('Edit Theme', 'edit-theme/',
+            "edit-$user->theme_color-$user->theme_brightness-theme",
+            ['id' => 'edit-theme'])
+        .'<div class="hr"></div>'
+        .create_tokens_link($user)
+        .'<div class="hr"></div>'
+        .create_connections_link($user)
+        .'<div class="hr"></div>'
+        .create_api_keys_link($user)
+        .'<div class="hr"></div>'
+        .Page\imageArrowLink('Authentication History',
+            'signins/', 'sign-ins', ['id' => 'signins'])
+        .'<div class="hr"></div>'
+        .Page\imageArrowLink('Close Account', 'close/', 'trash-bin', [
+            'id' => 'close',
+            'localNavigation' => true,
+        ]);
 
     include_once "$fnsDir/create_panel.php";
     return create_panel('Options', $content);
