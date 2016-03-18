aaa5ee1ad3e70f2c2991c6d420b3ff6d33acc261
Archil Imnadze <archil@imnadze.ge>
2016-03-18 09:51:42 +0000

    same column types for insert and update api key id columns

--- www/fns/Tasks/ensure.php
+++ www/fns/Tasks/ensure.php
@@ -9,6 +9,11 @@ function ensure ($mysqli) {
 
     $fnsDir = __DIR__.'/..';
 
+    $nullable_unsigned_bigint = [
+        'type' => 'bigint(20) unsigned',
+        'nullable' => true,
+    ];
+
     include_once "$fnsDir/ApiKeyName/column.php";
     $apiKeyNameColumn = \ApiKeyName\column(true);
 
@@ -25,10 +30,7 @@ function ensure ($mysqli) {
             'primary' => true,
         ],
         'id_users' => ['type' => 'bigint(20) unsigned'],
-        'insert_api_key_id' => [
-            'type' => 'bigint(20) unsigned',
-            'nullable' => true,
-        ],
+        'insert_api_key_id' => $nullable_unsigned_bigint,
         'insert_api_key_name' => $apiKeyNameColumn,
         'insert_time' => ['type' => 'bigint(20) unsigned'],
         'num_tags' => ['type' => 'tinyint(3) unsigned'],
@@ -46,10 +48,7 @@ function ensure ($mysqli) {
             'collation' => 'utf8_unicode_ci',
         ],
         'top_priority' => ['type' => 'tinyint(3) unsigned'],
-        'update_api_key_id' => [
-            'type' => 'bigint(20) unsigned',
-            'nullable' => true,
-        ],
+        'update_api_key_id' => $nullable_unsigned_bigint,
         'update_api_key_name' => $apiKeyNameColumn,
         'update_time' => ['type' => 'bigint(20) unsigned'],
     ]);
