ee5ea7b7dbd2171867a6f90d1929c48a21c063f4
Archil Imnadze <archil@imnadze.ge>
2016-03-23 04:58:30 -0400

    return early instead of if-else

--- www/fns/Session/authenticate.php
+++ www/fns/Session/authenticate.php
@@ -23,47 +23,48 @@ function authenticate ($mysqli, $username,
     $user = \Users\getByUsernameAndPassword($mysqli, $username, $password);
 
     if ($user) {
+
         if ($user->disabled) {
             $disabled = true;
             $user = null;
-        } else {
-
-            $_SESSION['user'] = $user;
-            $id_users = $user->id_users;
-
-            include_once "$fnsDir/Users/signIn.php";
-            \Users\signIn($mysqli, $id_users, $client_address);
-
-            include_once "$fnsDir/Signins/add.php";
-            \Signins\add($mysqli, $id_users, $client_address);
-
-            if ($remember) {
-                include_once __DIR__.'/remember.php';
-                remember($mysqli, $user);
-            }
-
-            if ($user->encryption_key === null) {
-
-                include_once "$fnsDir/EncryptionKey/random.php";
-                \EncryptionKey\random($password, $random_key,
-                    $encryption_key, $encryption_key_iv);
-
-                include_once "$fnsDir/Users/editEncryptionKey.php";
-                \Users\editEncryptionKey($mysqli, $id_users,
-                    $encryption_key, $encryption_key_iv);
-
-                $encryption_key = $random_key;
-
-            } else {
-                include_once "$fnsDir/Crypto/decrypt.php";
-                $encryption_key = \Crypto\decrypt($password,
-                    $user->encryption_key, $user->encryption_key_iv);
-            }
-
-            include_once __DIR__.'/EncryptionKey/set.php';
-            EncryptionKey\set($encryption_key);
-
+            return;
         }
+
+        $_SESSION['user'] = $user;
+        $id_users = $user->id_users;
+
+        include_once "$fnsDir/Users/signIn.php";
+        \Users\signIn($mysqli, $id_users, $client_address);
+
+        include_once "$fnsDir/Signins/add.php";
+        \Signins\add($mysqli, $id_users, $client_address);
+
+        if ($remember) {
+            include_once __DIR__.'/remember.php';
+            remember($mysqli, $user);
+        }
+
+        if ($user->encryption_key === null) {
+
+            include_once "$fnsDir/EncryptionKey/random.php";
+            \EncryptionKey\random($password, $random_key,
+                $encryption_key, $encryption_key_iv);
+
+            include_once "$fnsDir/Users/editEncryptionKey.php";
+            \Users\editEncryptionKey($mysqli, $id_users,
+                $encryption_key, $encryption_key_iv);
+
+            $encryption_key = $random_key;
+
+        } else {
+            include_once "$fnsDir/Crypto/decrypt.php";
+            $encryption_key = \Crypto\decrypt($password,
+                $user->encryption_key, $user->encryption_key_iv);
+        }
+
+        include_once __DIR__.'/EncryptionKey/set.php';
+        EncryptionKey\set($encryption_key);
+
     } else {
         include_once "$fnsDir/InvalidSignins/add.php";
         \InvalidSignins\add($mysqli, $username, $client_address);
