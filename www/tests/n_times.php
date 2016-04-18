f9b861f2e60aa441f9b0f35f73e3498421832831
Archil Imnadze <archil@imnadze.ge>
2016-04-18 07:55:10 +0000

    tests for n_times functions

---
+++ www/tests/n_times.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+
+chdir(__DIR__);
+include_once '../fns/n_times.php';
+assert(n_times(-1) === '-1 times');
+assert(n_times(0) === '0 times');
+assert(n_times(1) === 'once');
+assert(n_times(2) === 'twice');
+assert(n_times(3) === '3 times');
+assert(n_times(123) === '123 times');
