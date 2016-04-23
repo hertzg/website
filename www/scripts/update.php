619e62f04c5a98082844533291af03558a74460f
Archil Imnadze <archil@imnadze.ge>
2016-04-23 23:35:19 +0000

    cleanup update script

--- www/scripts/update.php
+++ www/scripts/update.php
@@ -6,8 +6,6 @@ include_once '../../lib/cli.php';
 include_once '../../lib/defaults.php';
 include_once '../lib/mysqli.php';
 
-system('git checkout -- ../fns/ClientAddress');
-
 include_once '../fns/Table/ensureAll.php';
 echo Table\ensureAll($mysqli);
 
