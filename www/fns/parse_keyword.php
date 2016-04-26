b8cb246e99b16f6fbe32fe760a8a9608d14a00c2
Archil Imnadze <archil@imnadze.ge>
2016-04-26 14:28:49 +0000

    script to parse search keyword

---
+++ www/fns/parse_keyword.php
@@ -0,0 +1,67 @@
+<?php
+
+function parse_keyword ($keyword, &$includes, &$excludes) {
+
+    $includes = $excludes = [];
+
+    $index = 0;
+    $length = mb_strlen($keyword, 'UTF-8');
+
+    $read_term = function (&$target_array) use ($keyword, $length, &$index) {
+        $char = mb_substr($keyword, $index, 1, 'UTF-8');
+        if ($char === '"') {
+            $term = '';
+            while (true) {
+                $index++;
+                if ($index === $length) break;
+                $char = mb_substr($keyword, $index, 1, 'UTF-8');
+                if ($char === '"') {
+                    $index++;
+                    if ($index === $length) break;
+                    $char = mb_substr($keyword, $index, 1, 'UTF-8');
+                    if ($char !== '"') break;
+                }
+                $term .= $char;
+            }
+            if ($term === '') return;
+        } else {
+            $term = $char;
+            while (true) {
+                $index++;
+                if ($index === $length) break;
+                $char = mb_substr($keyword, $index, 1, 'UTF-8');
+                if ($char === ' ') {
+                    $index++;
+                    break;
+                }
+                $term .= $char;
+            }
+        }
+        $target_array[] = $term;
+    };
+
+    while ($index < $length) {
+        $char = mb_substr($keyword, $index, 1, 'UTF-8');
+        if ($char === ' ') {
+            $index++;
+        } elseif ($char === '"') {
+            $read_term($includes);
+        } elseif ($char === '-') {
+            $index++;
+            if ($index === $length) {
+                $includes[] = '-';
+                break;
+            }
+            $char = mb_substr($keyword, $index, 1, 'UTF-8');
+            if ($char === ' ') {
+                $includes[] = '-';
+                $index++;
+            } else {
+                $read_term($excludes);
+            }
+        } else {
+            $read_term($includes);
+        }
+    }
+
+}
---
+++ www/tests/parse_keyword.php
@@ -0,0 +1,57 @@
+#!/usr/bin/php
+<?php
+
+function visual_assert ($keyword, $expected_includes, $expected_excludes) {
+    parse_keyword($keyword, $includes, $excludes);
+    assert($includes === $expected_includes);
+    assert($excludes === $expected_excludes);
+}
+
+chdir(__DIR__);
+include_once '../fns/parse_keyword.php';
+visual_assert('', [], []);
+visual_assert(' ', [], []);
+visual_assert('  ', [], []);
+visual_assert(' a', ['a'], []);
+visual_assert('a ', ['a'], []);
+visual_assert(' a ', ['a'], []);
+visual_assert('  a  ', ['a'], []);
+visual_assert('a', ['a'], []);
+visual_assert('ab', ['ab'], []);
+visual_assert('a b', ['a', 'b'], []);
+visual_assert('a  b', ['a', 'b'], []);
+visual_assert('aa bb', ['aa', 'bb'], []);
+visual_assert('ა ბ', ['ა', 'ბ'], []);
+visual_assert('-', ['-'], []);
+visual_assert('--', [], ['-']);
+visual_assert('---', [], ['--']);
+visual_assert(' -a', [], ['a']);
+visual_assert('-a ', [], ['a']);
+visual_assert(' -a ', [], ['a']);
+visual_assert('  -a  ', [], ['a']);
+visual_assert('-a', [], ['a']);
+visual_assert('-ab', [], ['ab']);
+visual_assert('-a -b', [], ['a', 'b']);
+visual_assert('-a  -b', [], ['a', 'b']);
+visual_assert('-aa -bb', [], ['aa', 'bb']);
+visual_assert('-ა -ბ', [], ['ა', 'ბ']);
+visual_assert('a -b', ['a'], ['b']);
+visual_assert('-a b', ['b'], ['a']);
+visual_assert('-"a b"', [], ['a b']);
+visual_assert('--"a b"', ['b"'], ['-"a']);
+visual_assert('  "a"  ', ['a'], []);
+visual_assert('  "ab"  ', ['ab'], []);
+visual_assert('  "a b"  ', ['a b'], []);
+visual_assert('  "a  b"  ', ['a  b'], []);
+visual_assert('  "a b"  "c d"', ['a b', 'c d'], []);
+visual_assert('"a b" "c d"', ['a b', 'c d'], []);
+visual_assert('"a b""c d"', ['a b"c d'], []);
+visual_assert('"a b', ['a b'], []);
+visual_assert('a b"', ['a', 'b"'], []);
+visual_assert('""', [], []);
+visual_assert('a "" b', ['a', 'b'], []);
+visual_assert('a b "', ['a', 'b'], []);
+visual_assert('a - b', ['a', '-', 'b'], []);
+visual_assert('a  -  b', ['a', '-', 'b'], []);
+visual_assert('a  -', ['a', '-'], []);
+visual_assert('-  a', ['-', 'a'], []);
