5b4d7671e40afc44357076793e4792e6f867e69c
Archil Imnadze <archil@imnadze.ge>
2016-04-28 11:56:21 +0000

    note about admin panel location when installation is done

--- www/install/done/index.php
+++ www/install/done/index.php
@@ -7,12 +7,19 @@ require_installed('../../');
 
 include_once '../../lib/mysqli.php';
 
+include_once '../../fns/get_absolute_base.php';
+$absolute_base = htmlspecialchars(get_absolute_base().'admin/');
+
 include_once '../fns/echo_html.php';
 echo_html(
     'Installation Finished',
     '<h2 style="text-align: center">'
         .'The installation has finished.<br />Zvini is ready to be used.'
     .'</h2>'
+    .'<div style="text-align: center; margin-top: 32px">'
+        .'You can administrate the instance at:<br />'
+        ."<a class=\"link\" href=\"$absolute_base\">$absolute_base</a>"
+    .'</div>'
     .'<div style="text-align: center; margin-top: 32px">'
         .'<a class="button" href="../.." style="width: 120px">Go to Zvini</a>'
     .'</div>'
