6d28d3246b3c8f255df3cd3b78ea9edb1ce27674
Archil Imnadze <archil@imnadze.ge>
2016-05-04 12:12:35 +0000

    fix: search places using includes and excludes

--- www/fns/Places/search.php
+++ www/fns/Places/search.php
@@ -2,16 +2,22 @@
 
 namespace Places;
 
-function search ($mysqli, $id_users, $keyword) {
+function search ($mysqli, $id_users, $includes, $excludes) {
 
     $fnsDir = __DIR__.'/..';
 
     include_once "$fnsDir/escape_like.php";
-    $keyword = escape_like($keyword);
-    $keyword = $mysqli->real_escape_string($keyword);
+    $sql = "select * from places where id_users = $id_users";
+    foreach ($includes as $include) {
+        $include = $mysqli->real_escape_string(escape_like($include));
+        $sql .= " and name like '%$include%'";
+    }
+    foreach ($excludes as $exclude) {
+        $exclude = $mysqli->real_escape_string(escape_like($exclude));
+        $sql .= " and name not like '%$exclude%'";
+    }
+    $sql .= ' order by update_time desc';
 
-    $sql = "select * from places where id_users = $id_users"
-        ." and name like '%$keyword%' order by update_time desc";
     include_once "$fnsDir/mysqli_query_object.php";
     return mysqli_query_object($mysqli, $sql);
 
