026e0c13a9a38858d8e573472cc9fd61a706a3e1
Archil Imnadze <archil@imnadze.ge>
2016-05-06 12:15:05 +0000

    use rounded bullets, rectangular bullets are almost impossible to note

--- www/fns/get_revisions.php
+++ www/fns/get_revisions.php
@@ -4,7 +4,7 @@ function get_revisions () {
     return [
         'css/barChart/compressed.css' => 8,
         'css/calendarIcon/compressed.css' => 4,
-        'css/common/compressed.css' => 173,
+        'css/common/compressed.css' => 174,
         'css/confirmDialog/compressed.css' => 5,
         'css/contact/compressed.css' => 5,
         'css/iconsets/compressed.css' => 103,
