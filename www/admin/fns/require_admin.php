6f195c211eca25c725a943669086c642dec70989
Archil Imnadze <archil@imnadze.ge>
2016-05-17 09:25:42 +0000

    authenticate admin user in admin panel using http authentication

--- www/admin/fns/require_admin.php
+++ www/admin/fns/require_admin.php
@@ -2,12 +2,12 @@
 
 function require_admin () {
 
-    $invalid = function () {
-        include_once __DIR__.'/../../fns/ErrorPage/unauthorized.php';
+    $fnsDir = __DIR__.'/../../fns';
+
+    $invalid = function () use ($fnsDir) {
+        include_once "$fnsDir/ErrorPage/unauthorized.php";
         ErrorPage\unauthorized('Basic realm="Zvini Admin"');
     };
-
-    $fnsDir = __DIR__.'/../../fns';
 
     include_once "$fnsDir/signed_user.php";
     $user = signed_user();
@@ -16,12 +16,30 @@ function require_admin () {
 
     if (!array_key_exists('PHP_AUTH_USER', $_SERVER)) $invalid();
 
-    include_once "$fnsDir/Admin/get.php";
-    Admin\get($username, $hash, $salt, $sha512_hash, $sha512_key);
-
-    if ($username !== $_SERVER['PHP_AUTH_USER']) $invalid();
-
+    $username = $_SERVER['PHP_AUTH_USER'];
     $password = $_SERVER['PHP_AUTH_PW'];
+
+    include_once "$fnsDir/get_mysqli.php";
+    $mysqli = get_mysqli();
+
+    if (!$mysqli->connect_errno) {
+
+        include_once "$fnsDir/Session/authenticate.php";
+        $user = Session\authenticate($mysqli, $username,
+            $password, false, $disabled, $rate_limited);
+
+        if ($user) {
+            if (!$user->disabled && $user->admin) return $user;
+            include_once "$fnsDir/Session/invalidate.php";
+            Session\invalidate();
+        }
+
+    }
+
+    include_once "$fnsDir/Admin/get.php";
+    Admin\get($admin_username, $hash, $salt, $sha512_hash, $sha512_key);
+
+    if ($admin_username !== $username) $invalid();
 
     include_once "$fnsDir/Password/match.php";
     $match = Password\match($hash, $salt,
@@ -35,7 +53,7 @@ function require_admin () {
         list($sha512_hash, $sha512_key) = Password\hash($password);
 
         include_once "$fnsDir/Admin/set.php";
-        Admin\set($username, $sha512_hash, $sha512_key);
+        Admin\set($admin_username, $sha512_hash, $sha512_key);
 
     }
 
